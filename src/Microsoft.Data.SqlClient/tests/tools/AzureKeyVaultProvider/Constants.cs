fecd1384a6757f61c0b0e8cbfcf87b686721882e
Saurabh Singh (SQL) <sausing@microsoft.com>
2019-01-12 00:27:04 +0000

    Merged PR 1071: Adding Azure KeyVault provider
    I have migrated the Azure Key Vault provider so that we can test AE. I could build this for .Net Core. I haven't plumbed the whole build pipeline yet, but this should help Jeff get unblocked.

---
+++ src/Microsoft.Data.SqlClient/tests/tools/AzureKeyVaultProvider/Constants.cs
@@ -0,0 +1,34 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.SqlServer.Management.AlwaysEncrypted.AzureKeyVaultProvider
+{
+    internal static class Constants
+    {
+        /// <summary>
+        /// Hashing algoirthm used for signing
+        /// </summary>
+        internal const string HashingAlgorithm = @"RS256";
+
+        /// <summary>
+        /// Azure Key Vault Domain Name
+        /// </summary>
+        internal const string AzureKeyVaultPublicDomainName = @"vault.azure.net";
+
+        /// <summary>
+        /// Always Encrypted Param names for exec handling
+        /// </summary>
+        internal const string AeParamColumnEncryptionKey = "columnEncryptionKey";
+        internal const string AeParamEncryptionAlgorithm = "encryptionAlgorithm";
+        internal const string AeParamMasterKeyPath = "masterKeyPath";
+        internal const string AeParamEncryptedCek = "encryptedColumnEncryptionKey";
+
+    }
+}
