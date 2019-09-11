0def478f198645d9d219ccb4f48ff74e8532dddb
Cheena Malhotra <v-chmalh@microsoft.com>
2019-09-10 23:35:32 -0700

    Init Samples project

---
+++ docs/samples/Microsoft.Data.SqlClient.Samples/Runner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Data.SqlClient.Samples
+{
+    class Runner
+    {
+        internal static void Main(string[] args)
+        {
+#if NET46
+            TransactionIsolationLevelsProgram.Main(null);
+#endif
+        }
+    }
+}
