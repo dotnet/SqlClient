48d46182897adad61279cd74322ffae29289f25f
v-kaywon <v-kaywon@microsoft.com>
2019-09-19 14:48:03 -0700

    add documentation for server package

---
+++ doc/samples/SqlUserDefinedAggregate.cs
@@ -0,0 +1,30 @@
+using System;
+//<Snippet1>
+using Microsoft.Data.SqlClient.Server;
+using System.IO;
+using System.Data.Sql;
+using System.Data.SqlTypes;
+using System.Text;
+
+[Serializable]
+[Microsoft.SqlServer.Server.SqlUserDefinedAggregate(
+   Microsoft.SqlServer.Server.Format.UserDefined, 
+   IsInvariantToNulls = true,			
+   IsInvariantToDuplicates = false,		
+   IsInvariantToOrder = false,			
+   MaxByteSize = 8000)				
+        ]
+public class Concatenate : Microsoft.SqlServer.Server.IBinarySerialize
+{
+
+   public void Read(BinaryReader r)
+   {
+            
+   }
+
+   public void Write(BinaryWriter w)
+   {
+            
+   }
+}
+//</Snippet1>
