322b27a6960688a93580876e36b3324b70271501
Cheena Malhotra <v-chmalh@microsoft.com>
2019-09-25 17:33:43 -0700

    Update test to add more scenarios

--- src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/SqlCommand/SqlCommandCompletedTest.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/SqlCommand/SqlCommandCompletedTest.cs
@@ -6,27 +6,50 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
     public static class SqlCommandCompletedTest
     {
         private static readonly string s_connStr = (new SqlConnectionStringBuilder(DataTestUtility.TcpConnStr) { PacketSize = 512 }).ConnectionString;
-        private static bool completedHandlerExecuted = false;
+        private static int completedHandlerExecuted = 0;
 
         [ConditionalFact(typeof(DataTestUtility), nameof(DataTestUtility.AreConnStringsSetup))]
         public static void VerifyStatmentCompletedCalled()
         {
+            string tableName = DataTestUtility.GetUniqueNameForSqlServer("stmt");
             using (var conn = new SqlConnection(s_connStr))
             {
                 using (var cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = "SELECT COUNT(*) FROM sys.databases";
                     cmd.StatementCompleted += StatementCompletedHandler;
                     conn.Open();
-                    var res = cmd.ExecuteScalar();
+                    try
+                    {
+                        cmd.CommandText = $"CREATE TABLE {tableName} (c1 int)";
+                        var res = cmd.ExecuteScalar();
+
+                        cmd.CommandText = $"INSERT {tableName} VALUES(1)"; //DML (+1)
+                        res = cmd.ExecuteScalar();
+
+                        cmd.CommandText = $"Update {tableName} set c1=2"; //DML (+1)
+                        res = cmd.ExecuteScalar();
+
+                        cmd.CommandText = $"SELECT * from {tableName}"; //DQL (+1)
+                        res = cmd.ExecuteScalar();
+
+                        cmd.CommandText = $"DELETE FROM {tableName}"; //DML (+1)
+                        res = cmd.ExecuteScalar();
+                    }
+                    finally
+                    {
+                        cmd.CommandText = $"DROP TABLE {tableName}";
+                        var res = cmd.ExecuteScalar();
+                    }
                 }
             }
-            Assert.True(completedHandlerExecuted);
+            // DDL and DQL queries that return DoneRowCount are accounted here.
+            Assert.True(completedHandlerExecuted == 4);
         }
 
         private static void StatementCompletedHandler(object sender, StatementCompletedEventArgs args)
         {
-            completedHandlerExecuted = true;
+            // Increment on evert pass through
+            completedHandlerExecuted++;
         }
     }
 }
