fda88f1e364f0fa52cb651265560340a6857f042
Cheena Malhotra <v-chmalh@microsoft.com>
2019-09-30 23:49:25 -0700

    Fix headers for new files

--- src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlTypes/UnsafeNativeMethods.cs
+++ src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlTypes/UnsafeNativeMethods.cs
@@ -1,11 +1,6 @@
-//------------------------------------------------------------------------------
-// <copyright file="SafeNativeMethods.cs" company="Microsoft">
-//      Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-// <owner current="true" primary="true">Microsoft</owner>
-// <owner current="true" primary="false">Microsoft</owner>
-// <owner current="true" primary="false">Microsoft</owner>
-//------------------------------------------------------------------------------
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 using System;
 using System.Diagnostics;
