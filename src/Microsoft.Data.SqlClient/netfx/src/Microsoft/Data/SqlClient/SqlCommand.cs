65d034ec5e60bed2127f93aaa3168ad0584a7144
Stefán Jökull Sigurðarson <stefanjokull@hotmail.com>
2019-10-04 23:33:56 +0000

    Removing filtering of SQL Command text for non-stored procs when sending the BeginExecute event. (#242)

--- src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/SqlCommand.cs
+++ src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/SqlCommand.cs
@@ -7464,8 +7464,7 @@ namespace Microsoft.Data.SqlClient
         {
             if (SqlEventSource.Log.IsEnabled() && Connection != null)
             {
-                string commandText = CommandType == CommandType.StoredProcedure ? CommandText : string.Empty;
-                SqlEventSource.Log.BeginExecute(GetHashCode(), Connection.DataSource, Connection.Database, commandText);
+                SqlEventSource.Log.BeginExecute(GetHashCode(), Connection.DataSource, Connection.Database, CommandText);
             }
         }
 
