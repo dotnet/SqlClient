6990cc366182413a7a9844d70aa070eb650184d4
Cheena Malhotra <v-chmalh@microsoft.com>
2019-10-16 20:19:18 -0700

    Do not build referenced projects to avoid test issues.

--- src/Microsoft.Data.SqlClient/tests/RunTests.cmd
+++ src/Microsoft.Data.SqlClient/tests/RunTests.cmd
@@ -1,9 +1,12 @@
 @echo off
 cd ..\..\..\
-msbuild /t:BuildAllConfigurations /p:Configuration="Release"
+msbuild /p:Configuration="Release" /t:BuildAllConfigurations
+echo Building Add-Ons
+msbuild /p:Configuration="Release" /t:BuildAKVNetFx
+msbuild /p:Configuration="Release" /t:BuildAKVNetCore
 echo Building tests
-msbuild /t:BuildTestsNetCore /p:Configuration="Release"
-msbuild /t:BuildTestsNetFx /p:Configuration="Release"
+msbuild /p:Configuration="Release" /t:BuildTestsNetCore
+msbuild /p:Configuration="Release" /t:BuildTestsNetFx
 echo Running SqlClient test suite
 dotnet test "src\Microsoft.Data.SqlClient\tests\FunctionalTests\Microsoft.Data.SqlClient.Tests.csproj" /p:Platform="AnyCPU" /p:Configuration="Release" /p:TestTargetOS="Windowsnetcoreapp" --no-build -v n --filter "category!=nonnetcoreapptests&category!=failing&category!=nonwindowstests"
 dotnet test "src\Microsoft.Data.SqlClient\tests\FunctionalTests\Microsoft.Data.SqlClient.Tests.csproj" /p:Platform="Win32" /p:Configuration="Release" /p:TestTargetOS="Windowsnetfx" --no-build -v n --filter "category!=nonnetfxtests&category!=failing&category!=nonwindowstests"
