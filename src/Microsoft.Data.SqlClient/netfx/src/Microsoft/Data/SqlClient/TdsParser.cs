7278243349fc0dedcd31eabd410a4e91e38f407f
David Engel <v-daenge@microsoft.com>
2020-04-06 14:48:12 -0700

    Address MARS TDS header contained errors (#490)

--- src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/TdsParser.cs
+++ src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/TdsParser.cs
@@ -3326,10 +3326,11 @@ namespace Microsoft.Data.SqlClient
                 }
             }
 
+            // _attentionSent set by 'SendAttention'
             // _pendingData set by e.g. 'TdsExecuteSQLBatch'
             // _hasOpenResult always set to true by 'WriteMarsHeader'
             //
-            if (!stateObj._pendingData && stateObj._hasOpenResult)
+            if (!stateObj._attentionSent && !stateObj._pendingData && stateObj._hasOpenResult)
             {
                 /*
                                 Debug.Assert(!((sqlTransaction != null               && _distributedTransaction != null) ||
