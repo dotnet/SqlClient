9190a1c45fffd4ffb1cba6fd7c3ef7c27df16ed0
DavidEshtehari <daeshte@yahoo.com>
2020-04-06 21:33:27 -0700

    Fixed wrong application domain selected (#410)
    This change fixed the FileNotFoundException at SqlDependency.Start() by replacing the default AppDomain with the current.

---
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/SqlDependencyTest/SqlDependencyTest.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Xunit;
+
+namespace Microsoft.Data.SqlClient.ManualTesting.Tests
+{
+    public class SqlDependencyTest
+    {
+        [ConditionalFact(typeof(DataTestUtility), nameof(DataTestUtility.AreConnStringsSetup), nameof(DataTestUtility.IsNotAzureServer))]
+        public void SqlDependencyStartStopTest()
+        {
+            try
+            {
+                SqlDependency.Start(DataTestUtility.TCPConnectionString);
+                SqlDependency.Stop(DataTestUtility.TCPConnectionString);
+            }
+            catch (Exception e)
+            {
+                Assert.True(false, e.Message);
+            }
+        }
+    }
+}
