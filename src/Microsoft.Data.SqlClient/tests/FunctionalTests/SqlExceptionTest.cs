ffe6ab4f090b2236221dd946832aeda4df761d8f
Cheena Malhotra <v-chmalh@microsoft.com>
2020-04-16 11:36:02 -0700

    Add tests for SqlException serialization

---
+++ src/Microsoft.Data.SqlClient/tests/FunctionalTests/SqlExceptionTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Microsoft.Data.SqlClient.Tests
+{
+    public class SqlExceptionTest
+    {
+        private const string badServer = "92B96911A0BD43E8ADA4451031F7E7CF";
+
+        [Fact]
+        public void SerializationTest()
+        {
+            SqlException e = CreateException();
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(e);
+
+            var settings = new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.All,
+            };
+
+            var sqlEx = Newtonsoft.Json.JsonConvert.DeserializeObject<Microsoft.Data.SqlClient.SqlException>(json, settings);
+            Assert.Equal(e.ClientConnectionId, sqlEx.ClientConnectionId);
+            Assert.Equal(e.StackTrace, sqlEx.StackTrace);
+        }
+
+        [Fact]
+        public void JSONSerializationTest()
+        {
+            string clientConnectionId = "90cdab4d-2145-4c24-a354-c8ccff903542";
+            string json = @"{""ClassName"":""Microsoft.Data.SqlClient.SqlException"","
+                        + @"""Message"":""A network-related or instance-specific error occurred while establishing a connection to SQL Server. The server was not found or was not accessible. Verify that the instance name is correct and that SQL Server is configured to allow remote connections. (provider: TCP Provider, error: 40 - Could not open a connection to SQL Server)"","
+                        + @"""Data"":{""HelpLink.ProdName"":""Microsoft SQL Server"","
+                        + @"""HelpLink.EvtSrc"":""MSSQLServer"","
+                        + @"""HelpLink.EvtID"":""0"","
+                        + @"""HelpLink.BaseHelpUrl"":""http://go.microsoft.com/fwlink"","
+                        + @"""HelpLink.LinkId"":""20476"","
+                        + @"""SqlError 1"":""Microsoft.Data.SqlClient.SqlError: A network-related or instance-specific error occurred while establishing a connection to SQL Server. The server was not found or was not accessible. Verify that the instance name is correct and that SQL Server is configured to allow remote connections. (provider: TCP Provider, error: 40 - Could not open a connection to SQL Server)"","
+                        + @"""$type"":""System.Collections.ListDictionaryInternal, System.Private.CoreLib""},"
+                        + @"""InnerException"":null,"
+                        + @"""HelpURL"":null,"
+                        + @"""StackTraceString"":""   at Microsoft.Data.SqlClient.SqlInternalConnectionTds..ctor(DbConnectionPoolIdentity identity, SqlConnectionString connectionOptions, SqlCredential credential, Object providerInfo, String newPassword, SecureString newSecurePassword, Boolean redirectedUserInstance, SqlConnectionString userConnectionOptions, SessionData reconnectSessionData, Boolean applyTransientFaultHandling, String accessToken)\\n   at Microsoft.Data.SqlClient.SqlConnectionFactory.CreateConnection(DbConnectionOptions options, DbConnectionPoolKey poolKey, Object poolGroupProviderInfo, DbConnectionPool pool, DbConnection owningConnection, DbConnectionOptions userOptions)\\n   at System.Data.ProviderBase.DbConnectionFactory.CreatePooledConnection(DbConnectionPool pool, DbConnection owningObject, DbConnectionOptions options, DbConnectionPoolKey poolKey, DbConnectionOptions userOptions)\\n   at System.Data.ProviderBase.DbConnectionPool.CreateObject(DbConnection owningObject, DbConnectionOptions userOptions, DbConnectionInternal oldConnection)\\n   at System.Data.ProviderBase.DbConnectionPool.UserCreateRequest(DbConnection owningObject, DbConnectionOptions userOptions, DbConnectionInternal oldConnection)\\n   at System.Data.ProviderBase.DbConnectionPool.TryGetConnection(DbConnection owningObject, UInt32 waitForMultipleObjectsTimeout, Boolean allowCreate, Boolean onlyOneCheckConnection, DbConnectionOptions userOptions, DbConnectionInternal& connection)\\n   at System.Data.ProviderBase.DbConnectionPool.WaitForPendingOpen()\\n"","
+                        + @"""RemoteStackTraceString"":null,"
+                        + @"""RemoteStackIndex"":0,"
+                        + @"""ExceptionMethod"":null,"
+                        + @"""HResult"":-2146232060,"
+                        + @"""Source"":""Core .Net SqlClient Data Provider"","
+                        + @"""WatsonBuckets"":null,"
+                        + @"""Errors"":null,"
+                        + @"""ClientConnectionId"":""90cdab4d-2145-4c24-a354-c8ccff903542""}";
+
+            var settings = new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.All,
+            };
+
+            var sqlEx = Newtonsoft.Json.JsonConvert.DeserializeObject<Microsoft.Data.SqlClient.SqlException>(json, settings);
+            Assert.IsType<Microsoft.Data.SqlClient.SqlException>(sqlEx);
+            Assert.Equal(clientConnectionId, sqlEx.ClientConnectionId.ToString());
+        }
+
+        private static SqlException CreateException()
+        {
+            var builder = new SqlConnectionStringBuilder()
+            {
+                DataSource = badServer,
+                ConnectTimeout = 1,
+                Pooling = false
+            };
+
+            using (var connection = new SqlConnection(builder.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (SqlException ex)
+                {
+                    Assert.NotNull(ex.Errors);
+                    Assert.Single(ex.Errors);
+
+                    return ex;
+                }
+            }
+
+            throw new InvalidOperationException("SqlException should have been returned.");
+        }
+    }
+}
