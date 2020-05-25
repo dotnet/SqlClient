35c2dc0cb9b142cf64f040f280dddc11c8f1b941
Davoud Eshtehari <David.esh@gmail.com>
2020-05-25 08:54:30 -0700

    Added license

--- src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/SqlCommand/SqlCommandSetTest.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/SqlCommand/SqlCommandSetTest.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
 using System.Data.SqlTypes;
 using System.Reflection;
-using System.Text;
 using Xunit;
 
 namespace Microsoft.Data.SqlClient.ManualTesting.Tests
