1d77693e87b80aaec464a4d22141be710e897305
Cheena Malhotra <v-chmalh@microsoft.com>
2020-06-01 12:06:28 -0700

    Fix Application Intent keyword issue

--- src/Microsoft.Data.SqlClient/tests/FunctionalTests/SqlConnectionStringBuilderTest.cs
+++ src/Microsoft.Data.SqlClient/tests/FunctionalTests/SqlConnectionStringBuilderTest.cs
@@ -37,7 +37,7 @@ namespace Microsoft.Data.SqlClient.Tests
         [InlineData("Enclave Attestation Url = http://dymmyurl")]
         [InlineData("Encrypt = true")]
         [InlineData("Enlist = false")]
-        [InlineData("Failover Partner = randomserver.sys.local")]
+        [InlineData("Initial Catalog = Northwind; Failover Partner = randomserver.sys.local")]
         [InlineData("Initial Catalog = Northwind")]
         [InlineData("Integrated Security = true")]
         [InlineData("Trusted_Connection = false")]
@@ -147,7 +147,10 @@ namespace Microsoft.Data.SqlClient.Tests
                 Assert.Equal(valueBuilder1, valueBuilder2);
                 Assert.True(builder2.ContainsKey(key));
             }
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                Assert.NotNull(connection);
+            }
         }
-
     }
 }
