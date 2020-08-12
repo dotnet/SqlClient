91747186dcafd632d831feba6c43bcc06f9e729c
Karina Zhou <v-jizho2@microsoft.com>
2020-08-12 12:40:46 -0700

    Wrap server information for enclave session

--- src/Microsoft.Data.SqlClient/tests/ManualTests/AlwaysEncrypted/TestFixtures/Setup/AkvColumnMasterKey.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/AlwaysEncrypted/TestFixtures/Setup/AkvColumnMasterKey.cs
@@ -2,16 +2,22 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+
 namespace Microsoft.Data.SqlClient.ManualTesting.Tests.AlwaysEncrypted.Setup
 {
     public class AkvColumnMasterKey : ColumnMasterKey
     {
         public override string KeyPath { get; }
 
-        public AkvColumnMasterKey(string name, string akvUrl) : base(name)
+        public AkvColumnMasterKey(string name, string akvUrl, SqlColumnEncryptionKeyStoreProvider akvProvider, bool allowEnclaveComputations) : base(name)
         {
             KeyStoreProviderName = @"AZURE_KEY_VAULT";
             KeyPath = akvUrl;
+
+            // For keys which allow enclave computation
+            byte[] cmkSign = akvProvider.SignColumnMasterKeyMetadata(akvUrl, allowEnclaveComputations);
+            cmkSignStr = string.Concat("0x", BitConverter.ToString(cmkSign).Replace("-", string.Empty));
         }
     }
 }
