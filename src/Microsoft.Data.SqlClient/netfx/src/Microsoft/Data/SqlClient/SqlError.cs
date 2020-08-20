95f4603f2f42a5aa6e76bc82e2f3522337c7eefc
Cheena Malhotra <v-chmalh@microsoft.com>
2020-08-20 15:55:43 -0700

    More improvements.

--- src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/SqlError.cs
+++ src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/SqlError.cs
@@ -6,109 +6,108 @@ using System;
 
 namespace Microsoft.Data.SqlClient
 {
-    /// <include file='..\..\..\..\..\..\..\doc\snippets\Microsoft.Data.SqlClient\SqlError.xml' path='docs/members[@name="SqlError"]/SqlError/*' />
+    /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlError.xml' path='docs/members[@name="SqlError"]/SqlError/*' />
     [Serializable]
     public sealed class SqlError
     {
-
         // bug fix - MDAC 48965 - missing source of exception
-        private string source = TdsEnums.SQL_PROVIDER_NAME;
-        private int number;
-        private byte state;
-        private byte errorClass;
-        [System.Runtime.Serialization.OptionalFieldAttribute(VersionAdded = 2)]
-        private string server;
-        private string message;
-        private string procedure;
-        private int lineNumber;
-        [System.Runtime.Serialization.OptionalFieldAttribute(VersionAdded = 4)]
-        private int win32ErrorCode;
+        private readonly string _source = TdsEnums.SQL_PROVIDER_NAME;
+        private readonly int _number;
+        private readonly byte _state;
+        private readonly byte _errorClass;
+        [System.Runtime.Serialization.OptionalField(VersionAdded = 2)]
+        private readonly string _server;
+        private readonly string _message;
+        private readonly string _procedure;
+        private readonly int _lineNumber;
+        [System.Runtime.Serialization.OptionalField(VersionAdded = 4)]
+        private readonly int _win32ErrorCode;
 
         internal SqlError(int infoNumber, byte errorState, byte errorClass, string server, string errorMessage, string procedure, int lineNumber, uint win32ErrorCode)
             : this(infoNumber, errorState, errorClass, server, errorMessage, procedure, lineNumber)
         {
-            this.win32ErrorCode = (int)win32ErrorCode;
+            _win32ErrorCode = (int)win32ErrorCode;
         }
 
         internal SqlError(int infoNumber, byte errorState, byte errorClass, string server, string errorMessage, string procedure, int lineNumber)
         {
-            this.number = infoNumber;
-            this.state = errorState;
-            this.errorClass = errorClass;
-            this.server = server;
-            this.message = errorMessage;
-            this.procedure = procedure;
-            this.lineNumber = lineNumber;
+            _number = infoNumber;
+            _state = errorState;
+            _errorClass = errorClass;
+            _server = server;
+            _message = errorMessage;
+            _procedure = procedure;
+            _lineNumber = lineNumber;
             if (errorClass != 0)
             {
                 SqlClientEventSource.Log.TraceEvent("<sc.SqlError.SqlError|ERR> infoNumber={0}, errorState={1}, errorClass={2}, errorMessage='{3}', procedure='{4}', lineNumber={5}", infoNumber, (int)errorState, (int)errorClass, errorMessage, procedure ?? "None", (int)lineNumber);
             }
-            this.win32ErrorCode = 0;
+            _win32ErrorCode = 0;
         }
 
-        /// <include file='..\..\..\..\..\..\..\doc\snippets\Microsoft.Data.SqlClient\SqlError.xml' path='docs/members[@name="SqlError"]/ToString/*' />
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlError.xml' path='docs/members[@name="SqlError"]/ToString/*' />
         // bug fix - MDAC #49280 - SqlError does not implement ToString();
         // I did not include an exception stack because the correct exception stack is only available 
         // on SqlException, and to obtain that the SqlError would have to have backpointers all the
         // way back to SqlException.  If the user needs a call stack, they can obtain it on SqlException.
         public override string ToString()
         {
-            //return this.GetType().ToString() + ": " + this.message;
-            return typeof(SqlError).ToString() + ": " + this.message; // since this is sealed so we can change GetType to typeof
+            //return GetType().ToString() + ": " + message;
+            return typeof(SqlError).ToString() + ": " + _message; // since this is sealed so we can change GetType to typeof
         }
 
-        /// <include file='..\..\..\..\..\..\..\doc\snippets\Microsoft.Data.SqlClient\SqlError.xml' path='docs/members[@name="SqlError"]/Source/*' />
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlError.xml' path='docs/members[@name="SqlError"]/Source/*' />
         // bug fix - MDAC #48965 - missing source of exception
         public string Source
         {
-            get { return this.source; }
+            get { return _source; }
         }
 
-        /// <include file='..\..\..\..\..\..\..\doc\snippets\Microsoft.Data.SqlClient\SqlError.xml' path='docs/members[@name="SqlError"]/Number/*' />
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlError.xml' path='docs/members[@name="SqlError"]/Number/*' />
         public int Number
         {
-            get { return this.number; }
+            get { return _number; }
         }
 
-        /// <include file='..\..\..\..\..\..\..\doc\snippets\Microsoft.Data.SqlClient\SqlError.xml' path='docs/members[@name="SqlError"]/State/*' />
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlError.xml' path='docs/members[@name="SqlError"]/State/*' />
         public byte State
         {
-            get { return this.state; }
+            get { return _state; }
         }
 
-        /// <include file='..\..\..\..\..\..\..\doc\snippets\Microsoft.Data.SqlClient\SqlError.xml' path='docs/members[@name="SqlError"]/Class/*' />
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlError.xml' path='docs/members[@name="SqlError"]/Class/*' />
         public byte Class
         {
-            get { return this.errorClass; }
+            get { return _errorClass; }
         }
 
-        /// <include file='..\..\..\..\..\..\..\doc\snippets\Microsoft.Data.SqlClient\SqlError.xml' path='docs/members[@name="SqlError"]/Server/*' />
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlError.xml' path='docs/members[@name="SqlError"]/Server/*' />
         public string Server
         {
-            get { return this.server; }
+            get { return _server; }
         }
 
-        /// <include file='..\..\..\..\..\..\..\doc\snippets\Microsoft.Data.SqlClient\SqlError.xml' path='docs/members[@name="SqlError"]/Message/*' />
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlError.xml' path='docs/members[@name="SqlError"]/Message/*' />
         public string Message
         {
-            get { return this.message; }
+            get { return _message; }
         }
 
-        /// <include file='..\..\..\..\..\..\..\doc\snippets\Microsoft.Data.SqlClient\SqlError.xml' path='docs/members[@name="SqlError"]/Procedure/*' />
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlError.xml' path='docs/members[@name="SqlError"]/Procedure/*' />
         public string Procedure
         {
-            get { return this.procedure; }
+            get { return _procedure; }
         }
 
-        /// <include file='..\..\..\..\..\..\..\doc\snippets\Microsoft.Data.SqlClient\SqlError.xml' path='docs/members[@name="SqlError"]/LineNumber/*' />
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlError.xml' path='docs/members[@name="SqlError"]/LineNumber/*' />
         public int LineNumber
         {
-            get { return this.lineNumber; }
+            get { return _lineNumber; }
         }
 
         internal int Win32ErrorCode
         {
-            get { return this.win32ErrorCode; }
+            get { return _win32ErrorCode; }
         }
     }
 }
