eab41065d08e7eed0068885510b0e7eb8d02a917
Davoud Eshtehari <David.esh@gmail.com>
2020-10-12 18:42:22 -0700

    Add parameter options & generalize the pre-condition

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/Reliability/SqlConfigurableRetryFactory.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/Reliability/SqlConfigurableRetryFactory.cs
@@ -5,44 +5,125 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Microsoft.Data.SqlClient
 {
-    /// Define the SQL command type by filtering purpose.
-    [Flags]
-    public enum FilterSqlStatements
+    /// <summary>
+    /// Provide fixed retry parameters.
+    /// </summary>
+    public interface IFixedRetryLogicOption
     {
-        /// Don't filter any SQL commands
-        None = 0,
-        /// Filter INSERT or INSERT INTO
-        Insert = 1,
-        /// Filter UPDATE
-        Update = 2,
-        /// Filter DELETE
-        Delete = 1 << 2,
-        /// Filter EXECUTE or EXEC
-        Execute = 1 << 3,
-        /// Filter ALTER
-        Alter = 1 << 4,
-        /// Filter CREATE
-        Create = 1 << 5,
-        /// Filter DROP
-        Drop = 1 << 6,
-        /// Filter TRUNCATE
-        Truncate = 1 << 7,
-        /// Filter SELECT
-        Select = 1 << 8,
-        /// Filter data manipulation commands consist of INSERT, INSERT INTO, UPDATE, and DELETE
-        DML = Insert | Update | Delete | Truncate,
-        /// Filter data definition commands consist of ALTER, CREATE, and DROP
-        DDL = Alter | Create | Drop,
-        /// Filter any SQL command types
-        All = DML | DDL | Execute | Select
+        /// <summary>
+        /// Number of retries.
+        /// </summary>
+        int NumberOfTries { get; set; }
+
+        /// <summary>
+        /// The gap time of each interval.
+        /// </summary>
+        TimeSpan DeltaTime { get; set; }
+
+        /// <summary>
+        /// Authorized faults to apply retry strategy.
+        /// To use the internal list leave it null.
+        /// </summary>
+        IEnumerable<int> TransientErrors { get; set; }
+
+        /// <summary>
+        /// Pre-retry validation regarding the input string before checking the transient faults.
+        /// To skip this checking leave it null.
+        /// </summary>
+        /// <returns>True if the sender is authorized to retry the operation.</returns>
+        Predicate<string> AuthorizedSqlCondition { get; set; }
     }
 
-    /// Provide different retry strategies. 
+    /// <summary>
+    /// Provide floating retry parameters like exponential and incremental.
+    /// </summary>
+    public interface IFloatingRetryLogicOption : IFixedRetryLogicOption
+    {
+        /// <summary>
+        /// Minimum allowed time interval with default value zero.
+        /// </summary>
+        TimeSpan MinTimeInterval { get; set; }
+
+        /// <summary>
+        /// Maximum allowed time interval.
+        /// </summary>
+        TimeSpan MaxTimeInterval { get; set; }
+    }
+
+    /// <summary>
+    /// Provide fixed retry parameters.
+    /// </summary>
+    public sealed class FixedRetryLogicOption : IFixedRetryLogicOption
+    {
+        /// <summary>
+        /// Number of retries.
+        /// </summary>
+        public int NumberOfTries { get; set; }
+
+        /// <summary>
+        /// The gap time of each interval.
+        /// </summary>
+        public TimeSpan DeltaTime { get; set; }
+
+        /// <summary>
+        /// Authorized faults to apply retry strategy.
+        /// To use the internal list leave it null.
+        /// </summary>
+        public IEnumerable<int> TransientErrors { get; set; }
+
+        /// <summary>
+        /// Pre-retry validation regarding the input string before checking the transient faults.
+        /// To skip this checking leave it null.
+        /// </summary>
+        /// <returns>True if the sender is authorized to retry the operation.</returns>
+        public Predicate<string> AuthorizedSqlCondition { get; set; }
+    }
+
+    /// <summary>
+    /// Provide floating retry parameters like exponential and incremental.
+    /// </summary>
+    public sealed class FloatingRetryLogicOption : IFloatingRetryLogicOption
+    {
+        /// <summary>
+        /// Number of retries.
+        /// </summary>
+        public int NumberOfTries { get; set; }
+
+        /// <summary>
+        /// The gap time of each interval.
+        /// </summary>
+        public TimeSpan DeltaTime { get; set; }
+
+        /// <summary>
+        /// Minimum allowed time interval with default value zero.
+        /// </summary>
+        public TimeSpan MinTimeInterval { get; set; } = TimeSpan.FromSeconds(0);
+
+        /// <summary>
+        /// Maximum allowed time interval.
+        /// </summary>
+        public TimeSpan MaxTimeInterval { get; set; }
+
+        /// <summary>
+        /// Authorized faults to apply retry strategy.
+        /// To use the internal list leave it null.
+        /// </summary>
+        public IEnumerable<int> TransientErrors { get; set; }
+
+        /// <summary>
+        /// Pre-retry validation regarding the input string before checking the transient faults.
+        /// To skip this checking leave it null.
+        /// </summary>
+        /// <returns>True if the sender is authorized to retry the operation.</returns>
+        public Predicate<string> AuthorizedSqlCondition { get; set; }
+    }
+
+    /// <summary>
+    /// Provide different retry strategies.
+    /// </summary>
     public sealed class SqlConfigurableRetryFactory
     {
         /// Default known transient error numbers.
@@ -71,57 +152,66 @@ namespace Microsoft.Data.SqlClient
                     233,    // A connection was successfully established with the server, but then an error occurred during the login process. (provider: Shared Memory Provider, error: 0 - No process is on the other end of the pipe.) (Microsoft SQL Server, Error: 233)
                     64,
                     20,
-                    0,
                     0
                 };
 
+        /// <summary>
         /// Provide an exponential retry strategy.
-        public static SqlRetryLogicBaseProvider CreateExponentialRetryProvider(int numberOfTries,
-                                                TimeSpan deltaTimeBackoff,
-                                                TimeSpan maxTimeInterval,
-                                                FilterSqlStatements unauthorizedSqlStatements = FilterSqlStatements.DML,
-                                                IEnumerable<int> transientErrors = null,
-                                                TimeSpan minTimeInterval = default)
+        /// </summary>
+        public static SqlRetryLogicBaseProvider CreateExponentialRetryProvider(IFloatingRetryLogicOption retryLogicOption)
         {
-            var retryLogic = new SqlRetryLogic(numberOfTries,
-                                        new SqlExponentialIntervalEnumerator(deltaTimeBackoff, maxTimeInterval, minTimeInterval),
-                                        (e) => TransientErrorsCondition(e, transientErrors ?? s_defaultTransientErrors),
-                                        RetryPreConditon(unauthorizedSqlStatements));
+            if (retryLogicOption == null)
+            {
+                throw new ArgumentNullException(nameof(retryLogicOption));
+            }
+
+            var retryLogic = new SqlRetryLogic(retryLogicOption.NumberOfTries,
+                                        new SqlExponentialIntervalEnumerator(retryLogicOption.DeltaTime, retryLogicOption.MaxTimeInterval, retryLogicOption.MinTimeInterval),
+                                        (e) => TransientErrorsCondition(e, retryLogicOption.TransientErrors ?? s_defaultTransientErrors),
+                                        retryLogicOption.AuthorizedSqlCondition);
 
             return new SqlRetryLogicProvider(retryLogic);
         }
 
+        /// <summary>
         /// Provide an incrimental retry strategy.
-        public static SqlRetryLogicBaseProvider CreateIncrimentalRetryProvider(int numberOfTries,
-                                                TimeSpan timeInterval,
-                                                TimeSpan maxTimeInterval,
-                                                FilterSqlStatements unauthorizedSqlStatements = FilterSqlStatements.DML,
-                                                IEnumerable<int> transientErrors = null,
-                                                TimeSpan minTimeInterval = default)
+        /// </summary>
+        public static SqlRetryLogicBaseProvider CreateIncrementalRetryProvider(IFloatingRetryLogicOption retryLogicOption)
         {
-            var retryLogic = new SqlRetryLogic(numberOfTries,
-                                        new SqlIncrementalIntervalEnumerator(timeInterval, maxTimeInterval, minTimeInterval),
-                                        (e) => TransientErrorsCondition(e, transientErrors ?? s_defaultTransientErrors),
-                                        RetryPreConditon(unauthorizedSqlStatements));
+            if (retryLogicOption == null)
+            {
+                throw new ArgumentNullException(nameof(retryLogicOption));
+            }
+
+            var retryLogic = new SqlRetryLogic(retryLogicOption.NumberOfTries,
+                                        new SqlIncrementalIntervalEnumerator(retryLogicOption.DeltaTime, retryLogicOption.MaxTimeInterval, retryLogicOption.MinTimeInterval),
+                                        (e) => TransientErrorsCondition(e, retryLogicOption.TransientErrors ?? s_defaultTransientErrors),
+                                        retryLogicOption.AuthorizedSqlCondition);
 
             return new SqlRetryLogicProvider(retryLogic);
         }
 
+        /// <summary>
         /// Provide a fixed linear retry strategy.
-        public static SqlRetryLogicBaseProvider CreateFixedRetryProvider(int numberOfTries,
-                                                TimeSpan timeInterval,
-                                                FilterSqlStatements unauthorizedSqlStatements = FilterSqlStatements.DML,
-                                                IEnumerable<int> transientErrors = null)
+        /// </summary>
+        public static SqlRetryLogicBaseProvider CreateFixedRetryProvider(IFixedRetryLogicOption retryLogicOption)
         {
-            var retryLogic = new SqlRetryLogic(numberOfTries,
-                                        new SqlFixedIntervalEnumerator(timeInterval),
-                                        (e) => TransientErrorsCondition(e, transientErrors ?? s_defaultTransientErrors),
-                                        RetryPreConditon(unauthorizedSqlStatements));
+            if (retryLogicOption == null)
+            {
+                throw new ArgumentNullException(nameof(retryLogicOption));
+            }
+
+            var retryLogic = new SqlRetryLogic(retryLogicOption.NumberOfTries,
+                                        new SqlFixedIntervalEnumerator(retryLogicOption.DeltaTime),
+                                        (e) => TransientErrorsCondition(e, retryLogicOption.TransientErrors ?? s_defaultTransientErrors),
+                                        retryLogicOption.AuthorizedSqlCondition);
 
             return new SqlRetryLogicProvider(retryLogic);
         }
 
+        /// <summary>
         /// Provide a none retry strategy.
+        /// </summary>
         public static SqlRetryLogicBaseProvider CreateNoneRetryProvider()
         {
             var retryLogic = new SqlRetryLogic(new SqlNoneIntervalEnumerator());
@@ -129,7 +219,6 @@ namespace Microsoft.Data.SqlClient
             return new SqlRetryLogicProvider(retryLogic);
         }
 
-        #region private
         /// Return true if the exception is a transient fault or a Timeout exception.
         private static bool TransientErrorsCondition(Exception e, IEnumerable<int> retriableConditions)
         {
@@ -152,67 +241,5 @@ namespace Microsoft.Data.SqlClient
             }
             return result;
         }
-
-        /// Generate a predicate function to skip unauthorized SQL commands.
-        private static Predicate<string> RetryPreConditon(FilterSqlStatements unauthorizedSqlStatements)
-        {
-            var pattern = GetRegexPattern(unauthorizedSqlStatements);
-            return (commandText) => string.IsNullOrEmpty(pattern)
-                                    || !Regex.IsMatch(commandText, pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        }
-
-        /// Provide a regex pattern regarding to the SQL statement.
-        private static string GetRegexPattern(FilterSqlStatements sqlStatements)
-        {
-            if (sqlStatements == FilterSqlStatements.None)
-            {
-                return string.Empty;
-            }
-
-            var pattern = new StringBuilder();
-
-            if (sqlStatements.HasFlag(FilterSqlStatements.Insert))
-            {
-                pattern.Append(@"INSERT( +INTO){0,1}|");
-            }
-            if (sqlStatements.HasFlag(FilterSqlStatements.Update))
-            {
-                pattern.Append(@"UPDATE|");
-            }
-            if (sqlStatements.HasFlag(FilterSqlStatements.Delete))
-            {
-                pattern.Append(@"DELETE|");
-            }
-            if (sqlStatements.HasFlag(FilterSqlStatements.Execute))
-            {
-                pattern.Append(@"EXEC(UTE){0,1}|");
-            }
-            if (sqlStatements.HasFlag(FilterSqlStatements.Alter))
-            {
-                pattern.Append(@"ALTER|");
-            }
-            if (sqlStatements.HasFlag(FilterSqlStatements.Create))
-            {
-                pattern.Append(@"CREATE|");
-            }
-            if (sqlStatements.HasFlag(FilterSqlStatements.Drop))
-            {
-                pattern.Append(@"DROP|");
-            }
-            if (sqlStatements.HasFlag(FilterSqlStatements.Truncate))
-            {
-                pattern.Append(@"TRUNCATE|");
-            }
-            if (sqlStatements.HasFlag(FilterSqlStatements.Select))
-            {
-                pattern.Append(@"SELECT|");
-            }
-            if (pattern.Length > 0)
-            {
-                pattern.Remove(pattern.Length - 1, 1);
-            }
-            return string.Format(@"\b({0})\b", pattern.ToString());
-        }
-        #endregion
     }
 }
