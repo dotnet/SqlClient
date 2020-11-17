f6b5da432839c6fc6df6c700430b652258511e03
Cheena Malhotra <v-chmalh@microsoft.com>
2020-11-16 23:40:07 -0800

    Unblock Sync overrides

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/SNI/SNISslStream.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/SNI/SNISslStream.cs
@@ -14,30 +14,28 @@ namespace Microsoft.Data.SqlClient.SNI
     /// </summary>
     internal class SNISslStream : SslStream
     {
-        private readonly ConcurrentQueueSemaphore _writeQueueSemaphore;
-        private readonly ConcurrentQueueSemaphore _readQueueSemaphore;
+        private readonly ConcurrentQueueSemaphore _writeAsyncQueueSemaphore;
+        private readonly ConcurrentQueueSemaphore _readAsyncQueueSemaphore;
 
         public SNISslStream(Stream innerStream, bool leaveInnerStreamOpen, RemoteCertificateValidationCallback userCertificateValidationCallback)
             : base(innerStream, leaveInnerStreamOpen, userCertificateValidationCallback)
         {
-            _writeQueueSemaphore = new ConcurrentQueueSemaphore(1);
-            _readQueueSemaphore = new ConcurrentQueueSemaphore(1);
+            _writeAsyncQueueSemaphore = new ConcurrentQueueSemaphore(1);
+            _readAsyncQueueSemaphore = new ConcurrentQueueSemaphore(1);
         }
 
         // Prevent the ReadAsync's collision by running task in Semaphore Slim
         public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
         {
-            _readQueueSemaphore.Wait();
-            return base.ReadAsync(buffer, offset, count, cancellationToken)
-                .ContinueWith(t => _readQueueSemaphore.Release(t.Result));
+            return _readAsyncQueueSemaphore.WaitAsync().ContinueWith<int>(t => base.ReadAsync(buffer, offset, count, cancellationToken).Result)
+                .ContinueWith(t => _readAsyncQueueSemaphore.Release(t.Result));
         }
 
         // Prevent the WriteAsync's collision by running task in Semaphore Slim
         public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
         {
-            _writeQueueSemaphore.Wait();
-            return base.WriteAsync(buffer, offset, count, cancellationToken)
-                .ContinueWith(t => _writeQueueSemaphore.Release());
+            return _writeAsyncQueueSemaphore.WaitAsync().ContinueWith(t => base.WriteAsync(buffer, offset, count, cancellationToken))
+                .ContinueWith(t => _writeAsyncQueueSemaphore.Release());
         }
     }
 }
