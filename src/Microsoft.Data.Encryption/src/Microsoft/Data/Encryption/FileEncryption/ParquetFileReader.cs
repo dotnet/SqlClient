e7d0503af455dc5d14f01b51374ef10faa5648e3
Jeff Trimmer <trimmerjeff@gmail.com>
2020-11-18 09:23:13 -0800

    Add logic to prevent ParquetWriter from being created multiple times.

--- src/Microsoft.Data.Encryption/src/Microsoft/Data/Encryption/FileEncryption/ParquetFileReader.cs
+++ src/Microsoft.Data.Encryption/src/Microsoft/Data/Encryption/FileEncryption/ParquetFileReader.cs
@@ -93,6 +93,7 @@ namespace Microsoft.Data.Encryption.FileEncryption
         public void Dispose()
         {
             ParquetReader.Dispose();
+            
         }
 
         private IList<FileEncryptionSettings> GetEncryptionSettings()
