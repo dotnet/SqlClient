d40491363bed19880a290790cb740a1cbe85d74e
DavoudEshtehari <61173489+DavoudEshtehari@users.noreply.github.com>
2020-11-18 08:25:00 -0800

    Add samples for ADO.NET documentation (#797)
    imported documentes from .NET framework.

---
+++ doc/samples/SqlConnection_Pooling_Use_Statement.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=localhost;Database=master;Integrated Security=true;"
+        // <Snippet1>
+        // Assume that connectionString connects to master.  
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        using (SqlCommand command = new SqlCommand())
+        {
+            connection.Open();
+            command.Connection = connection;
+            command.Text = "USE DatabaseName";
+            command.ExecuteNonQuery();
+        }
+            // </Snippet1>
+    }
+}
