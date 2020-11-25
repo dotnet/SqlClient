e13b3c8c5d4fdc3f5dd94f2bb6e49096963fbcbf
Davoud Eshtehari <v-daesht@microsoft.com>
2020-11-25 10:49:19 -0800

    Add samples

---
+++ doc/samples/SqlDataReader_GetSchemaTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace NextResultCS
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s = GetConnectionString();
+            SqlConnection c = new SqlConnection(s);
+            GetSchemaInfo(c);
+            Console.ReadLine();
+        }
+        // <Snippet1>
+        static void GetSchemaInfo(SqlConnection connection)
+        {
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(
+                  "SELECT CategoryID, CategoryName FROM Categories;",
+                  connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Retrieve schema information about the current result-set.
+                DataTable schemaTable = reader.GetSchemaTable();
+
+                foreach (DataRow row in schemaTable.Rows)
+                {
+                    foreach (DataColumn column in schemaTable.Columns)
+                    {
+                        Console.WriteLine(String.Format("{0} = {1}",
+                           column.ColumnName, row[column]));
+                    }
+                }
+
+                // Always call the Close method when you have finished using the DataReader object.
+                reader.Close();
+            }
+        }
+        // </Snippet1>
+
+        static private string GetConnectionString()
+        {
+            // To avoid storing the connection string in your code,
+            // you can retrieve it from a configuration file.
+            return "Data Source=(local);Initial Catalog=Northwind;"
+                + "Integrated Security=SSPI";
+        }
+    }
+}
---
+++ doc/samples/SqlDataReader_HasRows.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace NextResultCS
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s = GetConnectionString();
+            SqlConnection c = new SqlConnection(s);
+            HasRows(c);
+            Console.ReadLine();
+        }
+
+        // <Snippet1>
+        static void HasRows(SqlConnection connection)
+        {
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(
+                  "SELECT CategoryID, CategoryName FROM Categories;",
+                  connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Check if the DataReader has any row.
+                if (reader.HasRows)
+                {
+                    // Obtain a row from the query result.
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+                            reader.GetString(1));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                // Always call the Close method when you have finished using the DataReader object.
+                reader.Close();
+            }
+        }
+
+        // </Snippet1>
+        static private string GetConnectionString()
+        {
+            // To avoid storing the connection string in your code,
+            // you can retrieve it from a configuration file.
+            return "Data Source=(local);Initial Catalog=Northwind;"
+                + "Integrated Security=SSPI";
+        }
+    }
+}
