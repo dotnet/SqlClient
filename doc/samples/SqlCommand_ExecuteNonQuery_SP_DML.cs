21c3e18f432d341046efa281c74cad2e381cdd75
DavoudEshtehari <61173489+DavoudEshtehari@users.noreply.github.com>
2020-11-25 12:40:24 -0800

    Add samples (#821)
    Related to the second part of porting documents from .NET Framework documents.

---
+++ doc/samples/SqlCommand_ExecuteNonQuery_SP_DML.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace SqlCommandCS
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = "Data Source=(local);Initial Catalog=Northwind;"
+                + "Integrated Security=SSPI";
+
+            CreateStoredProcedure(str);
+            CreateCommand(str);
+        }
+
+        private static void CreateStoredProcedure(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // <Snippet3>
+                // Assumes connection is a valid SqlConnection.
+                string queryString = "CREATE PROCEDURE InsertCategory  " + 
+                    "@CategoryName nchar(15), " + 
+                    "@Identity int OUT " + 
+                    "AS " + 
+                    "INSERT INTO Categories (CategoryName) VALUES(@CategoryName) " + 
+                    "SET @Identity = @@Identity " + 
+                    "RETURN @@ROWCOUNT";
+
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.ExecuteNonQuery();
+                // </Snippet3>
+            }
+        }
+
+        private static void CreateCommand(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(connection);
+
+                // <Snippet1>
+                // Assumes connection is a valid SqlConnection.
+                connection.Open();
+
+                string queryString = "INSERT INTO Customers " +
+                "(CustomerID, CompanyName) Values('NWIND', 'Northwind Traders')";
+
+                SqlCommand command = new SqlCommand(queryString, connection);
+                Int32 recordsAffected = command.ExecuteNonQuery();
+                // </Snippet1>
+
+                // <Snippet2>
+                // Assumes command is a valid SqlCommand with an open connection.
+                command.CommandText = "InsertCategory";
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter parameter = command.Parameters.Add("@RowCount", SqlDbType.Int);
+                parameter.Direction = ParameterDirection.ReturnValue;
+
+                parameter = command.Parameters.Add("@CategoryName", SqlDbType.NChar, 15);
+
+                parameter = command.Parameters.Add("@Identity", SqlDbType.Int);
+                parameter.Direction = ParameterDirection.Output;
+
+                command.Parameters["@CategoryName"].Value = "New Category";
+                command.ExecuteNonQuery();
+
+                Int32 categoryID = (Int32) command.Parameters["@Identity"].Value;
+                Int32 rowCount = (Int32) command.Parameters["@RowCount"].Value;
+                // </Snippet2>
+            }
+        }
+    }
+}
---
+++ doc/samples/SqlDataAdapter_FillDataSet.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace NextResultCS
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s = GetConnectionString();
+            SqlConnection c = new SqlConnection(s);
+            GetCustomers(c);
+            PrintCustomersOrders(c, c);
+            Console.ReadLine();
+        }
+
+        static DataSet GetCustomers(SqlConnection connection)
+        {
+            using (connection)
+            {
+            // <Snippet1>
+                // Assumes that connection is a valid SqlConnection object.
+                string queryString =
+                "SELECT CustomerID, CompanyName FROM dbo.Customers";
+                SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
+                
+                DataSet customers = new DataSet();
+                adapter.Fill(customers, "Customers");
+            //  </Snippet1>
+                return customers;
+            }
+        }
+
+        static void PrintCustomersOrders(SqlConnection customerConnection, SqlConnection orderConnection)
+        {
+            using (customerConnection)
+            using (orderConnection)
+            {
+                // <Snippet2>
+                // Assumes that customerConnection and orderConnection are valid SqlConnection objects.
+                SqlDataAdapter custAdapter = new SqlDataAdapter(
+                "SELECT * FROM dbo.Customers", customerConnection);
+                SqlDataAdapter ordAdapter = new SqlDataAdapter(
+                "SELECT * FROM Orders", orderConnection);
+
+                DataSet customerOrders = new DataSet();
+
+                custAdapter.Fill(customerOrders, "Customers");
+                ordAdapter.Fill(customerOrders, "Orders");
+
+                DataRelation relation = customerOrders.Relations.Add("CustOrders",
+                customerOrders.Tables["Customers"].Columns["CustomerID"],
+                customerOrders.Tables["Orders"].Columns["CustomerID"]);
+
+                foreach (DataRow pRow in customerOrders.Tables["Customers"].Rows)
+                {
+                    Console.WriteLine(pRow["CustomerID"]);
+                    foreach (DataRow cRow in pRow.GetChildRows(relation))
+                        Console.WriteLine("\t" + cRow["OrderID"]);
+                }
+                // </Snippet2>
+            }
+        }
+
+        static private string GetConnectionString()
+        {
+            // To avoid storing the connection string in your code,
+            // you can retrieve it from a configuration file.
+            return "Data Source=(local);Initial Catalog=Northwind;"
+                + "Integrated Security=SSPI";
+        }
+    }
+}
