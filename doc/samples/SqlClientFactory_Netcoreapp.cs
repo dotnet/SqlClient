6a6a9df90b71d7723cc6a74295d8d4c180ac3804
DavoudEshtehari <61173489+DavoudEshtehari@users.noreply.github.com>
2020-12-22 18:24:39 -0800

    Add sample (#860)
    +  SqlClientFactory

---
+++ doc/samples/SqlClientFactory_Netcoreapp.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+    }
+
+    // <Snippet1>
+    private static DbProviderFactory GetFactory()
+    {
+        // register SqlClientFactory in provider factories
+        DbProviderFactories.RegisterFactory("Microsoft.Data.SqlClient", SqlClientFactory.Instance);
+
+        return DbProviderFactories.GetFactory("Microsoft.Data.SqlClient");
+    }
+    // </Snippet1>
+}
