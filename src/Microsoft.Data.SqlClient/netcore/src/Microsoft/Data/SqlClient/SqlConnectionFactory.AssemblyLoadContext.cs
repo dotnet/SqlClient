cfa4083734b757ef305116fe48d9deba92acf089
Wraith2 <wraith2@gmail.com>
2021-02-15 01:05:28 +0000

    initial changes for unloading

---
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/SqlConnectionFactory.AssemblyLoadContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Microsoft.Data.SqlClient
+{
+    sealed internal partial class SqlConnectionFactory
+    { 
+        partial void SubscribeToAssemblyLoadContextUnload()
+        {
+            AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly()).Unloading += SqlConnectionFactoryAssemblyLoadContext_Unloading;
+        }
+
+        private void SqlConnectionFactoryAssemblyLoadContext_Unloading(AssemblyLoadContext obj)
+        {
+            Unload(obj, EventArgs.Empty);
+        }
+    }
+}
