bc7e7c8317d3e4f80bbc12f1b795f013136721f8
Cheena Malhotra <v-chmalh@microsoft.com>
2021-03-03 11:54:21 -0800

    [Release 1.1] Fix | Rework timeout timer to ensure guaranteed execution

--- src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/Common/SystemDataInternals/ConnectionHelper.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/Common/SystemDataInternals/ConnectionHelper.cs
@@ -10,15 +10,22 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests.SystemDataInternals
 {
     internal static class ConnectionHelper
     {
-        private static Assembly s_systemDotData = Assembly.Load(new AssemblyName(typeof(SqlConnection).GetTypeInfo().Assembly.FullName));
-        private static Type s_sqlConnection = s_systemDotData.GetType("Microsoft.Data.SqlClient.SqlConnection");
-        private static Type s_sqlInternalConnection = s_systemDotData.GetType("Microsoft.Data.SqlClient.SqlInternalConnection");
-        private static Type s_sqlInternalConnectionTds = s_systemDotData.GetType("Microsoft.Data.SqlClient.SqlInternalConnectionTds");
-        private static Type s_dbConnectionInternal = s_systemDotData.GetType("Microsoft.Data.ProviderBase.DbConnectionInternal");
+        private static Assembly s_MicrosoftDotData = Assembly.Load(new AssemblyName(typeof(SqlConnection).GetTypeInfo().Assembly.FullName));
+        private static Type s_sqlConnection = s_MicrosoftDotData.GetType("Microsoft.Data.SqlClient.SqlConnection");
+        private static Type s_sqlInternalConnection = s_MicrosoftDotData.GetType("Microsoft.Data.SqlClient.SqlInternalConnection");
+        private static Type s_sqlInternalConnectionTds = s_MicrosoftDotData.GetType("Microsoft.Data.SqlClient.SqlInternalConnectionTds");
+        private static Type s_dbConnectionInternal = s_MicrosoftDotData.GetType("Microsoft.Data.ProviderBase.DbConnectionInternal");
+        private static Type s_tdsParser = s_MicrosoftDotData.GetType("Microsoft.Data.SqlClient.TdsParser");
+        private static Type s_tdsParserStateObject = s_MicrosoftDotData.GetType("Microsoft.Data.SqlClient.TdsParserStateObject");
         private static PropertyInfo s_sqlConnectionInternalConnection = s_sqlConnection.GetProperty("InnerConnection", BindingFlags.Instance | BindingFlags.NonPublic);
         private static PropertyInfo s_dbConnectionInternalPool = s_dbConnectionInternal.GetProperty("Pool", BindingFlags.Instance | BindingFlags.NonPublic);
         private static MethodInfo s_dbConnectionInternalIsConnectionAlive = s_dbConnectionInternal.GetMethod("IsConnectionAlive", BindingFlags.Instance | BindingFlags.NonPublic);
         private static FieldInfo s_sqlInternalConnectionTdsParser = s_sqlInternalConnectionTds.GetField("_parser", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static PropertyInfo s_innerConnectionProperty = s_sqlConnection.GetProperty("InnerConnection", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static PropertyInfo s_tdsParserProperty = s_sqlInternalConnectionTds.GetProperty("Parser", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static FieldInfo s_tdsParserStateObjectProperty = s_tdsParser.GetField("_physicalStateObj", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static FieldInfo s_enforceTimeoutDelayProperty = s_tdsParserStateObject.GetField("_enforceTimeoutDelay", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static FieldInfo s_enforcedTimeoutDelayInMilliSeconds = s_tdsParserStateObject.GetField("_enforcedTimeoutDelayInMilliSeconds", BindingFlags.Instance | BindingFlags.NonPublic);
 
         public static object GetConnectionPool(object internalConnection)
         {
@@ -28,11 +35,11 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests.SystemDataInternals
 
         public static object GetInternalConnection(this SqlConnection connection)
         {
+            VerifyObjectIsConnection(connection);
             object internalConnection = s_sqlConnectionInternalConnection.GetValue(connection, null);
             Debug.Assert(((internalConnection != null) && (s_dbConnectionInternal.IsInstanceOfType(internalConnection))), "Connection provided has an invalid internal connection");
             return internalConnection;
         }
-
 
         public static bool IsConnectionAlive(object internalConnection)
         {
@@ -45,13 +52,32 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests.SystemDataInternals
             if (internalConnection == null)
                 throw new ArgumentNullException(nameof(internalConnection));
             if (!s_dbConnectionInternal.IsInstanceOfType(internalConnection))
-                throw new ArgumentException("Object provided was not a DbConnectionInternal", "internalConnection");
+                throw new ArgumentException("Object provided was not a DbConnectionInternal", nameof(internalConnection));
+        }
+
+        private static void VerifyObjectIsConnection(object connection)
+        {
+            if (connection == null)
+                throw new ArgumentNullException(nameof(connection));
+            if (!s_sqlConnection.IsInstanceOfType(connection))
+                throw new ArgumentException("Object provided was not a SqlConnection", nameof(connection));
         }
 
         public static object GetParser(object internalConnection)
         {
             VerifyObjectIsInternalConnection(internalConnection);
             return s_sqlInternalConnectionTdsParser.GetValue(internalConnection);
+        }
+
+        public static void SetEnforcedTimeout(this SqlConnection connection, bool enforce, int timeout)
+        {
+            VerifyObjectIsConnection(connection);
+            var stateObj = s_tdsParserStateObjectProperty.GetValue(
+                            s_tdsParserProperty.GetValue(
+                                s_innerConnectionProperty.GetValue(
+                                    connection, null), null));
+            s_enforceTimeoutDelayProperty.SetValue(stateObj, enforce);
+            s_enforcedTimeoutDelayInMilliSeconds.SetValue(stateObj, timeout);
         }
     }
 }
