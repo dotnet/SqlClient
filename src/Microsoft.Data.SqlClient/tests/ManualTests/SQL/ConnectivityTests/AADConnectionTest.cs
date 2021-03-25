195da2d99af5f1ed9a04369cb9c6bfe47c1f067f
Cheena Malhotra <v-chmalh@microsoft.com>
2021-03-25 13:58:53 -0700

    Fix test

--- src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/ConnectivityTests/AADConnectionTest.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/ConnectivityTests/AADConnectionTest.cs
@@ -388,7 +388,7 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
 
             AggregateException e = Assert.Throws<AggregateException>(() => ConnectAndDisconnect(connStrWithNoCred));
 
-            string expectedMessage = "Response: {\"error\":\"invalid_request\",\"error_description\":\"Identity not found\"}";
+            string expectedMessage = "ManagedIdentityCredential authentication unavailable, the requested identity has not been assigned to this resource.";
             Assert.Contains(expectedMessage, e.GetBaseException().Message);
         }
 
