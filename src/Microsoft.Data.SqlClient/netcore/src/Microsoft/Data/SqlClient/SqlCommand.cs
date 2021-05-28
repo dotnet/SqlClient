054b01364d45e214dc4742ee8ee1c7cfb781ae64
Johnny Pham <v-jopha@microsoft.com>
2021-05-28 16:57:27 -0700

    Update src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/SqlCommand.cs
    Co-authored-by: Cheena Malhotra <v-chmalh@microsoft.com>

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/SqlCommand.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/SqlCommand.cs
@@ -156,11 +156,7 @@ namespace Microsoft.Data.SqlClient
         {
             get
             {
-                if (requiresEnclaveComputations && !_activeConnection.Parser.AreEnclaveRetriesSupported)
-                {
-                    return false;
-                }
-                return true;
+                return !requiresEnclaveComputations || _activeConnection.Parser.AreEnclaveRetriesSupported;
             }
         }
 
