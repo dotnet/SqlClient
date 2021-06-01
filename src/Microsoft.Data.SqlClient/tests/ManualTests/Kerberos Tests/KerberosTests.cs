c6434129843c23db92fbe468fe112698782a790d
jJRahnama <jrahnama@simba.com>
2021-06-01 13:29:07 -0700

    init

--- src/Microsoft.Data.SqlClient/tests/ManualTests/Kerberos Tests/KerberosTests.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/Kerberos Tests/KerberosTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Microsoft.Data.SqlClient.ManualTesting.Tests
@@ -14,29 +15,50 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
 
         readonly SqlConnectionStringBuilder _builder = new()
         {
-            DataSource="ADO-WS2019-KERBEROS-TEST.galaxy.ad",
-            IntegratedSecurity=true
+            DataSource = "ADO-WS2019-KERBEROS-TEST.galaxy.ad",
+            IntegratedSecurity = true
         };
 
         [Fact]
-        public void IsKerBerosSetup()
+        public void FailsToConnectWithNoTicketIssued()
         {
             using var conn = new SqlConnection(_builder.ConnectionString);
-            try
+            Assert.Throws<SqlException>(() => conn.Open());
+        }
+
+        [Fact]
+        public void IsKerBerosSetupTest()
+        {
+            Task t = Task.Run(() => KerberosTicketManagemnt.Init()).ContinueWith((i) =>
             {
-                conn.Open();
-                using var command = new SqlCommand("SELECT auth_scheme from sys.dm_exec_connections where session_id = @@spid", conn);
-                using SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
+                using var conn = new SqlConnection(_builder.ConnectionString);
+                try
                 {
-                    Assert.Equal(AuthenticationScheme.KERBEROS.ToString(), reader.GetString(0));
+                    conn.Open();
+                    using var command = new SqlCommand("SELECT auth_scheme from sys.dm_exec_connections where session_id = @@spid", conn);
+                    using SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Assert.Equal(AuthenticationScheme.KERBEROS.ToString(), reader.GetString(0));
+                    }
                 }
-            }
-            catch(SqlException ex)
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Assert.False(true);
+                }
+
+            });
+        }
+
+        [Fact]
+        public void ExpiredTicketTest()
+        {
+            Task t = Task.Run(() => KerberosTicketManagemnt.Destroy()).ContinueWith((i) =>
             {
-                Console.WriteLine(ex.Message);
-                Assert.False(true);
-            }
+                using var conn = new SqlConnection(_builder.ConnectionString);
+                Assert.Throws<SqlException>(() => conn.Open());
+            });
         }
     }
 }
