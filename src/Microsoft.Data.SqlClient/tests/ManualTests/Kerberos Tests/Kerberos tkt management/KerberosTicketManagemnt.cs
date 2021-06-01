4a431b9efb57329dc9911427320c18fd8fe70999
jJRahnama <jrahnama@simba.com>
2021-06-01 12:55:23 -0700

    inital

---
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/Kerberos Tests/Kerberos tkt management/KerberosTicketManagemnt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Data.SqlClient.ManualTesting.Tests
+{
+    internal static class KerberosTicketManagemnt
+    {
+        internal static void Init()
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("kinit");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        internal static void Destroy()
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("kdestroy");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        internal static void List()
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("klist");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/Kerberos Tests/KerberosTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Microsoft.Data.SqlClient.ManualTesting.Tests
+{
+    public class KerberosTests
+    {
+        public enum AuthenticationScheme
+        {
+            KERBEROS
+        }
+
+        readonly SqlConnectionStringBuilder _builder = new()
+        {
+            DataSource="ADO-WS2019-KERBEROS-TEST.galaxy.ad",
+            IntegratedSecurity=true
+        };
+
+        [Fact]
+        public void IsKerBerosSetup()
+        {
+            using var conn = new SqlConnection(_builder.ConnectionString);
+            try
+            {
+                conn.Open();
+                using var command = new SqlCommand("SELECT auth_scheme from sys.dm_exec_connections where session_id = @@spid", conn);
+                using SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Assert.Equal(AuthenticationScheme.KERBEROS.ToString(), reader.GetString(0));
+                }
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Assert.False(true);
+            }
+        }
+    }
+}
