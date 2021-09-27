23f03b7114e4c0fc8e03c7444f582def23658a8f
Lawrence LCI <lcheunglci@gmail.com>
2021-09-27 10:59:02 -0700

    Update the ifdef from NETFX to NETFRAMEWORK because NETFX does not exist in the netfx csporj

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCommandSet.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCommandSet.cs
@@ -277,14 +277,14 @@ namespace Microsoft.Data.SqlClient
 
         internal int ExecuteNonQuery()
         {
-#if NETFX
+#if NETFRAMEWORK
             SqlConnection.ExecutePermission.Demand();
 #else
             ValidateCommandBehavior(nameof(ExecuteNonQuery), CommandBehavior.Default);
 #endif
             using (TryEventScope.Create("SqlCommandSet.ExecuteNonQuery | API | Object Id {0}, Commands executed in Batch RPC mode", ObjectID))
             {
-#if NETFX
+#if NETFRAMEWORK
                 if (Connection.IsContextConnection)
                 {
                     throw SQL.BatchedUpdatesNotAvailableOnContextConnection();
