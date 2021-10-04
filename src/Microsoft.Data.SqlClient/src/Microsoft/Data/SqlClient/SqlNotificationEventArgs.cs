a8f87d3ce101b8e8d97c23e4a87b719212b82ecc
Lawrence LCI <lcheunglci@gmail.com>
2021-10-04 16:19:37 -0700

    Move the netcore version of SqlNotificationEventArgs to shared src and update references in the csprojs

---
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlNotificationEventArgs.cs
@@ -0,0 +1,35 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Microsoft.Data.SqlClient
+{
+    /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlNotificationEventArgs.xml' path='docs/members[@name="SqlNotificationEventArgs"]/SqlNotificationEventArgs/*' />
+    public class SqlNotificationEventArgs : EventArgs
+    {
+        private SqlNotificationType _type;
+        private SqlNotificationInfo _info;
+        private SqlNotificationSource _source;
+
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlNotificationEventArgs.xml' path='docs/members[@name="SqlNotificationEventArgs"]/ctor/*' />
+        public SqlNotificationEventArgs(SqlNotificationType type, SqlNotificationInfo info, SqlNotificationSource source)
+        {
+            _info = info;
+            _source = source;
+            _type = type;
+        }
+
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlNotificationEventArgs.xml' path='docs/members[@name="SqlNotificationEventArgs"]/Type/*' />
+        public SqlNotificationType Type => _type;
+
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlNotificationEventArgs.xml' path='docs/members[@name="SqlNotificationEventArgs"]/Info/*' />
+        public SqlNotificationInfo Info => _info;
+
+        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlNotificationEventArgs.xml' path='docs/members[@name="SqlNotificationEventArgs"]/Source/*' />
+        public SqlNotificationSource Source => _source;
+
+        internal static SqlNotificationEventArgs s_notifyError = new SqlNotificationEventArgs(SqlNotificationType.Subscribe, SqlNotificationInfo.Error, SqlNotificationSource.Object);
+    }
+}
