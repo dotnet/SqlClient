71aef5f45c9fd5fb5b4ed4d9878ef023b259caa2
Lawrence LCI <lcheunglci@gmail.com>
2021-10-06 20:18:19 -0700

    Revise changes based on comments restore original approach forthe method CalculateHashCode and Equals and move the fields and properties to the top

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnectionPoolKey.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnectionPoolKey.cs
@@ -16,18 +16,12 @@ namespace Microsoft.Data.SqlClient
         private readonly SqlCredential _credential;
         private readonly string _accessToken;
 
-        public override object Clone()
-        {
-            return new SqlConnectionPoolKey(this);
-        }
+        internal SqlCredential Credential => _credential;
+        internal string AccessToken => _accessToken;
 
         internal override string ConnectionString
         {
-            get
-            {
-                return base.ConnectionString;
-            }
-
+            get => base.ConnectionString;
             set
             {
                 base.ConnectionString = value;
@@ -35,61 +29,11 @@ namespace Microsoft.Data.SqlClient
             }
         }
 
-        internal SqlCredential Credential => _credential;
-
-        internal string AccessToken
-        {
-            get
-            {
-                return _accessToken;
-            }
-        }
-
-        public override int GetHashCode()
-        {
-            return _hashValue;
-        }
-
 #if NETFRAMEWORK
+        #region NET Framework
         private readonly ServerCertificateValidationCallback _serverCertificateValidationCallback;
         private readonly ClientCertificateRetrievalCallback _clientCertificateRetrievalCallback;
         private readonly SqlClientOriginalNetworkAddressInfo _originalNetworkAddressInfo;
-
-        private SqlConnectionPoolKey(SqlConnectionPoolKey key) : base(key)
-        {
-            _credential = key.Credential;
-            _accessToken = key.AccessToken;
-            _serverCertificateValidationCallback = key._serverCertificateValidationCallback;
-            _clientCertificateRetrievalCallback = key._clientCertificateRetrievalCallback;
-            CalculateHashCode();
-        }
-
-        internal SqlConnectionPoolKey(string connectionString,
-                                    SqlCredential credential,
-                                    string accessToken,
-                                    ServerCertificateValidationCallback serverCertificateValidationCallback,
-                                    ClientCertificateRetrievalCallback clientCertificateRetrievalCallback,
-                                    SqlClientOriginalNetworkAddressInfo originalNetworkAddressInfo) : base(connectionString)
-        {
-            Debug.Assert(_credential == null || _accessToken == null, "Credential and AccessToken can't have the value at the same time.");
-            _credential = credential;
-            _accessToken = accessToken;
-            _serverCertificateValidationCallback = serverCertificateValidationCallback;
-            _clientCertificateRetrievalCallback = clientCertificateRetrievalCallback;
-            _originalNetworkAddressInfo = originalNetworkAddressInfo;
-            CalculateHashCode();
-        }
-
-        public override bool Equals(object obj)
-        {
-            return (obj is SqlConnectionPoolKey key
-                && _credential == key._credential
-                && ConnectionString == key.ConnectionString
-                && string.CompareOrdinal(_accessToken, key._accessToken) == 0
-                && _serverCertificateValidationCallback == key._serverCertificateValidationCallback
-                && _clientCertificateRetrievalCallback == key._clientCertificateRetrievalCallback
-                && _originalNetworkAddressInfo == key._originalNetworkAddressInfo);
-        }
 
         internal ServerCertificateValidationCallback ServerCertificateValidationCallback
             => _serverCertificateValidationCallback;
@@ -100,41 +44,24 @@ namespace Microsoft.Data.SqlClient
         internal SqlClientOriginalNetworkAddressInfo OriginalNetworkAddressInfo
             => _originalNetworkAddressInfo;
 
-        internal void CalculateHashCode()
+        internal SqlConnectionPoolKey(string connectionString,
+                            SqlCredential credential,
+                            string accessToken,
+                            ServerCertificateValidationCallback serverCertificateValidationCallback,
+                            ClientCertificateRetrievalCallback clientCertificateRetrievalCallback,
+                            SqlClientOriginalNetworkAddressInfo originalNetworkAddressInfo) : base(connectionString)
         {
-            _hashValue = base.GetHashCode();
-
-            if (_credential != null)
-            {
-                unchecked
-                {
-                    _hashValue = _hashValue * 17 + _credential.GetHashCode();
-                }
-            }
-            else if (_accessToken != null)
-            {
-                unchecked
-                {
-                    _hashValue = _hashValue * 17 + _accessToken.GetHashCode();
-                }
-            }
-
-            if (_originalNetworkAddressInfo != null)
-            {
-                unchecked
-                {
-                    _hashValue = _hashValue * 17 + _originalNetworkAddressInfo.GetHashCode();
-                }
-            }
-        }
-#else
-        private SqlConnectionPoolKey(SqlConnectionPoolKey key) : base(key)
-        {
-            _credential = key.Credential;
-            _accessToken = key.AccessToken;
+            Debug.Assert(_credential == null || _accessToken == null, "Credential and AccessToken can't have the value at the same time.");
+            _credential = credential;
+            _accessToken = accessToken;
+            _serverCertificateValidationCallback = serverCertificateValidationCallback;
+            _clientCertificateRetrievalCallback = clientCertificateRetrievalCallback;
+            _originalNetworkAddressInfo = originalNetworkAddressInfo;
             CalculateHashCode();
         }
-
+        #endregion
+#else
+        #region NET Core
         internal SqlConnectionPoolKey(string connectionString, SqlCredential credential, string accessToken) : base(connectionString)
         {
             Debug.Assert(_credential == null || _accessToken == null, "Credential and AccessToken can't have the value at the same time.");
@@ -142,16 +69,45 @@ namespace Microsoft.Data.SqlClient
             _accessToken = accessToken;
             CalculateHashCode();
         }
+        #endregion
+#endif
+
+        private SqlConnectionPoolKey(SqlConnectionPoolKey key) : base(key)
+        {
+            _credential = key.Credential;
+            _accessToken = key.AccessToken;
+#if NETFRAMEWORK
+            _serverCertificateValidationCallback = key._serverCertificateValidationCallback;
+            _clientCertificateRetrievalCallback = key._clientCertificateRetrievalCallback;
+#endif
+            CalculateHashCode();
+        }
+
+        public override object Clone()
+        {
+            return new SqlConnectionPoolKey(this);
+        }
 
         public override bool Equals(object obj)
         {
             return (obj is SqlConnectionPoolKey key
                 && _credential == key._credential
                 && ConnectionString == key.ConnectionString
-                && string.CompareOrdinal(_accessToken, key._accessToken) == 0);
+                && string.CompareOrdinal(_accessToken, key._accessToken) == 0
+#if NETFRAMEWORK
+                && _serverCertificateValidationCallback == key._serverCertificateValidationCallback
+                && _clientCertificateRetrievalCallback == key._clientCertificateRetrievalCallback
+                && _originalNetworkAddressInfo == key._originalNetworkAddressInfo
+#endif
+                );
         }
 
-        internal void CalculateHashCode()
+        public override int GetHashCode()
+        {
+            return _hashValue;
+        }
+
+        private void CalculateHashCode()
         {
             _hashValue = base.GetHashCode();
 
@@ -169,7 +125,16 @@ namespace Microsoft.Data.SqlClient
                     _hashValue = _hashValue * 17 + _accessToken.GetHashCode();
                 }
             }
+
+#if NETFRAMEWORK
+            if (_originalNetworkAddressInfo != null)
+            {
+                unchecked
+                {
+                    _hashValue = _hashValue * 17 + _originalNetworkAddressInfo.GetHashCode();
+                }
+            }
+#endif
         }
-#endif // NETFRAMEWORK
     }
 }
