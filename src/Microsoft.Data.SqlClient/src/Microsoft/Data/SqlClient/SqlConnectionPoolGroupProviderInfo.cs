9fa041d0d57b23e4df9a2314ccb4970a9564f375
Lawrence LCI <31262254+lcheunglci@users.noreply.github.com>
2021-10-08 15:26:11 -0700

    Apply suggestion to remove the ifdef for the System.Security namespace
    Co-authored-by: DavoudEshtehari <61173489+DavoudEshtehari@users.noreply.github.com>

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnectionPoolGroupProviderInfo.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnectionPoolGroupProviderInfo.cs
@@ -2,9 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if NETFRAMEWORK
 using System.Security;
-#endif
 using Microsoft.Data.ProviderBase;
 
 namespace Microsoft.Data.SqlClient
