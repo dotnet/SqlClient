f9a7c94c6bd6ae5f805d00eec3a46d6222621ec6
Lawrence LCI <lcheunglci@gmail.com>
2021-10-07 17:50:32 -0700

    Fix compiler error after code revision due to missing namespace

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnectionPoolGroupProviderInfo.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnectionPoolGroupProviderInfo.cs
@@ -2,6 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if NETFRAMEWORK
+using System.Security;
+#endif
 using Microsoft.Data.ProviderBase;
 
 namespace Microsoft.Data.SqlClient
