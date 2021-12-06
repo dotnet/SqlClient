659ba229c7b61e92b35a12d1326c319ae7a573fb
JRahnama <v-jarahn@microsoft.com>
2021-12-06 13:40:03 -0800

    address issue with localdb named pipe

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/LocalDBAPI.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/LocalDBAPI.cs
@@ -5,30 +5,37 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Microsoft.Data
 {
     internal static partial class LocalDBAPI
     {
-        private const string const_localDbPrefix = @"(localdb)\";
+        private const string LocalDbPrefix = @"(localdb)\";
+        private const string LocalDbPrefix_namedInstance = @"np:\\.\pipe\LOCALDB#";
 
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
         private delegate int LocalDBFormatMessageDelegate(int hrLocalDB, uint dwFlags, uint dwLanguageId, StringBuilder buffer, ref uint buflen);
 
         // check if name is in format (localdb)\<InstanceName - not empty> and return instance name if it is
+        // localDB can also have a format of np:\\.\pipe\LOCALDB#<some number>\tsql\query
         internal static string GetLocalDbInstanceNameFromServerName(string serverName)
         {
-            if (serverName == null)
-                return null;
-            serverName = serverName.TrimStart(); // it can start with spaces if specified in quotes
-            if (!serverName.StartsWith(const_localDbPrefix, StringComparison.OrdinalIgnoreCase))
-                return null;
-            string instanceName = serverName.Substring(const_localDbPrefix.Length).Trim();
-            if (instanceName.Length == 0)
-                return null;
-            else
-                return instanceName;
+            string instanceName = null;
+            bool isLocalDb = serverName.StartsWith(LocalDbPrefix) || serverName.StartsWith(LocalDbPrefix_namedInstance);
+            if (isLocalDb)
+            {
+                if (serverName.StartsWith(LocalDbPrefix))
+                {
+                    instanceName = serverName.Substring(LocalDbPrefix.Length).Trim();
+                }
+                else
+                {
+                    instanceName = serverName;
+                }
+            }
+            return instanceName;
         }
     }
 }
--- src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/LocalDBAPI.cs
+++ src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/LocalDBAPI.cs
@@ -19,8 +19,9 @@ namespace Microsoft.Data
 
     internal static class LocalDBAPI
     {
-        const string const_localDbPrefix = @"(localdb)\";
-        const string const_partialTrustFlagKey = "ALLOW_LOCALDB_IN_PARTIAL_TRUST";
+        private const string LocalDbPrefix = @"(localdb)\";
+        private const string LocalDbPrefix_namedInstance = @"np:\\.\pipe\LOCALDB#";
+        const string Const_partialTrustFlagKey = "ALLOW_LOCALDB_IN_PARTIAL_TRUST";
 
         static PermissionSet _fullTrust = null;
         static bool _partialTrustFlagChecked = false;
@@ -30,16 +31,20 @@ namespace Microsoft.Data
         // check if name is in format (localdb)\<InstanceName - not empty> and return instance name if it is
         internal static string GetLocalDbInstanceNameFromServerName(string serverName)
         {
-            if (serverName == null)
-                return null;
-            serverName = serverName.TrimStart(); // it can start with spaces if specified in quotes
-            if (!serverName.StartsWith(const_localDbPrefix, StringComparison.OrdinalIgnoreCase))
-                return null;
-            string instanceName = serverName.Substring(const_localDbPrefix.Length).Trim();
-            if (instanceName.Length == 0)
-                return null;
-            else
-                return instanceName;
+            string instanceName = null;
+            bool isLocalDb = serverName.StartsWith(LocalDbPrefix) || serverName.StartsWith(LocalDbPrefix_namedInstance);
+            if (isLocalDb)
+            {
+                if (serverName.StartsWith(LocalDbPrefix))
+                {
+                    instanceName = serverName.Substring(LocalDbPrefix.Length).Trim();
+                }
+                else
+                {
+                    instanceName = serverName;
+                }
+            }
+            return instanceName;
         }
 
 
@@ -261,7 +266,7 @@ namespace Microsoft.Data
             {
                 if (!_partialTrustFlagChecked)
                 {
-                    object partialTrustFlagValue = AppDomain.CurrentDomain.GetData(const_partialTrustFlagKey);
+                    object partialTrustFlagValue = AppDomain.CurrentDomain.GetData(Const_partialTrustFlagKey);
                     if (partialTrustFlagValue != null && partialTrustFlagValue is bool)
                     {
                         _partialTrustAllowed = (bool)partialTrustFlagValue;
