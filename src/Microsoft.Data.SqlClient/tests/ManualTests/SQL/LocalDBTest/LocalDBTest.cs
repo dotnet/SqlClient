c805b8f00ce605696a0bf541bdfffb7dc9dcb18e
Javad <v-jarahn@microsoft.com>
2022-01-14 13:45:41 -0800

    Fix | SqlLocalDB instance pipename issue with Encryption (#1433)

--- src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/LocalDBTest/LocalDBTest.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/LocalDBTest/LocalDBTest.cs
@@ -1,7 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+using System.Threading;
 using Xunit;
 
 namespace Microsoft.Data.SqlClient.ManualTesting.Tests
@@ -13,14 +15,18 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
         private static readonly string s_localDbConnectionString = @$"server=(localdb)\{DataTestUtility.LocalDbAppName}";
         private static readonly string[] s_sharedLocalDbInstances = new string[] { @$"server=(localdb)\.\{DataTestUtility.LocalDbSharedInstanceName}", @$"server=(localdb)\." };
         private static readonly string s_badConnectionString = $@"server=(localdb)\{DataTestUtility.LocalDbAppName};Database=DOES_NOT_EXIST;Pooling=false;";
+        private static readonly string s_commandPrompt = "cmd.exe";
+        private static readonly string s_sqlLocalDbInfo = @$"/c SqlLocalDb info {DataTestUtility.LocalDbAppName}";
+        private static readonly string s_startLocalDbCommand = @$"/c SqlLocalDb start {DataTestUtility.LocalDbAppName}";
+        private static readonly string s_localDbNamedPipeConnectionString = @$"server={GetLocalDbNamedPipe()}";
 
-        static string LocalDbName = DataTestUtility.LocalDbAppName;
         #region LocalDbTests
         [SkipOnTargetFramework(TargetFrameworkMonikers.Uap)] // No Registry support on UAP
         [ConditionalFact(nameof(IsLocalDBEnvironmentSet))]
         public static void SqlLocalDbConnectionTest()
         {
             ConnectionTest(s_localDbConnectionString);
+            ConnectionTest(s_localDbNamedPipeConnectionString);
         }
 
         [SkipOnTargetFramework(TargetFrameworkMonikers.Uap)] // No Registry support on UAP
@@ -30,6 +36,7 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
             // Encryption is not supported by SQL Local DB.
             // But connection should succeed as encryption is disabled by driver.
             ConnectionWithEncryptionTest(s_localDbConnectionString);
+            ConnectionWithEncryptionTest(s_localDbNamedPipeConnectionString);
         }
 
         [SkipOnTargetFramework(TargetFrameworkMonikers.Uap)] // No Registry support on UAP
@@ -37,6 +44,7 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
         public static void LocalDBMarsTest()
         {
             ConnectionWithMarsTest(s_localDbConnectionString);
+            ConnectionWithMarsTest(s_localDbNamedPipeConnectionString);
         }
 
         [SkipOnTargetFramework(TargetFrameworkMonikers.Uap)] // No Registry support on UAP
@@ -122,6 +130,40 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
             using SqlCommand command = new SqlCommand("SELECT @@SERVERNAME", connection);
             var result = command.ExecuteScalar();
             Assert.NotNull(result);
+        }
+
+        private static string GetLocalDbNamedPipe()
+        {
+            string state = ExecuteLocalDBCommandProcess(s_commandPrompt, s_sqlLocalDbInfo, "state");
+            while (state.Equals("stopped", StringComparison.InvariantCultureIgnoreCase))
+            {
+                state = ExecuteLocalDBCommandProcess(s_commandPrompt, s_startLocalDbCommand, "state");
+                Thread.Sleep(2000);
+            }
+            return ExecuteLocalDBCommandProcess(s_commandPrompt, s_sqlLocalDbInfo, "pipeName");
+        }
+
+        private static string ExecuteLocalDBCommandProcess(string filename, string arguments, string infoType)
+        {
+            ProcessStartInfo sInfo = new()
+            {
+                FileName = filename,
+                Arguments = arguments,
+                UseShellExecute = false,
+                CreateNoWindow = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            };
+            string[] lines = Process.Start(sInfo).StandardOutput.ReadToEnd().Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            if (infoType.Equals("state"))
+            {
+                return lines[5].Split(':')[1].Trim();
+            }
+            else if (infoType.Equals("pipeName"))
+            {
+                return lines[7].Split(new string[] { "Instance pipe name:" }, StringSplitOptions.None)[1].Trim();
+            }
+            return null;
         }
     }
 }
