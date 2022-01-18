90d3aad06dc02e20ec7dea1c04e467b64ac5df35
Davoud Eshtehari <v-deshtehari@microsoft.com>
2022-01-17 20:05:15 -0800

    make public InvalidUdtException.Create

--- src/Microsoft.SqlServer.Server/InvalidUdtException.cs
+++ src/Microsoft.SqlServer.Server/InvalidUdtException.cs
@@ -39,7 +39,8 @@ namespace Microsoft.SqlServer.Server
             base.GetObjectData(si, context);
         }
 
-        internal static InvalidUdtException Create(Type udtType, string resourceReason)
+        /// <include file='../../doc/snippets/Microsoft.Data.SqlClient.Server/InvalidUdtException.xml' path='docs/members[@name="InvalidUdtException"]/Create/*' />
+        internal static InvalidUdtException Create(Type udtType, string resourceReason = "SqlUdtReason_NoUdtAttribute")
         {
             string reason = StringsHelper.GetString(resourceReason);
             string message = StringsHelper.GetString(Strings.SqlUdt_InvalidUdtMessage, udtType.FullName, reason);
