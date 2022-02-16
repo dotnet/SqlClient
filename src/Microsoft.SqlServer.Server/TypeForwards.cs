2d63d6536a845a3c12121d079a90d7a248e4b5b6
Davoud Eshtehari <v-deshtehari@microsoft.com>
2022-02-16 14:10:24 -0800

    add MS license

--- src/Microsoft.SqlServer.Server/TypeForwards.cs
+++ src/Microsoft.SqlServer.Server/TypeForwards.cs
@@ -1,4 +1,7 @@
-﻿
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 [assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(Microsoft.SqlServer.Server.IBinarySerialize))]
 [assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(Microsoft.SqlServer.Server.InvalidUdtException))]
 [assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(Microsoft.SqlServer.Server.Format))]
