3e5be8e62f2f5c51e4d7368e9018e1615603c7e4
David Engel <dengel1012@gmail.com>
2022-04-07 17:46:05 -0700

    Parallelize SSRP requests when MSF is specified

--- src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/InstanceNameTest/InstanceNameTest.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/InstanceNameTest/InstanceNameTest.cs
@@ -2,7 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -34,14 +38,115 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
             }
         }
 
+        [ConditionalTheory(typeof(DataTestUtility), nameof(DataTestUtility.IsNotAzureServer), nameof(DataTestUtility.IsUsingManagedSNI))]
+        [InlineData(true, SqlConnectionIPAddressPreference.IPv4First)]
+        [InlineData(true, SqlConnectionIPAddressPreference.IPv6First)]
+        [InlineData(true, SqlConnectionIPAddressPreference.UsePlatformDefault)]
+        [InlineData(false, SqlConnectionIPAddressPreference.IPv4First)]
+        [InlineData(false, SqlConnectionIPAddressPreference.IPv6First)]
+        [InlineData(false, SqlConnectionIPAddressPreference.UsePlatformDefault)]
+        public static void ConnectManagedWithInstanceNameTest(bool useMultiSubnetFailover, SqlConnectionIPAddressPreference ipPreference)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(DataTestUtility.TCPConnectionString);
+            builder.MultiSubnetFailover = useMultiSubnetFailover;
+            builder.IPAddressPreference = ipPreference;
+
+            string hostname;
+            string instanceName;
+            int port;
+
+            Assert.True(ParseDataSource(builder.DataSource, out hostname, out port, out instanceName));
+
+            if (IsBrowserAlive(hostname) && IsValidInstance(hostname, instanceName))
+            {
+                builder.DataSource = hostname + "\\" + instanceName;
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                    {
+                        connection.Open();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Assert.True(false, "Unexpected connection failure: " + ex.Message);
+                }
+            }
+
+            builder.ConnectTimeout = 2;
+            instanceName = "invalidinstance3456";
+            if (!IsValidInstance(hostname, instanceName))
+            {
+                builder.DataSource = hostname + "\\" + instanceName;
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                    {
+                        connection.Open();
+                        Assert.True(false, "Unexpected connection success against " + instanceName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Assert.Contains("Error Locating Server/Instance Specified", ex.Message);
+                }
+            }
+        }
+
+        private static bool ParseDataSource(string dataSource, out string hostname, out int port, out string instanceName)
+        {
+            hostname = string.Empty;
+            port = -1;
+            instanceName = string.Empty;
+
+            if (dataSource.Contains(",") && dataSource.Contains("\\"))
+                return false;
+
+            if (dataSource.Contains(":"))
+            {
+                dataSource = dataSource.Substring(dataSource.IndexOf(":") + 1);
+            }
+
+            if (dataSource.Contains(","))
+            {
+                if (!Int32.TryParse(dataSource.Substring(dataSource.LastIndexOf(",") + 1), out port))
+                {
+                    return false;
+                }
+                dataSource = dataSource.Substring(0, dataSource.IndexOf(",") - 1);
+            }
+
+            if (dataSource.Contains("\\"))
+            {
+                instanceName = dataSource.Substring(dataSource.LastIndexOf("\\") + 1);
+                dataSource = dataSource.Substring(0, dataSource.LastIndexOf("\\"));
+            }
+
+            hostname = dataSource;
+
+            return hostname.Length > 0 && hostname.IndexOfAny(new char[] { '\\', ':', ',' }) == -1;
+        }
+
         private static bool IsBrowserAlive(string browserHostname)
+        {
+            const byte ClntUcastEx = 0x03;
+
+            byte[] responsePacket = QueryBrowser(browserHostname, new byte[] { ClntUcastEx });
+            return responsePacket != null && responsePacket.Length > 0;
+        }
+
+        private static bool IsValidInstance(string browserHostName, string instanceName)
+        {
+            byte[] request = CreateInstanceInfoRequest(instanceName);
+            byte[] response = QueryBrowser(browserHostName, request);
+            return response != null && response.Length > 0;
+        }
+
+        private static byte[] QueryBrowser(string browserHostname, byte[] requestPacket)
         {
             const int DefaultBrowserPort = 1434;
             const int sendTimeout = 1000;
             const int receiveTimeout = 1000;
-            const byte ClntUcastEx = 0x03;
-
-            byte[] requestPacket = new byte[] { ClntUcastEx };
             byte[] responsePacket = null;
             using (UdpClient client = new UdpClient(AddressFamily.InterNetwork))
             {
@@ -56,7 +161,21 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
                 }
                 catch { }
             }
-            return responsePacket != null && responsePacket.Length > 0;
+
+            return responsePacket;
+        }
+
+        private static byte[] CreateInstanceInfoRequest(string instanceName)
+        {
+            const byte ClntUcastInst = 0x04;
+            instanceName += char.MinValue;
+            int byteCount = Encoding.ASCII.GetByteCount(instanceName);
+
+            byte[] requestPacket = new byte[byteCount + 1];
+            requestPacket[0] = ClntUcastInst;
+            Encoding.ASCII.GetBytes(instanceName, 0, instanceName.Length, requestPacket, 1);
+
+            return requestPacket;
         }
     }
 }
