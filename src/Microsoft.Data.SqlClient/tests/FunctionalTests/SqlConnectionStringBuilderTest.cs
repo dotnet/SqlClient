a35356ba925f0a179f5e2dbe780faf7180dd73ed
Lawrence LCI <lcheunglci@gmail.com>
2022-09-26 20:50:00 -0700

    Update functional test for the keyword support for server certificate

--- src/Microsoft.Data.SqlClient/tests/FunctionalTests/SqlConnectionStringBuilderTest.cs
+++ src/Microsoft.Data.SqlClient/tests/FunctionalTests/SqlConnectionStringBuilderTest.cs
@@ -87,6 +87,8 @@ namespace Microsoft.Data.SqlClient.Tests
         [InlineData("WSID = myworkstation")]
         [InlineData("Host Name In Certificate = tds.test.com")]
         [InlineData("HostNameInCertificate = tds.test.com")]
+        [InlineData("Server Certificate = c:\\test.cer")]
+        [InlineData("ServerCertificate = c:\\test.cer")]
         [InlineData("Server SPN = server1")]
         [InlineData("ServerSPN = server2")]
         [InlineData("Failover Partner SPN = server3")]
