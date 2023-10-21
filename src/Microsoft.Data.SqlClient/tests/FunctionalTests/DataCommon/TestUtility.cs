d44e0fab79f4ead9aa9fdd99850b5ecc5a6de878
JRahnama <v-jarahn@microsoft.com>
2023-10-21 16:53:18 -0700

    commit

--- src/Microsoft.Data.SqlClient/tests/FunctionalTests/DataCommon/TestUtility.cs
+++ src/Microsoft.Data.SqlClient/tests/FunctionalTests/DataCommon/TestUtility.cs
@@ -9,8 +9,8 @@ namespace Microsoft.Data.SqlClient.Tests
 {
     public static class TestUtility
     {
-        public static readonly bool s_isNotArmProcess = RuntimeInformation.ProcessArchitecture != Architecture.Arm;
-        public static bool s_isFullFramework => RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework");
-        public static bool s_netNative => RuntimeInformation.FrameworkDescription.StartsWith(".NET Native");
+        public static readonly bool IsNotArmProcess = RuntimeInformation.ProcessArchitecture != Architecture.Arm;
+        public static bool IsFullFramework => RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework");
+        public static bool NetNative => RuntimeInformation.FrameworkDescription.StartsWith(".NET Native");
     }
 }
