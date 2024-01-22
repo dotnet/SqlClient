a270a02b130c54bfcc20947dc389bfbe75b271a0
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2024-01-22 07:33:13 +0100

    Revert "Perf | avoid boxing of SqlGuid in SqlBuffer (#2306)"
    This reverts commit 9602271e20a1983cf4f0edf3fb8e18d8538e112c.

--- src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/TdsParser.cs
+++ src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/TdsParser.cs
@@ -6310,7 +6310,7 @@ namespace Microsoft.Data.SqlClient
                     break;
 
                 case SqlDbType.UniqueIdentifier:
-                    nullVal.SetToNullOfType(SqlBuffer.StorageType.SqlGuid);
+                    nullVal.SqlGuid = SqlGuid.Null;
                     break;
 
                 case SqlDbType.Bit:
