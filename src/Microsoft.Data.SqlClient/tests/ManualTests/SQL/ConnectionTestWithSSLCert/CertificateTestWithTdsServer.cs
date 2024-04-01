aa0dfc23ab4565d4b1e8ef9fbe0f006ed39b9f27
v-arellegue <v-arellegue@microsoft.com>
2024-04-01 16:29:38 -0700

    Removed unnecessary using.

--- src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/ConnectionTestWithSSLCert/CertificateTestWithTdsServer.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/ConnectionTestWithSSLCert/CertificateTestWithTdsServer.cs
@@ -6,7 +6,6 @@ using System;
 using System.Data;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
