0f7c222e2dd22052d665292a6ea7e5d610e1d163
v-arellegue <v-arellegue@microsoft.com>
2024-04-12 15:09:04 -0700

    Added DataSource Parser Test of the DataTestUtility class.

---
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/DataSourceParserTest/DataSourceParserTest.cs
@@ -0,0 +1,61 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Data.Common;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace Microsoft.Data.SqlClient.ManualTesting.Tests.SQL.DataSourceParserTest
+{
+    public class DataSourceParserTest
+    {
+        [ConditionalTheory(typeof(DataTestUtility), nameof(DataTestUtility.IsNotAzureServer), nameof(DataTestUtility.IsNotAzureSynapse))]
+        [InlineData("localhost")]
+        [InlineData("tcp:localhost")]
+        [InlineData("np:localhost")]
+        public void ParseDataSourceWithoutInstanceNorPortTestShouldSucceed(string dataSource)
+        {
+            DataTestUtility.ParseDataSource(dataSource, out string hostname, out _, out _);
+            Assert.Equal("localhost", hostname);
+        }
+
+        [ConditionalTheory(typeof(DataTestUtility), nameof(DataTestUtility.IsNotAzureServer), nameof(DataTestUtility.IsNotAzureSynapse))]
+        [InlineData("localhost,1433")]
+        [InlineData("tcp:localhost,1433")]
+        [InlineData("np:localhost,1433")]
+        public void ParseDataSourceWithoutInstanceButWithPortTestShouldSucceed(string dataSource)
+        {
+            DataTestUtility.ParseDataSource(dataSource, out string hostname, out int port, out _);
+            Assert.Equal("localhost", hostname);
+            Assert.Equal(1433, port);
+        }
+
+        [ConditionalTheory(typeof(DataTestUtility), nameof(DataTestUtility.IsNotAzureServer), nameof(DataTestUtility.IsNotAzureSynapse))]
+        [InlineData("localhost\\MSSQLSERVER02")]
+        [InlineData("tcp:localhost\\MSSQLSERVER02")]
+        [InlineData("np:localhost\\MSSQLSERVER02")]
+        public void ParseDataSourceWithInstanceButWithoutPortTestShouldSucceed(string dataSource)
+        {
+            DataTestUtility.ParseDataSource(dataSource, out string hostname, out _, out string instanceName );
+            Assert.Equal("localhost", hostname);
+            Assert.Equal("MSSQLSERVER02", instanceName);
+        }
+
+        [ConditionalTheory(typeof(DataTestUtility), nameof(DataTestUtility.IsNotAzureServer), nameof(DataTestUtility.IsNotAzureSynapse))]
+        [InlineData("localhost\\MSSQLSERVER02,1433")]
+        [InlineData("tcp:localhost\\MSSQLSERVER02,1433")]
+        [InlineData("np:localhost\\MSSQLSERVER02,1433")]
+        public void ParseDataSourceWithInstanceAndPortTestShouldSucceed(string dataSource)
+        {
+            DataTestUtility.ParseDataSource(dataSource, out string hostname, out int port, out string instanceName);
+            Assert.Equal("localhost", hostname);
+            Assert.Equal("MSSQLSERVER02", instanceName);
+            Assert.Equal(1433, port);
+        }
+    }
+}
