6b2817fe3e85f48a60ba81f033b8b0e69099e00d
saurabh500 <1623701+saurabh500@users.noreply.github.com>
2024-05-02 17:17:33 -0700

    Trim the files down

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -7,20 +7,16 @@ namespace TestApplication
 {
     internal class Program
     {
-
-        static string connectionString = "Server=tcp:127.0.0.1;" +
-            "Min Pool Size=120;Max Pool Size = 200;User Id=sa; pwd=; " +
-            "Connection Timeout=30;TrustServerCertificate=True;Timeout=0;Encrypt=False;Database=master"; // pooled
-
+        
         static void Main(string[] args)
         {
-            //TestConnections().Wait();
-            SimpleConnectionTest();
-
-            //Thread.Sleep(10000);
+            string connectionString = $"Server=tcp:127.0.0.1;" +
+            $"Min Pool Size=120;Max Pool Size = 200;User Id=sa; pwd={Environment.GetEnvironmentVariable("SQL_PWD")}; " +
+            "Connection Timeout=30;TrustServerCertificate=True;Timeout=0;Encrypt=False;Database=master"; // pooled
+            SimpleConnectionTest(connectionString);
         }
 
-        private static void SimpleConnectionTest()
+        private static void SimpleConnectionTest(string connectionString)
         {
             //AppContext.SetSwitch("Switch.Microsoft.Data.SqlClient.UseManagedNetworkingOnWindows", true);
 
@@ -36,89 +32,5 @@ namespace TestApplication
                 Console.WriteLine(result);
             }
         }
-
-        static async Task TestConnections()
-        {
-
-            Console.WriteLine("WARM UP");
-            await MeasureSingleConnectionAndReuse(connectionString);
-
-            ClearPools();
-            await MeasureSingleConnectionAndReuse(connectionString);
-
-            ClearPools();
-            await MeasureSingleConnectionAndReuse(connectionString);
-
-            Console.WriteLine("\n\nCONCURRENT POOLED CONNECTIONS");
-            ClearPools();
-            MeasureParallelConnections(connectionString);
-
-            Console.WriteLine("\n\nCONCURRENT NON-POOLED CONNECTIONS");
-            ClearPools();
-            MeasureParallelConnections(connectionString + "Pooling=false;");
-
-
-            Console.WriteLine("\nTesting finished");
-            Console.ReadLine();
-        }
-
-        private static void ClearPools()
-        {
-            SqlConnection.ClearAllPools();
-            Console.WriteLine("ALL POOLS CLEARED");
-        }
-        static ConcurrentDictionary<Guid, object> _connectionIDs = new ConcurrentDictionary<Guid, object>();
-        private static void MeasureParallelConnections(string connectionString)
-        {
-            Console.WriteLine("Start delay      OpenAsync time     Connection ID                        ReusedFromPool");
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            int numOpens = 100;
-            Task[] tasks = new Task[numOpens];
-            Stopwatch start = new Stopwatch();
-            start.Start();
-            for (int i = 0; i < numOpens; i++)
-            {
-                tasks[i] = MeasureSingleConnection(i, start, connectionString);
-            }
-            Task.WaitAll(tasks);
-            Console.WriteLine($"{sw.Elapsed} {numOpens} connections opened in paralel");
-        }
-
-        private static async Task MeasureSingleConnection(int index, Stopwatch start, string connectionString)
-        {
-            TimeSpan startDelay = start.Elapsed;
-            Stopwatch sw = new Stopwatch();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                sw.Start();
-                await connection.OpenAsync();
-                Console.WriteLine($"{startDelay} {sw.Elapsed} {index} {connection.ClientConnectionId} {IsReuse(connection)}");
-                await Task.Delay(4000);
-            }
-        }
-
-        private static async Task MeasureSingleConnectionAndReuse(string connectionString)
-        {
-            Stopwatch sw = new Stopwatch();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                sw.Start();
-                await connection.OpenAsync();
-                Console.WriteLine($"{sw.Elapsed} {connection.ClientConnectionId} {IsReuse(connection)} Single open time ");
-            }
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                sw.Restart();
-                await connection.OpenAsync();
-                Console.WriteLine($"{sw.Elapsed} {connection.ClientConnectionId} {IsReuse(connection)} Single open time with one previously opened connection");
-            }
-        }
-
-        private static bool IsReuse(SqlConnection connection)
-        {
-            return !_connectionIDs.TryAdd(connection.ClientConnectionId, null);
-        }
     }
-
 }
