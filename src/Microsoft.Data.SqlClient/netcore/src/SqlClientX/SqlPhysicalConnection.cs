c004ab2db996dc8af9af7b7a5878dc5ce30bc5ee
saurabh500 <1623701+saurabh500@users.noreply.github.com>
2024-05-08 21:20:38 -0700

    Adding the refactor prelogin

--- src/Microsoft.Data.SqlClient/netcore/src/SqlClientX/SqlPhysicalConnection.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/SqlClientX/SqlPhysicalConnection.cs
@@ -114,6 +114,8 @@ namespace simplesqlclient
             _sslOverTdsStream = new SslOverTdsStream(_tcpStream);
 
             _sslStream = new SslStream(_sslOverTdsStream, true, new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
+
+            _writeStream = new TdsWriteStream(_tcpStream);
         }
 
         private bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
@@ -124,132 +126,8 @@ namespace simplesqlclient
         #region Prelogin
         internal async ValueTask SendPreloginAsync(bool isAsync, CancellationToken ct)
         {
-            // 5 bytes for each option (1 byte length, 2 byte offset, 2 byte payload length)
-            int preloginOptionsCount = 7;
-            int offset = 36; // 7 * 5 + 1 add 1 to start after the first 40 bytes
-            // The payload is the bytes for all the options and the maximum length of the payload
-            byte[] payload = new byte[preloginOptionsCount * 5 + TdsConstants.MAX_PRELOGIN_PAYLOAD_LENGTH];
-            int payLoadIndex = 0;
-            _writeStream = new TdsWriteStream(_tcpStream, TdsConstants.DEFAULT_LOGIN_PACKET_SIZE)
-            {
-                PacketHeaderType = LucidTdsEnums.MT_PRELOGIN
-            };
-
-            for (int option = 0; option < preloginOptionsCount; option++)
-            {
-                int optionDataSize = 0;
-
-                // Fill in the option
-                await _writeStream.WriteByteAsync((byte)option, isAsync, ct).ConfigureAwait(false);
-
-                // Fill in the offset of the option data
-                await _writeStream.WriteByteAsync((byte)((offset & 0xff00) >> 8), isAsync, ct).ConfigureAwait(false); // send upper order byte
-                await _writeStream.WriteByteAsync((byte)(offset & 0x00ff), isAsync, ct).ConfigureAwait(false); // send lower order byte
-
-                switch (option)
-                {
-                    case 0:
-                        Version.TryParse("6.0.0.0", out Version systemDataVersion);
-
-                        // Major and minor
-                        payload[payLoadIndex++] = (byte)(systemDataVersion.Major & 0xff);
-                        payload[payLoadIndex++] = (byte)(systemDataVersion.Minor & 0xff);
-
-                        // Build (Big Endian)
-                        payload[payLoadIndex++] = (byte)((systemDataVersion.Build & 0xff00) >> 8);
-                        payload[payLoadIndex++] = (byte)(systemDataVersion.Build & 0xff);
-
-                        // Sub-build (Little Endian)
-                        payload[payLoadIndex++] = (byte)(systemDataVersion.Revision & 0xff);
-                        payload[payLoadIndex++] = (byte)((systemDataVersion.Revision & 0xff00) >> 8);
-                        offset += 6;
-                        optionDataSize = 6;
-                        break;
-
-                    case 1:
-
-                        // Assume that the encryption is off
-                        payload[payLoadIndex] = (byte)0;
-
-                        payLoadIndex += 1;
-                        offset += 1;
-                        optionDataSize = 1;
-                        break;
-
-                    case 2:
-                        int i = 0;
-                        // Assume we dont need to send the instance name.
-                        byte[] instanceName = new byte[1];
-                        while (instanceName[i] != 0)
-                        {
-                            payload[payLoadIndex] = instanceName[i];
-                            payLoadIndex++;
-                            i++;
-                        }
-
-                        payload[payLoadIndex] = 0; // null terminate
-                        payLoadIndex++;
-                        i++;
-
-                        offset += i;
-                        optionDataSize = i;
-                        break;
-
-                    case (int)PreLoginOptions.THREADID:
-                        int threadID = 1234; // Hard code some thread on the client side.
-
-                        payload[payLoadIndex++] = (byte)((0xff000000 & threadID) >> 24);
-                        payload[payLoadIndex++] = (byte)((0x00ff0000 & threadID) >> 16);
-                        payload[payLoadIndex++] = (byte)((0x0000ff00 & threadID) >> 8);
-                        payload[payLoadIndex++] = (byte)(0x000000ff & threadID);
-                        offset += 4;
-                        optionDataSize = 4;
-                        break;
-
-                    case (int)PreLoginOptions.MARS:
-                        payload[payLoadIndex++] = (byte)(0); // Turn off MARS
-                        offset += 1;
-                        optionDataSize += 1;
-                        break;
-
-                    case (int)PreLoginOptions.TRACEID:
-                        Guid connectionId = Guid.NewGuid();
-                        connectionId.TryWriteBytes(payload.AsSpan(payLoadIndex, TdsConstants.GUID_SIZE)); // 16 is the size of a GUID
-                        payLoadIndex += TdsConstants.GUID_SIZE;
-                        offset += TdsConstants.GUID_SIZE;
-                        optionDataSize = TdsConstants.GUID_SIZE;
-
-                        Guid activityId = Guid.NewGuid();
-                        uint sequence = 123;
-                        activityId.TryWriteBytes(payload.AsSpan(payLoadIndex, 16)); // 16 is the size of a GUID
-                        payLoadIndex += TdsConstants.GUID_SIZE;
-                        payload[payLoadIndex++] = (byte)(0x000000ff & sequence);
-                        payload[payLoadIndex++] = (byte)((0x0000ff00 & sequence) >> 8);
-                        payload[payLoadIndex++] = (byte)((0x00ff0000 & sequence) >> 16);
-                        payload[payLoadIndex++] = (byte)((0xff000000 & sequence) >> 24);
-                        int actIdSize = TdsConstants.GUID_SIZE + sizeof(uint);
-                        offset += actIdSize;
-                        optionDataSize += actIdSize;
-                        break;
-
-                    case (int)PreLoginOptions.FEDAUTHREQUIRED:
-                        payload[payLoadIndex++] = 0x01;
-                        offset += 1;
-                        optionDataSize += 1;
-                        break;
-
-                    default:
-                        Debug.Fail("UNKNOWN option in SendPreLoginHandshake");
-                        break;
-                }
-
-                // Write data length
-                await _writeStream.WriteByteAsync((byte)((optionDataSize & 0xff00) >> 8), isAsync, ct).ConfigureAwait(false);
-                await _writeStream.WriteByteAsync((byte)(optionDataSize & 0x00ff), isAsync, ct).ConfigureAwait(false);
-            }
-            await _writeStream.WriteByteAsync((byte)255, isAsync, ct).ConfigureAwait(false);
-            await _writeStream.WriteArrayAsync(isAsync, payload[..payLoadIndex], ct).ConfigureAwait(false);
-            await _writeStream.FlushAsync(ct, isAsync, hardFlush: true).ConfigureAwait(false);
+            TdsPreLoginHandler preloginHandler = new TdsPreLoginHandler(_writeStream);
+            await preloginHandler.Send(isAsync, ct).ConfigureAwait(false);
         }
 
 
