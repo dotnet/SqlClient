ba09e3a7d9d8f37feb1d510f511ecccafa2990c8
Saurabh Singh <singhsaura@microsoft.com>
2024-06-10 16:15:20 -0700

    Fix the enums

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/IO/TdsStreamPacketType.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/IO/TdsStreamPacketType.cs
@@ -4,12 +4,13 @@
     {
         PreLogin = 0x12,
         Login7 = 0x10,
-        TransactionManagerRequest = 0x0E,
         SQLBatch = 0x01,
         RPC = 0x03,
         Attention = 0x06,
         BulkLoadBCP = 0x07,
-        FederatedAuth = 0x18,
+        FederatedAuth = 0x08,
         FedAuthInfo = 0x19,
+        SSPIMessage = 0x0A,
+        TransactionManagerRequest = 0x0E,
     }
 }
