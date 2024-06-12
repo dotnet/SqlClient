98d88c3999e49f01a28de18a5f3c69fa8a939038
Saurabh Singh <1623701+saurabh500@users.noreply.github.com>
2024-06-12 14:31:39 -0700

    Adding the Tds Stream APIs (#2561)
    * adding the Tds Stream APIs
    
    * Fix the NS
    
    * Fix the enums
    
    * adding the change underlying stream
    
    * Address CR comments
    
    ---------
    
    Co-authored-by: Saurabh Singh <singhsaura@microsoft.com>

---
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/IO/TdsStreamPacketType.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.Data.SqlClientX.IO
+{
+    internal enum TdsStreamPacketType
+    {
+        PreLogin = 0x12,
+        Login7 = 0x10,
+        SqlBatch = 0x01,
+        Rpc = 0x03,
+        Attention = 0x06,
+        BulkLoadBcp = 0x07,
+        FederatedAuth = 0x08,
+        FedAuthInfo = 0x19,
+        SspiMessage = 0x0A,
+        TransactionManagerRequest = 0x0E,
+    }
+}
