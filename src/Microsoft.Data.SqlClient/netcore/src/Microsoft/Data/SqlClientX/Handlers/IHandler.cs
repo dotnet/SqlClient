93855e99ca791ce0e18e87079470ada3f6380cd3
Ben Russell <russellben@microsoft.com>
2024-06-12 11:23:47 -0500

    Doing light cleanup that wasn't caught during ultra-rapid PR mode

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/Handlers/IHandler.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/Handlers/IHandler.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Data.SqlClientX.Handlers;
 
 namespace Microsoft.Data.SqlClientX.Handlers
 {
 
     /// <summary>
-    /// Interface to repreent the handler.
+    /// Interface to represent the handler.
     /// </summary>
     /// <typeparam name="TRequest">The Type of the request object.</typeparam>
-    internal interface IHandler<TRequest> where TRequest : HandlerRequest
+    internal interface IHandler<TRequest>
+        where TRequest : HandlerRequest
     {
         /// <summary>
         /// Allows getting and setting the next handler.
         /// </summary>
-        public IHandler<TRequest> NextHandler { get; set; }
+        IHandler<TRequest> NextHandler { get; set; }
 
         /// <summary>
         /// The call to handler to execute the request.
@@ -27,6 +27,6 @@ namespace Microsoft.Data.SqlClientX.Handlers
         /// <param name="request">The request object of the chain of responsibilities</param>
         /// <param name="ct">Cancellation token in case of async</param>
         /// <returns></returns>
-        public abstract ValueTask Handle(TRequest request, bool isAsync, CancellationToken ct);
+        ValueTask Handle(TRequest request, bool isAsync, CancellationToken ct);
     }
 }
