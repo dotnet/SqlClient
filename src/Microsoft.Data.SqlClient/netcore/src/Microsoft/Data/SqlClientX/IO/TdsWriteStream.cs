7f1323e3356fd7e4153149724761dfd64ad44be8
saurabh500 <1623701+saurabh500@users.noreply.github.com>
2024-06-14 08:08:52 -0700

    Reorder statements

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/IO/TdsWriteStream.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/IO/TdsWriteStream.cs
@@ -17,6 +17,8 @@ namespace Microsoft.Data.SqlClientX.IO
     /// </summary>
     internal class TdsWriteStream : Stream, ITdsWriteStream
     {
+        #region Private Fields
+
         private Stream _underlyingStream;
 
         private byte[] _writeBuffer;
@@ -24,10 +26,12 @@ namespace Microsoft.Data.SqlClientX.IO
         // Start at the end of the Tds header
         private int _writeBufferOffset { get; set; } = TdsEnums.HEADER_LEN;
 
-        /// <inheritdoc />
-        public TdsStreamPacketType? PacketHeaderType { get; set; }
+        // Packets always start with 1.
+        private byte _packetNumber { get; set; } = 1;
 
-        internal byte PacketNumber { get; private set; } = 1; // Packets always start with 1.
+        #endregion
+
+        #region Constructors
 
         /// <summary>
         /// Constructor for the TDS write stream.
@@ -37,14 +41,12 @@ namespace Microsoft.Data.SqlClientX.IO
         {
             _underlyingStream = underLyingStream;
             _writeBuffer = new byte[TdsEnums.DEFAULT_LOGIN_PACKET_SIZE];
+            InitializeHeaderBytesToZero();
         }
 
-        /// <inheritdoc />
-        public void SetPacketSize(int bufferSize) 
-            => _writeBuffer = new byte[bufferSize];
+        #endregion
 
-        /// <inheritdoc />
-        public void ReplaceUnderlyingStream(Stream stream) => _underlyingStream = stream;
+        #region Properties
 
         /// <inheritdoc />
         public override bool CanRead => false;
@@ -58,78 +60,48 @@ namespace Microsoft.Data.SqlClientX.IO
         public override bool CanWrite => _underlyingStream != null;
 
         /// <inheritdoc />
-        public override long Length 
+        public override long Length
             => throw new NotSupportedException();
 
         /// <inheritdoc />
-        public override long Position 
-        { 
-            get => throw new NotSupportedException(); 
-            set => throw new NotSupportedException(); 
+        public TdsStreamPacketType? PacketHeaderType { get; set; }
+
+        /// <inheritdoc />
+        public override long Position
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <inheritdoc />
+        public override async ValueTask DisposeAsync()
+        {
+            await _underlyingStream.DisposeAsync();
+            _underlyingStream = null;
+            _writeBuffer = null;
         }
 
         /// <inheritdoc />
+        public override void Flush()
+            => FlushAsync(CancellationToken.None, isAsync: false, FlushMode.HardFlush).Wait();
+
+        /// <inheritdoc />
         public override async Task FlushAsync(CancellationToken ct)
+            => await FlushAsync(ct, true).ConfigureAwait(false);
+
+        /// <inheritdoc />
+        public void SetPacketSize(int bufferSize)
         {
-            await FlushAsync(ct, true).ConfigureAwait(false);
+            _writeBuffer = new byte[bufferSize];
+            InitializeHeaderBytesToZero();
         }
 
-        /// <summary>
-        /// Use hard flush when we definitely know that the end of packet is reached.
-        /// </summary>
-        /// <param name="ct">Cancellation token for async operation.</param>
-        /// <param name="isAsync">Bool to indicate async operation</param>
-        /// <param name="flushMode">Whether this is a hard flush or a softflush</param>
-        private async Task FlushAsync(CancellationToken ct, bool isAsync, FlushMode flushMode = FlushMode.SoftFlush)
-        {
-            Debug.Assert(PacketHeaderType != null, "PacketHeaderType is not set. Cannot flush the buffer without setting the packet header type.");
-            _writeBuffer[0] = (byte)PacketHeaderType;
-            byte status;
-
-            // TODO: Handle cancellation queueing. If there is a cancellation queued up, then send the status with IGNORE bit set.
-
-            if (flushMode == FlushMode.HardFlush)
-            {
-                status = TdsEnums.ST_EOM;
-            }
-            else
-            {
-                status = TdsEnums.ST_BATCH;
-                // We have filled up the buffer. We need to send it out, hence next packet gets an increment.
-                PacketNumber++;
-            }
-
-            _writeBuffer[1] = status;
-            _writeBuffer[2] = (byte)(_writeBufferOffset >> 8);   // Length upper byte
-            _writeBuffer[3] = (byte)(_writeBufferOffset & 0xff); // Length lower byte
-            _writeBuffer[6] = PacketNumber;
-            _writeBuffer[4] = 0;
-            _writeBuffer[5] = 0;
-            _writeBuffer[7] = 0;
-
-            if (isAsync)
-            {
-                await _underlyingStream.WriteAsync(_writeBuffer.AsMemory(0, _writeBufferOffset), ct).ConfigureAwait(false);
-                await _underlyingStream.FlushAsync(ct).ConfigureAwait(false);
-            }
-            else
-            {
-                _underlyingStream.Write(_writeBuffer, 0, _writeBufferOffset);
-                _underlyingStream.Flush();
-            }
-
-            // A hard flush means that the TDS message is terminated.
-            // Reset the packet header type so that the next message sender on the stream
-            // will have to set the packet header type.
-            if (flushMode == FlushMode.HardFlush)
-            {
-                // Reset the packet header type to null.
-                PacketHeaderType = null;
-            }
-
-            // Reset the offset since we will start filling up the packet again.
-            _writeBufferOffset = TdsEnums.HEADER_LEN;
-        }
+        /// <inheritdoc />
+        public void ReplaceUnderlyingStream(Stream stream) => _underlyingStream = stream;
 
         /// <inheritdoc />
         public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException();
@@ -140,15 +112,36 @@ namespace Microsoft.Data.SqlClientX.IO
         /// <inheritdoc />
         public override void SetLength(long value) => throw new NotSupportedException();
 
-        /// <summary>
-        /// Write operation with the byte buffer is not implemented yet.
-        /// </summary>
-        /// <param name="buffer"></param>
-        /// <param name="offset"></param>
-        /// <param name="count"></param>
-        /// <exception cref="NotSupportedException"></exception>
-        public override void Write(byte[] buffer, int offset, int count) 
+        /// <inheritdoc />
+        public override void Write(byte[] buffer, int offset, int count)
             => Write(buffer.AsSpan(offset, count));
+
+
+        /// <inheritdoc/>
+        public override void Write(ReadOnlySpan<byte> buffer)
+        {
+            int len = buffer.Length;
+            // The buffer may not have enough space. Write what we can and then flush the buffer with a soft flush.
+            while (len > 0)
+            {
+                if (len > _writeBuffer.Length - _writeBufferOffset)
+                {
+                    // Only a part of the length fits in the buffer.
+                    int bytesToWrite = _writeBuffer.Length - _writeBufferOffset;
+                    buffer[..bytesToWrite].CopyTo(_writeBuffer.AsSpan(_writeBufferOffset));
+                    _writeBufferOffset += bytesToWrite;
+                    len -= bytesToWrite;
+                    FlushAsync(CancellationToken.None, isAsync: false, FlushMode.SoftFlush).ConfigureAwait(false);
+                }
+                else
+                {
+                    // The whole length can be added to the buffer.
+                    buffer.CopyTo(_writeBuffer.AsSpan(_writeBufferOffset));
+                    _writeBufferOffset += len;
+                    len = 0;
+                }
+            }
+        }
 
         /// <summary>
         /// An overload for writing a byte to the stream.
@@ -181,32 +174,6 @@ namespace Microsoft.Data.SqlClientX.IO
                 await FlushAsync(ct, isAsync, FlushMode.SoftFlush).ConfigureAwait(false);
             }
             _writeBuffer[_writeBufferOffset++] = value;
-        }
-
-        /// <inheritdoc/>
-        public override void Write(ReadOnlySpan<byte> buffer)
-        {
-            int len = buffer.Length;
-            // The buffer may not have enough space. Write what we can and then flush the buffer with a soft flush.
-            while (len > 0)
-            {
-                if (len > _writeBuffer.Length - _writeBufferOffset)
-                {
-                    // Only a part of the length fits in the buffer.
-                    int bytesToWrite = _writeBuffer.Length - _writeBufferOffset;
-                    buffer[..bytesToWrite].CopyTo(_writeBuffer.AsSpan(_writeBufferOffset));
-                    _writeBufferOffset += bytesToWrite;
-                    len -= bytesToWrite;
-                    FlushAsync(CancellationToken.None, isAsync: false, FlushMode.SoftFlush).ConfigureAwait(false);
-                }
-                else
-                {
-                    // The whole length can be added to the buffer.
-                    buffer.CopyTo(_writeBuffer.AsSpan(_writeBufferOffset));
-                    _writeBufferOffset += len;
-                    len = 0;
-                }
-            }
         }
 
         /// <inheritdoc/>
@@ -243,9 +210,7 @@ namespace Microsoft.Data.SqlClientX.IO
         public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
             => WriteAsync(buffer.AsMemory(offset, count), cancellationToken).AsTask();
 
-        /// <inheritdoc />
-        public override void Flush() => FlushAsync(CancellationToken.None, isAsync: false, FlushMode.HardFlush).Wait();
-
+        
         /// <summary>
         /// Queues the TDS cancellation token for the stream.
         /// </summary>
@@ -255,12 +220,9 @@ namespace Microsoft.Data.SqlClientX.IO
             throw new NotImplementedException();
         }
 
-        public override async ValueTask DisposeAsync()
-        {
-            await _underlyingStream.DisposeAsync();
-            _underlyingStream = null;
-            _writeBuffer = null;
-        }
+        #endregion
+
+        #region Protected Methods
 
         /// <inheritdoc />
         protected override void Dispose(bool disposing)
@@ -270,6 +232,81 @@ namespace Microsoft.Data.SqlClientX.IO
             _writeBuffer = null;
         }
 
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Use hard flush when we definitely know that the end of packet is reached.
+        /// </summary>
+        /// <param name="ct">Cancellation token for async operation.</param>
+        /// <param name="isAsync">Bool to indicate async operation</param>
+        /// <param name="flushMode">Whether this is a hard flush or a softflush</param>
+        private async Task FlushAsync(CancellationToken ct, bool isAsync, FlushMode flushMode = FlushMode.SoftFlush)
+        {
+            Debug.Assert(PacketHeaderType != null, "PacketHeaderType is not set. Cannot flush the buffer without setting the packet header type.");
+            _writeBuffer[0] = (byte)PacketHeaderType;
+            byte status;
+
+            // TODO: Handle cancellation queueing. If there is a cancellation queued up, then send the status with IGNORE bit set.
+
+            if (flushMode == FlushMode.HardFlush)
+            {
+                status = TdsEnums.ST_EOM;
+            }
+            else
+            {
+                status = TdsEnums.ST_BATCH;
+                // We have filled up the buffer. We need to send it out, hence next packet gets an increment.
+                _packetNumber++;
+            }
+
+            // Set the variables in the header, the rest of the fields should already be set
+            // when the buffer was initialized.
+            _writeBuffer[1] = status;
+            _writeBuffer[2] = (byte)(_writeBufferOffset >> 8);   // Length upper byte
+            _writeBuffer[3] = (byte)(_writeBufferOffset & 0xff); // Length lower byte
+            _writeBuffer[6] = _packetNumber;
+
+            if (isAsync)
+            {
+                await _underlyingStream.WriteAsync(_writeBuffer.AsMemory(0, _writeBufferOffset), ct).ConfigureAwait(false);
+                await _underlyingStream.FlushAsync(ct).ConfigureAwait(false);
+            }
+            else
+            {
+                _underlyingStream.Write(_writeBuffer, 0, _writeBufferOffset);
+                _underlyingStream.Flush();
+            }
+
+            // A hard flush means that the TDS message is terminated.
+            // Reset the packet header type so that the next message sender on the stream
+            // will have to set the packet header type.
+            if (flushMode == FlushMode.HardFlush)
+            {
+                // Reset the packet header type to null.
+                PacketHeaderType = null;
+            }
+
+            // Reset the offset since we will start filling up the packet again.
+            _writeBufferOffset = TdsEnums.HEADER_LEN;
+        }
+
+        private void InitializeHeaderBytesToZero()
+        {
+            _writeBuffer[7] = 0;
+            _writeBuffer[1] = 0;
+            _writeBuffer[2] = 0;
+            _writeBuffer[3] = 0;
+            _writeBuffer[6] = 0;
+            _writeBuffer[4] = 0;
+            _writeBuffer[5] = 0;
+        }
+
+        #endregion
+
+        #region Private types
+
         /// <summary>
         /// Flush modes for the stream.
         /// </summary>
@@ -278,5 +315,7 @@ namespace Microsoft.Data.SqlClientX.IO
             HardFlush,
             SoftFlush
         }
+
+        #endregion
     }
 }
