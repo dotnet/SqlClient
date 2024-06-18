df4f485c1e43b326d4ba50e99f045cf7d7ca3f2e
Saurabh Singh <1623701+saurabh500@users.noreply.github.com>
2024-06-18 16:22:42 -0700

    Adding the test for read stream

---
+++ src/Microsoft.Data.SqlClient/tests/UnitTests/IO/TdsReadStreamTest.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClientX.IO;
+using Xunit;
+
+namespace Microsoft.Data.SqlClient.NetCore.UnitTests.IO
+{
+    public class TdsReadStreamTest
+    {
+        [Fact]
+        public void ReadStream_ReadSinglePacket()
+        {
+            MemoryStream underlyingStream = new();
+            TdsReadStream stream = new TdsReadStream(underlyingStream);
+            
+        }
+    }
+}
