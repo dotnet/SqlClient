35682b24f677ca0c839fca74726b1ce16b3a0ee0
Saurabh Singh <singhsaura@microsoft.com>
2024-06-17 17:26:52 -0700

    Adding a test for internals

--- src/Microsoft.Data.SqlClient/tests/UnitTests/DummyTest.cs
+++ src/Microsoft.Data.SqlClient/tests/UnitTests/DummyTest.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Data.SqlClient;
 using Xunit;
 
 namespace UnitTests
@@ -10,9 +11,10 @@ namespace UnitTests
     public class DummyTest
     {
         [Fact]
-        public void TestSomething()
+        public void TestSomethingInternal()
         {
-            
+            SqlConnection conn = new SqlConnection();
+            Assert.False(conn.AsyncCommandInProgress);
         }
     }
 
