7647e99a87c1e709939f1b37c0c208f79cacb270
samsharma2700 <sarasharma@microsoft.com>
2024-06-27 14:44:49 -0700

    Add HandlerOrchestrator and update ConnectionHandlerContext

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/Handlers/ConnectionHandlerContext.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/Handlers/ConnectionHandlerContext.cs
@@ -10,7 +10,7 @@ namespace Microsoft.Data.SqlClientX.Handlers
 {    /// <summary>
      /// Class that contains all context data needed for various handlers.
      /// </summary>
-    internal class ConnectionHandlerContext : HandlerRequest
+    internal class ConnectionHandlerContext : HandlerRequest, ICloneable
     {
         /// <summary>
         /// Stream used by readers.
@@ -31,5 +31,40 @@ namespace Microsoft.Data.SqlClientX.Handlers
         /// Class used by orchestrator while chaining handlers.
         /// </summary>
         public Exception Error { get; set; }
+
+        /// <summary>
+        /// Method needed to clone ConnectionHandlerContext as part of history along the chain.
+        /// </summary>
+        public object Clone() 
+        {
+            ConnectionHandlerContext clonedContext = new ConnectionHandlerContext();
+
+            if (this.ConnectionStream != null)
+            {
+                MemoryStream clonedStream = new MemoryStream();
+                this.ConnectionStream.CopyTo(clonedStream);
+                clonedStream.Position = 0;
+                clonedContext.ConnectionStream = clonedStream;
+            }
+
+            if (this.ConnectionString != null)
+            {
+                clonedContext.ConnectionString = new SqlConnectionString(this.ConnectionString.ToString());
+            }
+
+            if (this.DataSource != null)
+            {
+                //TODO: Move DataSource class into SqlClientX and implement clone
+                clonedContext.DataSource = (DataSource)this.DataSource;
+            }
+
+            if (this.Error != null)
+            {
+                clonedContext.Error = new Exception(this.Error.Message, this.Error.InnerException);
+            }
+
+            return clonedContext;
+        }
     }
 }
+
---
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/Handlers/HandlerOrchestrator.cs
@@ -0,0 +1,95 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClientX.Handlers.TransportCreation;
+
+namespace Microsoft.Data.SqlClientX.Handlers
+{
+    internal class HandlerOrchestrator
+    {
+        /// <summary>
+        /// Class responsible to create the chain of handlers, initiate chain, retain handler-context, maintain 
+        /// routing count and history of all handler contexts for all handlers in the chain
+        /// </summary>
+        private  IHandler<HandlerRequest> _initialHandler;
+        private ConnectionHandlerContext _initialConnectionHandlerContext;
+        private InternalConnectionContext _internalConnectionContext;
+        private int _routingCount;
+        private List<ConnectionHandlerContext> _routingHistory;
+
+        public HandlerOrchestrator(IHandler<HandlerRequest> initialHandler, ConnectionHandlerContext initialConnectionHandlerContext, InternalConnectionContext internalConnectionContext, List<ConnectionHandlerContext> history)
+        {
+            _initialHandler = initialHandler;
+            _initialConnectionHandlerContext = initialConnectionHandlerContext;
+            _internalConnectionContext = internalConnectionContext;
+            _routingHistory = history;
+            _routingCount = 0;
+        }
+
+        /// <summary>
+        /// Intiates the pre-defined chain of handlers
+        /// </summary>
+        /// <param name="isAsync"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        /// <exception cref="HandlerNotFoundException"></exception>
+        public async Task ProcessRequestAsync(bool isAsync, CancellationToken ct) 
+        {
+            CreateHandlerChain();
+            if (_initialHandler != null)
+            {
+                await _initialHandler.Handle(_initialConnectionHandlerContext, isAsync, ct).ConfigureAwait(false);
+            }
+            else
+            {
+                //TODO: Deal with missing handler exception in the connector and populate relevant SqlException in future.
+                throw new HandlerNotFoundException();
+            }
+        }
+
+        /// <summary>
+        /// Creates the chain of handlers in the order definition
+        /// </summary>
+        private void CreateHandlerChain()
+        {
+            // List of handlers to be added to the chain, ex : DSH -> TCH -> Prelogin -> Login -> null
+            List<IHandler<ConnectionHandlerContext>> handlers = new List<IHandler<ConnectionHandlerContext>>
+            {
+                new DataSourceParsingHandler(),
+                new TransportCreationHandler(),
+                //new PreLoginHandler(),
+            };
+
+            for (int i = 0; i < handlers.Count - 1; i++)
+            {
+                handlers[i].NextHandler = handlers[i + 1];
+            }
+
+            // Set the first handler in the chain
+            if (handlers.Count > 0)
+            {
+                _initialHandler = (IHandler<HandlerRequest>)handlers[0];
+            }
+        }
+
+        /// <summary>
+        /// Adds the context of previous handler into the routingHistory object in case
+        /// of a reroute, exception etc.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        private void AddToHistory(ConnectionHandlerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context), "Context cannot be null");
+            }
+            _routingHistory.Add((ConnectionHandlerContext)context.Clone());
+            _routingCount++;
+        }
+    }
+}
