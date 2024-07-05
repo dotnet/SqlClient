939bcc47718bfa31687211442ec37a148fedd17a
David Engel <davidengel@microsoft.com>
2024-07-05 11:10:58 -0700

    Fix #2563

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/ActiveDirectoryAuthenticationProvider.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/ActiveDirectoryAuthenticationProvider.cs
@@ -120,7 +120,11 @@ namespace Microsoft.Data.SqlClient
             using CancellationTokenSource cts = new();
 
             // Use Connection timeout value to cancel token acquire request after certain period of time.
-            cts.CancelAfter(parameters.ConnectionTimeout * 1000); // Convert to milliseconds
+            int timeout = parameters.ConnectionTimeout * 1000;
+            if (timeout > 0) // if ConnectionTimeout is 0 or the above overflows an int, no need to set CancelAfter
+            {
+                cts.CancelAfter(parameters.ConnectionTimeout * 1000); // Convert to milliseconds
+            }
 
             string scope = parameters.Resource.EndsWith(s_defaultScopeSuffix, StringComparison.Ordinal) ? parameters.Resource : parameters.Resource + s_defaultScopeSuffix;
             string[] scopes = new string[] { scope };
