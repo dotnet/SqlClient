d3bd2b9df4a2a1e8a6e162d85edf0a0ed4bb341f
Taylor Southwick <tasou@microsoft.com>
2024-07-10 09:44:27 -0700

    fix

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SSPI/NegotiateSSPIContextProvider.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SSPI/NegotiateSSPIContextProvider.cs
@@ -13,13 +13,16 @@ namespace Microsoft.Data.SqlClient
     {
         protected override void GenerateSspiClientContext(ReadOnlyMemory<byte> incomingBlob, IBufferWriter<byte> outgoingBlobWriter, byte[][] _sniSpnBuffer)
         {
+            NegotiateAuthenticationStatusCode statusCode = default;
+
             for (int i = 0; i < _sniSpnBuffer.Length; i++)
             {
-                var negotiateAuth = new(new NegotiateAuthenticationClientOptions { Package = "Negotiate", TargetName = Encoding.Unicode.GetString(_sniSpnBuffer[i]) });
-                var result = _negotiateAuth.GetOutgoingBlob(incomingBlob.Span, out var statusCode)!;
+                var negotiateAuth = new NegotiateAuthentication(new NegotiateAuthenticationClientOptions { Package = "Negotiate", TargetName = Encoding.Unicode.GetString(_sniSpnBuffer[i]) });
+                var result = negotiateAuth.GetOutgoingBlob(incomingBlob.Span, out statusCode);
+
                 // Log session id, status code and the actual SPN used in the negotiation
                 SqlClientEventSource.Log.TryTraceEvent("{0}.{1} | Info | Session Id {2}, StatusCode={3}, SPN={4}", nameof(NegotiateSSPIContextProvider),
-                    nameof(GenerateSspiClientContext), _physicalStateObj.SessionId, statusCode, _negotiateAuth.TargetName);
+                    nameof(GenerateSspiClientContext), _physicalStateObj.SessionId, statusCode, negotiateAuth.TargetName);
                 if (statusCode == NegotiateAuthenticationStatusCode.Completed || statusCode == NegotiateAuthenticationStatusCode.ContinueNeeded)
                 {
                     outgoingBlobWriter.Write(result);
