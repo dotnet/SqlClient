723a232964d9894e3eec66971b309b13c84d3345
samsharma2700 <sarasharma@microsoft.com>
2024-07-11 13:26:02 -0700

    Fix build failure

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/Handlers/HandlerOrchestrator.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClientX/Handlers/HandlerOrchestrator.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Data.SqlClientX.Handlers.TransportCreation;
+using Microsoft.Data.SqlClientX.Handlers.Connection;
 
 namespace Microsoft.Data.SqlClientX.Handlers
 {
