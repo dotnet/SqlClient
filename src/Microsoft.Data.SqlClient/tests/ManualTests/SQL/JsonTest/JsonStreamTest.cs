aa956b5b575aff9d2859807ebd8363e93182e3c4
Apoorv Deshmukh <apdeshmukh@microsoft.com>
2024-08-26 21:32:39 +0530

    Add async testcase for streaming support

--- src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/JsonTest/JsonStreamTest.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/JsonTest/JsonStreamTest.cs
@@ -13,7 +13,7 @@ using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Data.SqlClient.ManualTesting.Tests
 {
-    public class Record
+    public class JsonRecord
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -22,24 +22,24 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
     public  class JsonStreamTest
     {
         private readonly ITestOutputHelper _output;
-        private static readonly string jsonFilename = "randomRecords.json";
-        private static readonly string outputFilename = "serverRecords.json";
+        private static readonly string _jsonFile = "randomRecords.json";
+        private static readonly string _outputFile = "serverRecords.json";
 
         public JsonStreamTest(ITestOutputHelper output)
         {
             _output = output;
         }
 
-        private void generateJsonFile(int noOfRecords, string filename)
+        private void GenerateJsonFile(int noOfRecords, string filename)
         {
-            deleteFile(filename);
+            DeleteFile(filename);
             var random = new Random();
-            var records = new List<Record>();
+            var records = new List<JsonRecord>();
             int recordCount = noOfRecords;
 
             for (int i = 0; i < recordCount; i++)
             {
-                records.Add(new Record
+                records.Add(new JsonRecord
                 {
                     Id = i + 1,
                     Name = "𩸽json" + random.Next(1, noOfRecords),
@@ -49,13 +49,13 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
             string json = JsonConvert.SerializeObject(records, Formatting.Indented);
             File.WriteAllText(filename, json);
             Assert.True(File.Exists(filename));
-            _output.WriteLine("Generated Json File "+filename);
+            _output.WriteLine("Generated JSON file "+filename);
         }
 
-        private void compareJsonFiles()
+        private void CompareJsonFiles()
         {
-            using (var stream1 = File.OpenText(jsonFilename))
-            using (var stream2 = File.OpenText(outputFilename))
+            using (var stream1 = File.OpenText(_jsonFile))
+            using (var stream2 = File.OpenText(_outputFile))
             using (var reader1 = new JsonTextReader(stream1))
             using (var reader2 = new JsonTextReader(stream2))
             {
@@ -65,15 +65,15 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
             }
         }
 
-        private void printJsonDataToFile(SqlConnection connection)
+        private void PrintJsonDataToFile(SqlConnection connection)
         {
-            deleteFile(outputFilename);
+            DeleteFile(_outputFile);
             Console.OutputEncoding = Encoding.UTF8;
             using (SqlCommand command = new SqlCommand("SELECT [data] FROM [jsonTab]", connection))
             {
                 using (SqlDataReader reader = command.ExecuteReader(CommandBehavior.SequentialAccess))
                 {
-                    using (StreamWriter sw = new StreamWriter(outputFilename))
+                    using (StreamWriter sw = new StreamWriter(_outputFile))
                     {
                         while (reader.Read())
                         {
@@ -89,18 +89,49 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
 
                                 } while (charsRead > 0);
                             }
-                            _output.WriteLine("Output written to " + outputFilename);
+                            _output.WriteLine("Output written to " + _outputFile);
                         }
                     }
                 }
             }
         }
 
-        private void streamJsonFileToServer(SqlConnection connection)
+        private async Task PrintJsonDataToFileAsync(SqlConnection connection)
+        {
+            DeleteFile(_outputFile);
+            Console.OutputEncoding = Encoding.UTF8;
+            using (SqlCommand command = new SqlCommand("SELECT [data] FROM [jsonTab]", connection))
+            {
+                using (SqlDataReader reader = await command.ExecuteReaderAsync(CommandBehavior.SequentialAccess))
+                {
+                    using (StreamWriter sw = new StreamWriter(_outputFile))
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            char[] buffer = new char[4096];
+                            int charsRead = 0;
+
+                            using (TextReader data = reader.GetTextReader(0))
+                            {
+                                do
+                                {
+                                    charsRead = await data.ReadAsync(buffer, 0, buffer.Length);
+                                    await sw.WriteAsync(buffer, 0, charsRead);
+
+                                } while (charsRead > 0);
+                            }
+                            _output.WriteLine("Output written to file " + _outputFile);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void StreamJsonFileToServer(SqlConnection connection)
         {
             using (SqlCommand cmd = new SqlCommand("INSERT INTO [jsonTab] (data) VALUES (@jsondata)", connection))
             {
-                using (StreamReader jsonFile = File.OpenText(jsonFilename))
+                using (StreamReader jsonFile = File.OpenText(_jsonFile))
                 {
                     cmd.Parameters.Add("@jsondata", Microsoft.Data.SqlDbTypeExtensions.Json, -1).Value = jsonFile;
                     cmd.ExecuteNonQuery();
@@ -108,7 +139,19 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
             }
         }
 
-        private void deleteFile(string filename)
+        private async Task StreamJsonFileToServerAsync(SqlConnection connection)
+        {
+            using (SqlCommand cmd = new SqlCommand("INSERT INTO [jsonTab] (data) VALUES (@jsondata)", connection))
+            {
+                using (StreamReader jsonFile = File.OpenText(_jsonFile))
+                {
+                    cmd.Parameters.Add("@jsondata", Microsoft.Data.SqlDbTypeExtensions.Json, -1).Value = jsonFile;
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        private void DeleteFile(string filename)
         {
             if (File.Exists(filename))
             {
@@ -121,24 +164,34 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
         [ConditionalFact(typeof(DataTestUtility), nameof(DataTestUtility.IsJsonSupported))]
         public void TestJsonStreaming()
         {
-            generateJsonFile(10000, jsonFilename);
+            GenerateJsonFile(10000, _jsonFile);
 
-            string tableCreate = "CREATE TABLE jsonTab(data json)";
             using (SqlConnection connection = new SqlConnection(DataTestUtility.TCPConnectionString))
             {
                 connection.Open();
-                DataTestUtility.DropTable(connection, "jsonTab");
-                using (SqlCommand command = connection.CreateCommand())
-                {
-                    //Create Table
-                    command.CommandText = tableCreate;
-                    command.ExecuteNonQuery();
-                }
-                streamJsonFileToServer(connection);
-                printJsonDataToFile(connection);
-                compareJsonFiles();
-                deleteFile(jsonFilename);
-                deleteFile(outputFilename);
+                DataTestUtility.CreateTable(connection, "jsonTab", "(data json)");
+                StreamJsonFileToServer(connection);
+                PrintJsonDataToFile(connection);
+                CompareJsonFiles();
+                DeleteFile(_jsonFile);
+                DeleteFile(_outputFile);
+            }
+        }
+
+        [ConditionalFact(typeof(DataTestUtility), nameof(DataTestUtility.IsJsonSupported))]
+        public async Task TestJsonStreamingAsync()
+        {
+            GenerateJsonFile(10000, _jsonFile);
+
+            using (SqlConnection connection = new SqlConnection(DataTestUtility.TCPConnectionString))
+            {
+                await connection.OpenAsync();
+                DataTestUtility.CreateTable(connection, "jsonTab", "(data json)");
+                await StreamJsonFileToServerAsync(connection);
+                await PrintJsonDataToFileAsync(connection);
+                CompareJsonFiles();
+                DeleteFile(_jsonFile);
+                DeleteFile(_outputFile);
             }
         }
     }
