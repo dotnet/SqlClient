cf92a05fd5ba458c1001dcd4af1b5bfba93dde66
David Engel <davidengel@microsoft.com>
2024-09-05 11:30:55 -0700

    Sample code improvements around token caching (#2821)

--- doc/samples/CustomDeviceCodeFlowAzureAuthenticationProvider.cs
+++ doc/samples/CustomDeviceCodeFlowAzureAuthenticationProvider.cs
@@ -1,8 +1,10 @@
 //<Snippet1>
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Identity.Client;
 using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
 
 namespace CustomAuthenticationProviderExamples
 {
@@ -12,28 +14,46 @@ namespace CustomAuthenticationProviderExamples
     /// </summary>
     public class CustomDeviceCodeFlowAzureAuthenticationProvider : SqlAuthenticationProvider
     {
+        private const string clientId = "my-client-id";
+        private const string clientName = "My Application Name";
+        private const string s_defaultScopeSuffix = "/.default";
+
+        // Maintain a copy of the PublicClientApplication object to cache the underlying access tokens it provides
+        private static IPublicClientApplication pcApplication;
+
         public override async Task<SqlAuthenticationToken> AcquireTokenAsync(SqlAuthenticationParameters parameters)
         {
-            string clientId = "my-client-id";
-            string clientName = "My Application Name";
-            string s_defaultScopeSuffix = "/.default";
-
             string[] scopes = new string[] { parameters.Resource.EndsWith(s_defaultScopeSuffix) ? parameters.Resource : parameters.Resource + s_defaultScopeSuffix };
 
-            IPublicClientApplication app = PublicClientApplicationBuilder.Create(clientId)
-                .WithAuthority(parameters.Authority)
-                .WithClientName(clientName)
-                .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
+            IPublicClientApplication app = pcApplication;
+            if (app == null)
+            {
+                pcApplication = app = PublicClientApplicationBuilder.Create(clientId)
+                    .WithAuthority(parameters.Authority)
+                    .WithClientName(clientName)
+                    .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
                 .Build();
+            }
 
-            AuthenticationResult result = await app.AcquireTokenWithDeviceCode(scopes,
-                    deviceCodeResult => CustomDeviceFlowCallback(deviceCodeResult)).ExecuteAsync();
+            AuthenticationResult result;
+
+            try
+            {
+                IEnumerable<IAccount> accounts = await app.GetAccountsAsync();
+                result = await app.AcquireTokenSilent(scopes, accounts.FirstOrDefault()).ExecuteAsync();
+            }
+            catch (MsalUiRequiredException)
+            {
+                result = await app.AcquireTokenWithDeviceCode(scopes,
+                        deviceCodeResult => CustomDeviceFlowCallback(deviceCodeResult)).ExecuteAsync();
+            }
+
             return new SqlAuthenticationToken(result.AccessToken, result.ExpiresOn);
         }
 
         public override bool IsSupported(SqlAuthenticationMethod authenticationMethod) => authenticationMethod.Equals(SqlAuthenticationMethod.ActiveDirectoryDeviceCodeFlow);
 
-        private Task CustomDeviceFlowCallback(DeviceCodeResult result)
+        private static Task<int> CustomDeviceFlowCallback(DeviceCodeResult result)
         {
             Console.WriteLine(result.Message);
             return Task.FromResult(0);
