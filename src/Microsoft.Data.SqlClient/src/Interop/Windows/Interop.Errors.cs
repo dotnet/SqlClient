68ce7ead05c9a8c5d5785757dc2e252c545bb6e7
Ben Russell <russellben@microsoft.com>
2024-09-26 12:35:28 -0500

    Moving enough interop files into the common project to enable SqlFileStream migration

---
+++ src/Microsoft.Data.SqlClient/src/Interop/Windows/Interop.Errors.cs
@@ -0,0 +1,15 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+internal partial class Interop
+{
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382.aspx
+    internal partial class Errors
+    {
+        internal const int ERROR_FILE_NOT_FOUND = 0x2;
+        internal const int ERROR_INVALID_HANDLE = 0x6;
+        internal const int ERROR_SHARING_VIOLATION = 0x20;
+        internal const int ERROR_INVALID_PARAMETER = 0x57;
+    }
+}
