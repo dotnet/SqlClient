d5f46bba700d24f5a927ccc2237fac2d072fedbe
Ben Russell <russellben@microsoft.com>
2024-11-14 17:57:13 -0600

    Remove common file so we can just have unix and windows file. I do *not* want to try to decompose stuff in TdsParser to remove LocalDBAPI references on non-windows systems.

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/LocalDBAPI.Unix.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/LocalDBAPI.Unix.cs
@@ -10,5 +10,8 @@ namespace Microsoft.Data
     {
         internal static string GetLocalDBMessage(int hrCode) =>
             throw new PlatformNotSupportedException(Strings.LocalDBNotSupported); // LocalDB is not available for Unix and hence it cannot be supported.
+
+        internal static string GetLocalDbInstanceNameFromServerName(string serverName) =>
+            null;
     }
 }
--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/LocalDBAPI.Windows.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/LocalDBAPI.Windows.cs
@@ -15,6 +15,40 @@ namespace Microsoft.Data
 {
     internal static partial class LocalDBAPI
     {
+        private const string LocalDbPrefix = @"(localdb)\";
+        private const string LocalDbPrefix_NP = @"np:\\.\pipe\LOCALDB#";
+
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        private delegate int LocalDBFormatMessageDelegate(int hrLocalDB, uint dwFlags, uint dwLanguageId, StringBuilder buffer, ref uint buflen);
+
+        // check if name is in format (localdb)\<InstanceName - not empty> and return instance name if it is
+        // localDB can also have a format of np:\\.\pipe\LOCALDB#<some number>\tsql\query
+        internal static string GetLocalDbInstanceNameFromServerName(string serverName)
+        {
+            if (serverName is not null)
+            {
+                // it can start with spaces if specified in quotes
+                // Memory allocation is reduced by using ReadOnlySpan
+                ReadOnlySpan<char> input = serverName.AsSpan().Trim();
+                if (input.StartsWith(LocalDbPrefix.AsSpan(), StringComparison.OrdinalIgnoreCase))
+                {
+                    input = input.Slice(LocalDbPrefix.Length);
+                    if (!input.IsEmpty)
+                    {
+                        return input.ToString();
+                    }
+                }
+                else if (input.StartsWith(LocalDbPrefix_NP.AsSpan(), StringComparison.OrdinalIgnoreCase))
+                {
+                    return input.ToString();
+                }
+
+            }
+            return null;
+        }
+        
+        
         private static LocalDBFormatMessageDelegate s_localDBFormatMessage = null;
 
         internal static void ReleaseDLLHandles()
