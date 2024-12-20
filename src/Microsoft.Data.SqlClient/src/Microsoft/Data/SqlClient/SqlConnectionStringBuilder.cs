98f0a7630afd7097bb0ade26e6fe6c82f313cbdd
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2024-12-20 23:25:25 +0000

    Correct failing debug assertion

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnectionStringBuilder.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnectionStringBuilder.cs
@@ -132,7 +132,7 @@ namespace Microsoft.Data.SqlClient
         private bool _transparentNetworkIPResolution = DbConnectionStringDefaults.TransparentNetworkIPResolution;
         private string _networkLibrary = DbConnectionStringDefaults.NetworkLibrary;
 #else
-        internal const int DeprecatedKeywordsCount = 5;
+        internal const int DeprecatedKeywordsCount = 2;
 #endif
         #endregion //Fields
 
