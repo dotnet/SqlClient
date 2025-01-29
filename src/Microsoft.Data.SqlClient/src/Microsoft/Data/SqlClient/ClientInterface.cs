1b95cf9dcd9cf1f0f60a7d8127a4b2082f1aeef8
Paul Medynski <31868385+paulmedynski@users.noreply.github.com>
2025-01-29 17:36:30 -0400

    Task 33562: Finalize the format and tests
    - Further tweaks to the format.
    - Additional tests for cleaning field values.

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/ClientInterface.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/ClientInterface.cs
@@ -3,6 +3,8 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
 
+#nullable enable
+
 namespace Microsoft.Data.SqlClient
 {
     // ========================================================================
@@ -19,44 +21,76 @@ namespace Microsoft.Data.SqlClient
         //
         // Format:
         //
-        //   Microsoft SqlClient - {OS Name} {OS Version}, {Runtime} - {Arch}
+        //   Microsoft SqlClient|{OS Name}|{Arch}|{OS Info}|{Framework Info}
         //
         // The {OS Name} will be one of the following strings:
         //
         //   Windows
         //   Linux
-        //   macOS
+        //   MacOS
         //   FreeBSD
         //   Unknown
         //
-        // The {OS Version} will be whatever the value of
-        // System.Environment.OSVersion.Version is.
+        // The {Arch} will be the process architecture, either the bare metal
+        // hardware architecture or the virtualized architecture.  See
+        // System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture
+        // for possible values.  This value will never be longer than 15
+        // characters.
         //
-        // The {Runtime} will be the value of
-        // System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.
+        // The {OS Info} will be sourced from the the
+        // System.Runtime.InteropServices.RuntimeInformation.OSDescription
+        // value, or "Unknown" if that value is empty or all whitespace.  This
+        // field participates in truncation if the entire Name is too long, and
+        // may be as short as 42 characters.
         //
-        // The {Arch} will be the value of
-        // System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture.
+        // The {Framework Info} will be sourced from the
+        // System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription
+        // value, or "Unknown" if that value is empty or all whitespace.  This
+        // field participates in truncation if the entire Name is too long, and
+        // may be as short as 42 characters.
         //
         // This adheres to the TDS v37.0 spec, which specifies that the client
         // interface name has a maximum length as noted above.  If the fully
-        // formed length is beyond that limit, it will be truncated.
+        // formed Name length is beyond that limit, it will be truncated to the
+        // maximum by reducing the length of the {OS Info} and {Framework Info}
+        // fields as evenly as possible.
         //
-        // All known exceptions are caught and handled by providing a fallback
-        // value.  However, no effort is made to catch all exceptions, for
-        // example process-fatal memory allocation errors.
+        // Any characters that are not one of the following are replaced with 
+        // underscore ('_'):
+        //
+        //   - ASCII letters ([A-za-z])
+        //   - ASCII digits ([0-9])
+        //   - Space (' ')
+        //   - Period ('.')
+        //   - Plus ('+')
+        //   - Underscore ('_')
+        //   - Hyphen ('-')
+        //
+        // All known exceptions are caught and handled by injecting the fallback
+        // value of "Unknown".  However, no effort is made to catch all
+        // exceptions, for example process-fatal memory allocation errors.
         // 
         public static string Name => _name;
 
         // ====================================================================
-        // Private Helpers
+        // Helpers
 
         // Static construction builds the client interface name.
         //
-        // We make a best effort to avoid known exceptions escaping.
+        // We make a best effort to avoid allowing known exceptions to escape.
         //
         static ClientInterface()
         {
+            // The max length must not be negative, or our assumptions below
+            // won't hold.
+            //
+            // C# doesn't have compile-time type traits, so we can't confirm
+            // that MAXLEN_CLIENTINTERFACE is unsigned.  Instead, we capture
+            // it into a ushort, and let the compiler decide if that is a
+            // permitted operation.
+            //
+            ushort maxLen = TdsEnums.MAXLEN_CLIENTINTERFACE;
+
             try
             {
                 // Expect to build a string whose length is up to our max
@@ -65,12 +99,12 @@ namespace Microsoft.Data.SqlClient
                 // This isn't a max capacity, but a hint for initial buffer
                 // allocation.  We will truncate to our max length after all of
                 // the pieces have been appended.
-                StringBuilder name =
-                  new StringBuilder(TdsEnums.MAXLEN_CLIENTINTERFACE);
+                //
+                StringBuilder name = new StringBuilder(maxLen);
 
                 // Start with the well-known name prefix of this driver.
-                name.Append(Common.DbConnectionStringDefaults.ApplicationName);
-                name.Append(" - ");
+                name.Append(DriverName);
+                name.Append('|');
 
                 // Add the OS name, in order of likelihood.
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
@@ -83,7 +117,7 @@ namespace Microsoft.Data.SqlClient
                 }
                 else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                 {
-                    name.Append("macOS");
+                    name.Append("MacOS");
                 }
 // The FreeBSD platform doesn't exist in .NET Framework at all.
 #if NET
@@ -97,66 +131,193 @@ namespace Microsoft.Data.SqlClient
                     name.Append(Unknown);
                 }
 
-                name.Append(' ');
-
-                try
-                {
-                    // Add the OS version.
-                    //
-                    // Per the System.Version.ToString() documentation, this
-                    // takes the form:
-                    //
-                    //   {Major}.{Minor}[.{Build}[.{Revision}]]
-                    //
-                    // Where [] indicates an optional part.
-                    //
-                    // All parts are decimal integers.
-                    //
-                    // Note that due to the use of signed integers, any of the
-                    // parts may be negative!
-                    //
-                    name.Append(Environment.OSVersion.Version);
-                }
-                catch (InvalidOperationException)
-                {
-                    // Environment.OSVersion failed in an unexpected way, so use
-                    // a fallback value.
-                    name.Append(Unknown);
-                }
-                name.Append(", ");
-
-                // Add the .NET runtime info.
-                //
-                // The documentation for FrameworkDescription doesn't specify
-                // that it will never return a null value, so apply our unknown
-                // value in that unlikely case.
-                //
-                name.Append(
-                    RuntimeInformation.FrameworkDescription ?? Unknown);
-                name.Append(" - ");
+                name.Append('|');
 
                 // Add the architecture.
-                name.Append(RuntimeInformation.ProcessArchitecture);
-
-                // Remember it!
-                _name = name.ToString();
-
-                // Truncate to our max length.
-                if (_name.Length > TdsEnums.MAXLEN_CLIENTINTERFACE)
+                //
+                // We don't expect the architecture to be very long; 15
+                // characters max should be plenty.
+                //
+                var arch =
+                    Clean(RuntimeInformation.ProcessArchitecture.ToString());
+                if (arch.Length > 15)
                 {
-                    _name = _name.Substring(0, TdsEnums.MAXLEN_CLIENTINTERFACE);
+                    arch = arch.Substring(0, 15);
+                }
+                name.Append(arch);
+                name.Append('|');
+
+                // We should have appended no more 44 characters here:
+                //
+                //  - DriverName "Microsoft SqlClient" (19)
+                //  - OS Name "Windows" (max 7)
+                //  - Arch (max 15)
+                //  - 3 pipe characters
+                //
+                Debug.Assert(name.Length <= 44);
+
+                // Obtain cleaned versions of OS and framework descriptions.
+                var osDesc = Clean(RuntimeInformation.OSDescription);
+                var frameworkDesc =
+                    Clean(RuntimeInformation.FrameworkDescription);
+
+                // How many more characters can we append?
+                Debug.Assert(name.Length <= maxLen);
+                ushort remaining =
+                    (ushort)
+                    ((name.Length > maxLen ? 0 : maxLen - name.Length)
+                     // Subtract 1 to accommodate the pipe character between
+                     // the OS and framework descriptions.
+                     - 1);
+
+                // Can we fit both descriptions as-is?
+                if (osDesc.Length + frameworkDesc.Length > remaining)
+                {
+                    // No, so we will have to truncate something.
+                    //
+                    // We want to keep the descriptions as balanced as possible,
+                    // so we'll truncate them to no shorter than half of the
+                    // remaining space each.
+                    //
+                    // If remaining is odd, we'll give the extra character to
+                    // the OS description if we need to truncate both.
+                    //
+                    ushort half = (ushort)(remaining / 2);
+                    
+                    // Do we need to truncate the OS description?
+                    if (osDesc.Length <= half)
+                    {
+                        // No, so truncate the framework description as little
+                        // as possible.
+                        frameworkDesc =
+                            frameworkDesc.Substring(
+                                0,
+                                remaining - osDesc.Length);
+                    }
+                    // Do we need to truncate the framework description?
+                    else if (frameworkDesc.Length <= half)
+                    {
+                        // No, so truncate the OS description as little as
+                        // possible.
+                        osDesc =
+                            osDesc.Substring(
+                                0,
+                                remaining - frameworkDesc.Length);
+                    }
+                    // Otherwise, we need to truncate them both.
+                    else
+                    {
+                        frameworkDesc = frameworkDesc.Substring(0, half);
+
+                        // Give the extra character to the OS description if
+                        // remaining was odd.
+                        if (remaining % 2 != 0)
+                        {
+                            half++;
+                        }
+                        osDesc = osDesc.Substring(0, half);
+                    }
+
+                    Debug.Assert(
+                        osDesc.Length + frameworkDesc.Length <= remaining);
                 }
 
-                Debug.Assert(_name.Length <= TdsEnums.MAXLEN_CLIENTINTERFACE);
+                // Append them.
+                name.Append(osDesc);
+                name.Append('|');
+                name.Append(frameworkDesc);
+
+                // Remember the final name.
+                _name = name.ToString();
+
+                Debug.Assert(_name.Length <= maxLen);
             }
             catch (ArgumentOutOfRangeException)
             {
-                // StringBuilder failed in an unexpected way, so use a fallback
-                // value.
-                _name =
-                  Common.DbConnectionStringDefaults.ApplicationName +
-                  " - " +
-                  Unknown;
+                // StringBuilder failed in an unexpected way, so use our
+                // fallback value.
+                _name = $"{DriverName}|{Unknown}|{Unknown}|{Unknown}|{Unknown}";
+            }
+
+            // Truncate to our max length.
+            //
+            // This is a paranoia step, as we've already been careful to ensure
+            // that we don't exceed the max length.
+            //
+            if (_name.Length > maxLen)
+            {
+                // We know this won't throw ArgumentOutOfRangeException because
+                // we've already confirmed that Length is greater than the
+                // maximum.
+                _name = _name.Substring(0, maxLen);
+            }
+        }
+
+        // Clean the given value of any disallowed characters, replacing them
+        // with underscore ('_'), and return the cleaned value.
+        //
+        // Each disallowed character is replaced with an underscore, preserving
+        // the original length of the value.  No effort is made to collapse
+        // adjacent disallowed characters.
+        //
+        // Permitted characters are:
+        //
+        //   - ASCII letters ([A-za-z])
+        //   - ASCII digits ([0-9])
+        //   - Space (' ')
+        //   - Period ('.')
+        //   - Plus ('+')
+        //   - Underscore ('_')
+        //   - Hyphen ('-')
+        //
+        // If the given value is null, empty, or all whitespace, or an error
+        // occurs, the fallback value is returned.
+        //
+        public static string Clean(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return Unknown;
+            }
+
+            try
+            {
+                // Build the cleaned value by hand, avoiding the overhead and
+                // failure scenarios of regexes or other more complex solutions.
+                //
+                // We expect the value to be short, and this code a few times
+                // per process.  Robustness and simplicity are more important
+                // than performance here.
+                //
+                StringBuilder cleaned = new StringBuilder(value.Length);
+                foreach (char c in value)
+                {
+                    // Is it a permitted character?
+                    if (char.IsAsciiLetter(c)
+                        || char.IsAsciiDigit(c)
+                        || c == ' '
+                        || c == '.'
+                        || c == '+'
+                        || c == '_'
+                        || c == '-')
+                    {
+                        // Yes, so append it as-is.
+                        cleaned.Append(c);
+                    }
+                    else
+                    {
+                        // No, so replace it with an underscore.
+                        cleaned.Append('_');
+                    }
+                }
+
+                return cleaned.ToString();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // StringBuilder failed in an unexpected way, so use our
+                // fallback value.
+                return Unknown;
             }
         }
 
@@ -166,8 +327,11 @@ namespace Microsoft.Data.SqlClient
         // The client interface name.
         private static readonly string _name;
 
-        // A placeholder string for parts of the client interface name that are
-        // unknown.
+        // Our well-known .NET driver name.
+        private const string DriverName = "Microsoft SqlClient";
+
+        // A fallback value for parts of the client interface name that are
+        // unknown, invalid, or when errors occur.
         private const string Unknown = "Unknown";
     }
 }
--- src/Microsoft.Data.SqlClient/tests/FunctionalTests/ClientInterfaceTests.cs
+++ src/Microsoft.Data.SqlClient/tests/FunctionalTests/ClientInterfaceTests.cs
@@ -5,10 +5,17 @@ using System.Text.RegularExpressions;
 using Xunit;
 using Xunit.Abstractions;
 
+#nullable enable
+
 namespace Microsoft.Data.SqlClient.Tests
 {
     public sealed class ClientInterfaceTests
     {
+        // ====================================================================
+        // Test Setup
+
+        // Setup to test by acquiring handles to the internal ClientInterface
+        // APIs we need.
         public ClientInterfaceTests(ITestOutputHelper output)
         {
             // Use --logger option to see the output for successful test runs:
@@ -36,14 +43,28 @@ namespace Microsoft.Data.SqlClient.Tests
             _output.WriteLine($"ClientInterface type: {clientInterfaceType}");
 
             // Find the Name property.
-            _nameProperty =
+            var nameProperty =
                 clientInterfaceType.GetProperty(
                     "Name",
                     BindingFlags.Public | BindingFlags.Static);
-            Assert.NotNull(_nameProperty);
+            Assert.NotNull(nameProperty);
+            _nameProperty = nameProperty;
             _output.WriteLine($"Name property: {_nameProperty}");
+
+            // Find the Clean() function.
+            var cleanFunction =
+                clientInterfaceType.GetMethod(
+                    "Clean",
+                    BindingFlags.Public | BindingFlags.Static);
+            Assert.NotNull(cleanFunction);
+            _cleanFunction = cleanFunction;
+            _output.WriteLine($"Clean function: {_cleanFunction}");
         }
 
+        // ====================================================================
+        // Tests
+
+        // Test the Name property.
         [Fact]
         public void Name()
         {
@@ -60,26 +81,14 @@ namespace Microsoft.Data.SqlClient.Tests
             //
             // The format should be:
             //
-            // Microsoft SqlClient - {OS Name} {OS Version}, {Runtime} - {Arch}
+            // Microsoft SqlClient|{OS Name}|{Arch}|{OS Info}|{Framework Info}
             //
-            Regex regex = new Regex(
-                @"^Microsoft SqlClient - " +
-                @"([A-Za-z]+) " +
-                @"([0-9.]+), " +
-                @"(.+) - " +
-                @"(.+)$");
-
-            // GOTCHA: This match will likely fail if the name was truncated.
-            // This is on purpose, to help us detect strange underlying values
-            // as the OSes and runtimes we target evolve.
-            Match match = regex.Match(name);
-            Assert.True(match.Success);
-            // We get 5 capture groups - the full match itself, and the 4
-            // explicit captures.
-            Assert.Equal(5, match.Groups.Count);
+            var parts = name.Split('|');
+            Assert.Equal(5, parts.Length);
+            Assert.Equal("Microsoft SqlClient", parts[0]);
             
             // Check the OS name against the guaranteed values.
-            var osName = match.Groups[1].Value;
+            var osName = parts[1];
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 Assert.Equal("Windows", osName);
@@ -90,7 +99,7 @@ namespace Microsoft.Data.SqlClient.Tests
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                Assert.Equal("macOS", osName);
+                Assert.Equal("MacOS", osName);
             }
 #if NET
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
@@ -103,11 +112,77 @@ namespace Microsoft.Data.SqlClient.Tests
                 Assert.Equal("Unknown", osName);
             }
 
-            // The remaining parts have no guaranteed format/content, so we
-            // can't check them further.
+            // The remaining parts have no guaranteed format/content, but they
+            // must all be "Unknown" or non-empty.
+            Assert.True(parts[2] == "Unknown" || parts[2].Length > 0);
+            Assert.True(parts[3] == "Unknown" || parts[3].Length > 0);
+            Assert.True(parts[4] == "Unknown" || parts[4].Length > 0);
         }
 
+        // Test the Clean() function.
+        [Fact]
+        public void Clean()
+        {
+            // Null becomes "Unknown".
+            Assert.Equal("Unknown", DoClean(null));
+
+            // Empty string becomes "Unknown".
+            Assert.Equal("Unknown", DoClean(string.Empty));
+
+            // Whitespace string becomes "Unknown".
+            Assert.Equal("Unknown", DoClean(" "));
+            Assert.Equal("Unknown", DoClean("\t"));
+            Assert.Equal("Unknown", DoClean("\r"));
+            Assert.Equal("Unknown", DoClean("\n"));
+            Assert.Equal("Unknown", DoClean(" \t\r\n"));
+
+            // All permitted characters are preserved.
+            const string AllPermitted =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "abcdefghijklmnopqrstuvwxyz" +
+                "0123456789" +
+                " .+_-";
+            Assert.Equal(AllPermitted, DoClean(AllPermitted));
+
+            // Each disallowed character is replaced with underscore.
+            Assert.Equal(
+                "A_B_C_D_E_F_G_H_I_J_K_L_M_N_O_P",
+                DoClean("A|B,C;D:E'F\"G[H{I]J}K\\L/M<N>O?P"));
+            Assert.Equal(
+                "Q_R_S_T_U_V_W_X_Y_Z_a_b_c_d_e_f_g_h_i_j_k_l_m_n_o",
+                DoClean("Q^R_S`T~U(V)W*X+Y-Z_a.b,c/d:e<f>g'h\"i[j]k{l}m|n\\o"));
+
+            // All disallowed characters are replaced with underscore.
+            for (char c = (char)0u; c <= 0xffff; c++)
+            {
+                Assert.Equal(
+                    AllPermitted.Contains(c) ? c.ToString() : "_",
+                    DoClean(c.ToString()));
+            }
+        }
+
+        // ====================================================================
+        // Private Helpers
+        
+        // Convenience helper to call the Clean() function.
+        private string DoClean(string? value)
+        {
+            var cleaned =
+                _cleanFunction.Invoke(null, new object?[] { value }) as string;
+            Assert.NotNull(cleaned);
+            return cleaned;
+        }
+
+        // ====================================================================
+        // Private Fields
+        
+        // The xUnit output helper.
         private readonly ITestOutputHelper _output;
+        
+        // The ClientInterface.Name property.
         private readonly PropertyInfo _nameProperty;
+        
+        // The ClientInterface.Clean() function.
+        private readonly MethodInfo _cleanFunction;
     }
 }
