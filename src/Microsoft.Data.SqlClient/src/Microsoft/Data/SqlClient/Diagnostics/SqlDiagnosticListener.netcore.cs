24cda5dc15b8c41aba0348744ffc5d6c244f381f
Ben Russell <russellben@microsoft.com>
2025-03-11 16:51:44 -0500

    Move diagnostic files into common project in diagnostics folder

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/Diagnostics/SqlDiagnosticListener.netcore.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/Diagnostics/SqlDiagnosticListener.netcore.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if NET
+
 using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.Loader;
@@ -21,3 +23,5 @@ namespace Microsoft.Data.SqlClient
         }
     }
 }
+
+#endif
