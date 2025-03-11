d7f97a87ed2ec32686036ea0fefd3ada113e03b0
Ben Russell <russellben@microsoft.com>
2025-03-11 17:14:03 -0500

    Add stubs for sqlcommand and sqlconnection (for sqldiagnostic work)

---
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCommand.stub.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// @TODO: This is only a stub class for removing clearing errors while merging other files.
+
+namespace Microsoft.Data.SqlClient
+{
+    public class SqlCommand
+    {
+        public SqlConnection Connection { get; set; }
+
+        internal SqlStatistics Statistics { get; set; }
+    }
+}
