adce139eac5539db8caf40ffc49679da38802f47
Benjamin Russell <russellben@microsoft.com>
2025-03-19 12:46:58 -0500

    Merge | Diagnostic Files (#3213)
    * Move diagnostic files into common project in diagnostics folder
    
    * Add stubs for sqlcommand and sqlconnection (for sqldiagnostic work)
    
    * Splitting SqlClientDiagnostic.netcore.cs into one file per class
    
    * Put extension methods back in the listener class itself.
    
    * Decompose the extensions file into separate files for diagnostic scopes
    
    * Move remaining files to diagnostics namespace

---
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/Diagnostics/DiagnosticTransactionScope.netcore.cs
@@ -0,0 +1,88 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#if NET
+
+using System;
+using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.Data.SqlClient.Diagnostics
+{
+    internal ref struct DiagnosticTransactionScope //: IDisposable //ref structs cannot implement interfaces but the compiler will use pattern matching
+    {
+        public const int TransactionCommit = 1;
+        public const int TransactionRollback = 2;
+
+        private readonly SqlDiagnosticListener _diagnostics;
+        private readonly int _operation;
+        private readonly Guid _operationId;
+        private readonly string _operationName;
+        private readonly IsolationLevel _isolationLevel;
+        private readonly SqlConnection _connection;
+        private readonly SqlInternalTransaction _transaction;
+        private readonly string _transactionName;
+        private Exception _exception;
+
+        public DiagnosticTransactionScope(SqlDiagnosticListener diagnostics, int operation, Guid operationId, string operationName, IsolationLevel isolationLevel, SqlConnection connection, SqlInternalTransaction transaction, string transactionName)
+        {
+            _diagnostics = diagnostics;
+            _operation = operation;
+            _operationId = operationId;
+            _operationName = operationName;
+            _isolationLevel = isolationLevel;
+            _connection = connection;
+            _transaction = transaction;
+            _transactionName = transactionName;
+            _exception = null;
+        }
+
+        public void Dispose()
+        {
+            switch (_operation)
+            {
+                case TransactionCommit:
+                    if (_exception != null)
+                    {
+                        _diagnostics.WriteTransactionCommitError(_operationId, _isolationLevel, _connection, _transaction, _exception, _operationName);
+                    }
+                    else
+                    {
+                        _diagnostics.WriteTransactionCommitAfter(_operationId, _isolationLevel, _connection, _transaction, _operationName);
+                    }
+                    break;
+
+                case TransactionRollback:
+                    if (_exception != null)
+                    {
+                        _diagnostics.WriteTransactionRollbackError(_operationId, _isolationLevel, _connection, _transaction, _exception, _transactionName, _operationName);
+                    }
+                    else
+                    {
+                        _diagnostics.WriteTransactionRollbackAfter(_operationId, _isolationLevel, _connection, _transaction, _transactionName, _operationName);
+                    }
+                    break;
+            }
+        }
+
+        public void SetException(Exception ex)
+        {
+            _exception = ex;
+        }
+
+        public static DiagnosticTransactionScope CreateTransactionCommitScope(SqlDiagnosticListener diagnostics, IsolationLevel isolationLevel, SqlConnection connection, SqlInternalTransaction transaction, [CallerMemberName] string operationName = "")
+        {
+            Guid operationId = diagnostics.WriteTransactionCommitBefore(isolationLevel, connection, transaction, operationName);
+            return new DiagnosticTransactionScope(diagnostics, TransactionCommit, operationId, operationName, isolationLevel, connection, transaction, null);
+        }
+
+        public static DiagnosticTransactionScope CreateTransactionRollbackScope(SqlDiagnosticListener diagnostics, IsolationLevel isolationLevel, SqlConnection connection, SqlInternalTransaction transaction, string transactionName, [CallerMemberName] string operationName = "")
+        {
+            Guid operationId = diagnostics.WriteTransactionRollbackBefore(isolationLevel, connection, transaction, transactionName, operationName);
+            return new DiagnosticTransactionScope(diagnostics, TransactionCommit, operationId, operationName, isolationLevel, connection, transaction, transactionName);
+        }
+    }
+}
+
+#endif
---
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnection.stub.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// @TODO: This is only a stub class for removing clearing errors while merging other files.
+
+using System;
+
+namespace Microsoft.Data.SqlClient
+{
+    public class SqlConnection
+    {
+        internal Guid ClientConnectionId { get; set; }
+
+        internal SqlStatistics Statistics { get; set; }
+    }
+}
