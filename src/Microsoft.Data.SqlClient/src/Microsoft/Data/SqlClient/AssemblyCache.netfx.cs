947f9a76c0b184f37bc154590f6d30a6d9d5fefe
Ben Russell <russellben@microsoft.com>
2025-03-20 17:46:49 -0500

    Move assemblycache to common project

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/AssemblyCache.netfx.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/AssemblyCache.netfx.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if NETFRAMEWORK
+
 //------------------------------------------------------------------------------
 //ThisAssembly class keeps a map of the following:
 //AssemblyName to AssemblyID
@@ -62,3 +64,5 @@ namespace Microsoft.Data.SqlClient
         }
     }
 }
+
+#endif
