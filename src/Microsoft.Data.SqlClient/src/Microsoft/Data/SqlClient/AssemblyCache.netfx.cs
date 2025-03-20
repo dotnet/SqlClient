2612d9529cd0f3578f5da6e70833c80e4f3d1b11
Ben Russell <russellben@microsoft.com>
2025-03-20 17:50:39 -0500

    Light cleanup - remove comments that make no sense anymore, make class static, couple style changes

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/AssemblyCache.netfx.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/AssemblyCache.netfx.cs
@@ -4,41 +4,19 @@
 
 #if NETFRAMEWORK
 
-//------------------------------------------------------------------------------
-//ThisAssembly class keeps a map of the following:
-//AssemblyName to AssemblyID
-//TypeName to TypeID
-//AssemblyID to AssemblyRef and State
-//TypeID to TypeRef and AssemblyId
-//
-//Adding an assembly to this class will NOT enable users to create types from that assembly. Users should explicitly add type details and link types to assemblies.
-// This class also registers for assembly resolve events so that dependent assemblies can be resolved if they are registered.
-//This class does NOT know anything about assembly dependencies. It simply loads assemblies as handed over to it.
-//Users can take advantage of connection pooling by tying this instance to a pooling-aware component.
-//------------------------------------------------------------------------------
-
 using System;
 using System.Diagnostics;
 using Microsoft.Data.SqlClient.Server;
 
 namespace Microsoft.Data.SqlClient
 {
-
-    internal sealed class AssemblyCache
+    internal static class AssemblyCache
     {
-        private AssemblyCache()
-        { /* prevent utility class from being instantiated*/
-        }
-
-        internal static int GetLength(Object inst)
+        internal static int GetLength(object inst)
         {
-            //caller should have allocated enough, based on MaxByteSize
+            // Caller should have allocated enough, based on MaxByteSize
             return SerializationHelperSql9.SizeInBytes(inst);
         }
-
-        //The attribute we are looking for is now moved to an external dll that server provides. If the name is changed.
-        //then we we have to make corresponding changes here.
-        //please also change sqludcdatetime.cs, sqltime.cs and sqldate.cs
 
         internal static SqlUdtInfo GetInfoFromType(Type t)
         {
@@ -53,10 +31,8 @@ namespace Microsoft.Data.SqlClient
                 {
                     return attr;
                 }
-                else
-                {
-                    t = t.BaseType;
-                }
+
+                t = t.BaseType;
             }
             while (t != null);
 
