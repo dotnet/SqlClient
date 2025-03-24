04b0a8db855acde490af350467c7c41ef2d46c04
Paul Medynski <31868385+paulmedynski@users.noreply.github.com>
2025-03-24 08:35:12 -0300

    Tidy up performance tests (#3230)
    * User Story 34145: Tidy up Bulk Copy unmatched column name work
    
    - Tidied up the existing Performance benchmarks.
    - Added environment variables to specify config files.
    - Removed Newtonsoft.Json as a dependency.
    - Simplified csproj a bit.
    - Improved benchmark running instructions.

--- src/Microsoft.Data.SqlClient/tests/PerformanceTests/BenchmarkRunners/BaseRunner.cs
+++ src/Microsoft.Data.SqlClient/tests/PerformanceTests/BenchmarkRunners/BaseRunner.cs
@@ -2,17 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.IO;
-using Newtonsoft.Json;
-
 namespace Microsoft.Data.SqlClient.PerformanceTests
 {
     public abstract class BaseRunner
     {
         public BaseRunner()
         {
-            s_config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("runnerconfig.json"));
-            s_datatypes = JsonConvert.DeserializeObject<DataTypes>(File.ReadAllText("datatypes.json"));
+            s_config = Config.Load();
+            s_datatypes = DataTypes.Load();
         }
 
         internal static Config s_config;
