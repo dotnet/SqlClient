86835d3ec7f032b913006adba287360e20e44a99
Ben Russell <russellben@microsoft.com>
2025-03-24 13:57:32 -0500

    Wrapping files in #if NET and doing some very light formatting changes

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/PacketHandle.netcore.Windows.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/PacketHandle.netcore.Windows.cs
@@ -2,15 +2,19 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if NET
+
 using System;
 
 namespace Microsoft.Data.SqlClient
 {
-    // this structure is used for transporting packet handle references between the TdsParserStateObject
-    //  base class and Managed or Native implementations. 
-    // It prevents the native IntPtr type from being boxed and prevents the need to cast from object which loses compile time type safety
-    // It carries type information so that assertions about the type of handle can be made in the implemented abstract methods 
-    // it is a ref struct so that it can only be used to transport the handles and not store them
+    // This structure is used for transporting packet handle references between the
+    // TdsParserStateObject base class and Managed or Native implementations. 
+    // It prevents the native IntPtr type from being boxed and prevents the need to cast from
+    // object which loses compile time type safety.
+    // It carries type information so that assertions about the type of handle can be made in the
+    // implemented abstract methods. 
+    // It is a ref struct so that it can only be used to transport the handles and not store them
 
     // N.B. If you change this type you must also change the version for the other platform
 
@@ -20,10 +24,9 @@ namespace Microsoft.Data.SqlClient
         public const int NativePacketType = 2;
         public const int ManagedPacketType = 3;
 
-        public readonly IntPtr NativePointer;
-        public readonly SNIPacket NativePacket;
-
         public readonly SNI.SNIPacket ManagedPacket;
+        public readonly SNIPacket NativePacket;
+        public readonly IntPtr NativePointer;
         public readonly int Type;
 
         private PacketHandle(IntPtr nativePointer, SNIPacket nativePacket, SNI.SNIPacket managedPacket, int type)
@@ -34,12 +37,17 @@ namespace Microsoft.Data.SqlClient
             NativePacket = nativePacket;
         }
 
-        public static PacketHandle FromManagedPacket(SNI.SNIPacket managedPacket) => new PacketHandle(default, default, managedPacket, ManagedPacketType);
+        public static PacketHandle FromManagedPacket(SNI.SNIPacket managedPacket) =>
+            new PacketHandle(default, default, managedPacket, ManagedPacketType);
 
-        public static PacketHandle FromNativePointer(IntPtr nativePointer) => new PacketHandle(nativePointer, default, default, NativePointerType);
+        public static PacketHandle FromNativePointer(IntPtr nativePointer) =>
+            new PacketHandle(nativePointer, default, default, NativePointerType);
 
-        public static PacketHandle FromNativePacket(SNIPacket nativePacket) => new PacketHandle(default, nativePacket, default, NativePacketType);
+        public static PacketHandle FromNativePacket(SNIPacket nativePacket) =>
+            new PacketHandle(default, nativePacket, default, NativePacketType);
 
 
     }
 }
+
+#endif
