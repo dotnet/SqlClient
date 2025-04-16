7694aa8811bb69ae5fe6153c2775cc93462b9e3b
Apoorv Deshmukh <apdeshmukh@microsoft.com>
2025-04-16 22:17:11 +0530

    Feature Extension Changes for Vector Datatype Support  (#3209)
    This commit adds feature extension for vector datatype support.
    Additionally, GenericTDSServer has been enhanced to enable tests
    to validate login requests and response for the vector feature extension.
    Tests have been added under SqlConnectionBasicTests through
    TestConnWithVectorFeatExtVersionNegotiation.

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsEnums.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsEnums.cs
@@ -240,6 +240,7 @@ namespace Microsoft.Data.SqlClient
         public const byte FEATUREEXT_UTF8SUPPORT = 0x0A;
         public const byte FEATUREEXT_SQLDNSCACHING = 0x0B;
         public const byte FEATUREEXT_JSONSUPPORT = 0x0D;
+        public const byte FEATUREEXT_VECTORSUPPORT = 0x0E;
 
         [Flags]
         public enum FeatureExtension : uint
@@ -253,7 +254,8 @@ namespace Microsoft.Data.SqlClient
             DataClassification = 1 << (TdsEnums.FEATUREEXT_DATACLASSIFICATION - 1),
             UTF8Support = 1 << (TdsEnums.FEATUREEXT_UTF8SUPPORT - 1),
             SQLDNSCaching = 1 << (TdsEnums.FEATUREEXT_SQLDNSCACHING - 1),
-            JsonSupport = 1 << (TdsEnums.FEATUREEXT_JSONSUPPORT - 1)
+            JsonSupport = 1 << (TdsEnums.FEATUREEXT_JSONSUPPORT - 1),
+            VectorSupport = 1 << (TdsEnums.FEATUREEXT_VECTORSUPPORT -1)
         }
 
         public const uint UTF8_IN_TDSCOLLATION = 0x4000000;
@@ -977,6 +979,9 @@ namespace Microsoft.Data.SqlClient
 
         // JSON Support constants
         internal const byte MAX_SUPPORTED_JSON_VERSION = 0x01;
+
+        // Vector Support constants
+        internal const byte MAX_SUPPORTED_VECTOR_VERSION = 0x01;
 
         // TCE Related constants
         internal const byte MAX_SUPPORTED_TCE_VERSION = 0x03; // max version
