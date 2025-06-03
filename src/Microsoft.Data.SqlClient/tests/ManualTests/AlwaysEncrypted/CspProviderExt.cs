9d0617386c9a7a785f1fe8f078c1bf80f2ab28ac
Ben Russell <russellben@microsoft.com>
2025-06-03 18:01:52 -0500

    Address comment that we don't need a CspParameters object as part of the test arguments * Move test arguments into property (the class was only used in a single location) * Cleanup test code * Tweak default provider discovery code to handle edge cases a bit better

--- src/Microsoft.Data.SqlClient/tests/ManualTests/AlwaysEncrypted/CspProviderExt.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/AlwaysEncrypted/CspProviderExt.cs
@@ -2,13 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using Xunit;
 using System.Security.Cryptography;
 using Microsoft.Data.SqlClient.TestUtilities.Fixtures;
-#if !NETFRAMEWORK
+using Microsoft.Win32;
+
+#if NET
 using System.Runtime.Versioning;
 #endif
 
@@ -26,7 +29,7 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests.AlwaysEncrypted
     {
         // [Fact(Skip="Run this in non-parallel mode")] or [ConditionalFact()]
         [Fact(Skip = "Failing in TCE")]
-        public void TestRoundTripWithCSPAndCertStoreProvider()
+        public void TestRoundTripWithCspAndCertStoreProvider()
         {
             using CspCertificateFixture cspCertificateFixture = new CspCertificateFixture();
 
@@ -48,24 +51,73 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests.AlwaysEncrypted
         }
 
         [ConditionalTheory(typeof(DataTestUtility), nameof(DataTestUtility.AreConnStringSetupForAE))]
-        [ClassData(typeof(AEConnectionStringProviderWithCspParameters))]
-        public void TestEncryptDecryptWithCSP(string connectionString, CspParameters cspParameters)
+        [MemberData(nameof(TestEncryptDecryptWithCsp_Data))]
+        public void TestEncryptDecryptWithCsp(string connectionString, string providerName, int providerType)
         {
             string keyIdentifier = DataTestUtility.GetUniqueNameForSqlServer("CSP");
-            CspParameters namedCspParameters = new CspParameters(cspParameters.ProviderType, cspParameters.ProviderName, keyIdentifier);
+            CspParameters namedCspParameters = new CspParameters(providerType, providerName, keyIdentifier);
             using SQLSetupStrategyCspProvider sqlSetupStrategyCsp = new SQLSetupStrategyCspProvider(namedCspParameters);
 
             using SqlConnection sqlConn = new(connectionString);
             sqlConn.Open();
 
             // Test INPUT parameter on an encrypted parameter
-            using SqlCommand sqlCommand = new(@$"SELECT CustomerId, FirstName, LastName FROM [{sqlSetupStrategyCsp.ApiTestTable.Name}] WHERE FirstName = @firstName",
-                                                        sqlConn, null, SqlCommandColumnEncryptionSetting.Enabled);
+            string commandText = @$"SELECT CustomerId, FirstName, LastName " +
+                                 @$"FROM [{sqlSetupStrategyCsp.ApiTestTable.Name}] " +
+                                 @$"WHERE FirstName = @firstName";
+            using SqlCommand sqlCommand = new(commandText, sqlConn, null, SqlCommandColumnEncryptionSetting.Enabled);
+            
             SqlParameter customerFirstParam = sqlCommand.Parameters.AddWithValue(@"firstName", @"Microsoft");
             customerFirstParam.Direction = System.Data.ParameterDirection.Input;
 
             using SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
             ValidateResultSet(sqlDataReader);
+        }
+
+        public static IEnumerable<object[]> TestEncryptDecryptWithCsp_Data
+        {
+            get
+            {
+                const string providerRegistryKeyPath = @"SOFTWARE\Microsoft\Cryptography\Defaults\Provider";
+                using RegistryKey defaultProviderRegistryKey = Registry.LocalMachine.OpenSubKey(providerRegistryKeyPath);
+                if (defaultProviderRegistryKey is null)
+                {
+                    // No test cases can be generated if the registry key doesn't exist.
+                    yield break;
+                }
+                
+                foreach (string subKeyName in defaultProviderRegistryKey.GetSubKeyNames())
+                {
+                    // Skip inappropriate providers
+                    if (!subKeyName.Contains(@"RSA and AES"))
+                    {
+                        continue;
+                    }
+
+                    // Open the provider
+                    using RegistryKey providerKey = defaultProviderRegistryKey.OpenSubKey(subKeyName);
+                    if (providerKey is null)
+                    {
+                        continue;
+                    }
+                    
+                    // Read provider name
+                    string providerName = Path.GetFileName(providerKey.Name);
+                    
+                    // Read provider type
+                    object providerTypeValue = providerKey.GetValue(@"Type");
+                    if (providerTypeValue is not int providerType)
+                    {
+                        continue;
+                    }
+                    
+                    // Combine with AE connection strings
+                    foreach (string aeConnectionString in DataTestUtility.AEConnStrings)
+                    {
+                        yield return new object[] { aeConnectionString, providerName, providerType };
+                    }
+                }
+            }
         }
 
         /// <summary>
