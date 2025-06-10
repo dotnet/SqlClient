0cd4e79e82b6996c33269de162feb37bdafb6152
Ben Russell <russellben@microsoft.com>
2025-06-10 17:46:00 -0500

    Remove dead code in SmiContextFactory and distill properties down to exceptions they will always throw b/c _smiLink will always be null

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/Server/SmiContextFactory.netfx.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/Server/SmiContextFactory.netfx.cs
@@ -4,33 +4,15 @@
 
 #if NETFRAMEWORK
 
-using System;
-using System.Diagnostics;
-using System.Security.Permissions;
-
 namespace Microsoft.Data.SqlClient.Server
 {
     internal sealed class SmiContextFactory
     {
         public static readonly SmiContextFactory Instance = new SmiContextFactory();
 
-        private readonly SmiLink _smiLink;
-        private readonly ulong _negotiatedSmiVersion;
-        private readonly string _serverVersion;
-        private readonly SmiEventSink_Default _eventSinkForGetCurrentContext;
-
         internal const ulong Sql2005Version = 100;
         internal const ulong Sql2008Version = 210;
         internal const ulong LatestVersion = Sql2008Version;
-
-        private readonly ulong[] _supportedSmiVersions = { Sql2005Version, Sql2008Version };
-
-        // Used as the key for SmiContext.GetContextValue()
-        internal enum ContextKey
-        {
-            Connection = 0,
-            SqlContext = 1
-        }
 
         private SmiContextFactory()
         {
@@ -38,62 +20,16 @@ namespace Microsoft.Data.SqlClient.Server
 
         internal ulong NegotiatedSmiVersion
         {
-            get
-            {
-                if (_smiLink == null)
-                {
-                    throw SQL.ContextUnavailableOutOfProc();    // Must not be a valid version of Sql Server, or not be SqlCLR
-                }
-
-                return _negotiatedSmiVersion;
-            }
+            get => throw SQL.ContextUnavailableOutOfProc();    // Must not be a valid version of Sql Server, or not be SqlCLR
         }
 
         internal string ServerVersion
         {
-            get
-            {
-                if (_smiLink == null)
-                {
-                    throw SQL.ContextUnavailableOutOfProc();    // Must not be a valid version of Sql Server, or not be SqlCLR
-                }
-
-                return _serverVersion;
-            }
+            get => throw SQL.ContextUnavailableOutOfProc();    // Must not be a valid version of Sql Server, or not be SqlCLR
         }
 
-        internal SmiContext GetCurrentContext()
-        {
-            if (_smiLink == null)
-            {
-                throw SQL.ContextUnavailableOutOfProc();    // Must not be a valid version of Sql Server, or not be SqlCLR
-            }
-
-            object result = _smiLink.GetCurrentContext(_eventSinkForGetCurrentContext);
-            _eventSinkForGetCurrentContext.ProcessMessagesAndThrow();
-
-            if (result == null)
-            {
-                throw SQL.ContextUnavailableWhileInProc();
-            }
-
-            Debug.Assert(result is SmiContext, "didn't get SmiContext from GetCurrentContext?");
-            return (SmiContext)result;
-        }
-
-        [ReflectionPermission(SecurityAction.Assert, MemberAccess = true)]
-        private object GetValue(System.Reflection.FieldInfo fieldInfo)
-        {
-            object result = fieldInfo.GetValue(null);
-            return result;
-        }
-
-        [ReflectionPermission(SecurityAction.Assert, MemberAccess = true)]
-        private System.Reflection.FieldInfo GetStaticField(Type aType, string fieldName)
-        {
-            System.Reflection.FieldInfo result = aType.GetField(fieldName, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.GetField);
-            return result;
-        }
+        internal SmiContext GetCurrentContext() =>
+            throw SQL.ContextUnavailableOutOfProc();    // Must not be a valid version of Sql Server, or not be SqlCLR
     }
 }
 
