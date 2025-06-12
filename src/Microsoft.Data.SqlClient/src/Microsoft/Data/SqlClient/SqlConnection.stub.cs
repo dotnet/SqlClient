fb2f629bc4f483b7ba53d96d53e0e565cea471b1
Ben Russell <russellben@microsoft.com>
2025-06-12 18:23:42 -0500

    Adding more stubs :)

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnection.stub.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnection.stub.cs
@@ -5,19 +5,42 @@
 // @TODO: This is only a stub class for removing clearing errors while merging other files.
 
 using System;
+using System.Data.Common;
+using Microsoft.Data.Common.ConnectionString;
+using Microsoft.Data.ProviderBase;
+using Microsoft.Data.SqlClient.ConnectionPool;
 
 namespace Microsoft.Data.SqlClient
 {
-    public class SqlConnection
+    public class SqlConnection : DbConnection
     {
+        internal bool _applyTransientFaultHandling = false;
+        internal SessionData _recoverySessionData = null;
+        
         internal Guid ClientConnectionId { get; set; }
 
         #if NETFRAMEWORK
         internal static System.Security.CodeAccessPermission ExecutePermission { get; set; }
         #endif
 
+        internal DbConnectionInternal InnerConnection { get; set; }
+        
+        internal int ObjectID { get; set; }
+        
+        internal DbConnectionPoolGroup PoolGroup { get; set; } 
+        
         internal SqlStatistics Statistics { get; set; }
+        
+        internal DbConnectionOptions UserConnectionOptions { get; set; }
 
         internal void Abort(Exception e) { }
+        
+        internal void PermissionDemand() { }
+
+        internal void SetInnerConnectionEvent(DbConnectionInternal to) { }
+
+        internal bool SetInnerConnectionFrom(DbConnectionInternal to, DbConnectionInternal from) => false;
+
+        internal void SetInnerConnectionTo(DbConnectionInternal to) { }
     }
 }
