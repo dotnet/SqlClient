efc0c2826a8b5de3bf3084e9deb1e304870482d1
Ben Russell <russellben@microsoft.com>
2025-06-13 16:40:21 -0500

    Move TryGetConnection

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnectionFactory.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlConnectionFactory.cs
@@ -197,6 +197,149 @@ namespace Microsoft.Data.SqlClient
             SqlClientEventSource.Metrics.EnterInactiveConnectionPoolGroup();
             SqlClientEventSource.Metrics.ExitActiveConnectionPoolGroup();
         }
+
+        internal bool TryGetConnection(
+            DbConnection owningConnection,
+            TaskCompletionSource<DbConnectionInternal> retry,
+            DbConnectionOptions userOptions,
+            DbConnectionInternal oldConnection,
+            out DbConnectionInternal connection)
+        {
+            Debug.Assert(owningConnection is not null, "null owningConnection?");
+
+            connection = null;
+
+            //  Work around race condition with clearing the pool between GetConnectionPool obtaining pool 
+            //  and GetConnection on the pool checking the pool state.  Clearing the pool in this window
+            //  will switch the pool into the ShuttingDown state, and GetConnection will return null.
+            //  There is probably a better solution involving locking the pool/group, but that entails a major
+            //  re-design of the connection pooling synchronization, so is postponed for now.
+
+            // Use retriesLeft to prevent CPU spikes with incremental sleep
+            // start with one msec, double the time every retry
+            // max time is: 1 + 2 + 4 + ... + 2^(retries-1) == 2^retries -1 == 1023ms (for 10 retries)
+            int retriesLeft = 10;
+            int timeBetweenRetriesMilliseconds = 1;
+
+            do
+            {
+                DbConnectionPoolGroup poolGroup = GetConnectionPoolGroup(owningConnection);
+                
+                // Doing this on the callers thread is important because it looks up the WindowsIdentity from the thread.
+                IDbConnectionPool connectionPool = GetConnectionPool(owningConnection, poolGroup);
+                if (connectionPool == null)
+                {
+                    // If GetConnectionPool returns null, we can be certain that this connection
+                    // should not be pooled via DbConnectionPool or have a disabled pool entry.
+                    poolGroup = GetConnectionPoolGroup(owningConnection); // previous entry have been disabled
+
+                    if (retry is not null)
+                    {
+                        Task<DbConnectionInternal> newTask;
+                        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                        lock (s_pendingOpenNonPooled)
+                        {
+                            // look for an available task slot (completed or empty)
+                            int idx;
+                            for (idx = 0; idx < s_pendingOpenNonPooled.Length; idx++)
+                            {
+                                Task task = s_pendingOpenNonPooled[idx];
+                                if (task is null)
+                                {
+                                    s_pendingOpenNonPooled[idx] = GetCompletedTask();
+                                    break;
+                                }
+                                
+                                if (task.IsCompleted)
+                                {
+                                    break;
+                                }
+                            }
+
+                            // if didn't find one, pick the next one in round-robin fashion
+                            if (idx == s_pendingOpenNonPooled.Length)
+                            {
+                                idx = (int)(s_pendingOpenNonPooledNext % s_pendingOpenNonPooled.Length);
+                                unchecked
+                                {
+                                    s_pendingOpenNonPooledNext++;
+                                }
+                            }
+
+                            // now that we have an antecedent task, schedule our work when it is completed.
+                            // If it is a new slot or a completed task, this continuation will start right away.
+                            newTask = CreateReplaceConnectionContinuation(s_pendingOpenNonPooled[idx], owningConnection, retry, userOptions, oldConnection, poolGroup, cancellationTokenSource);
+
+                            // Place this new task in the slot so any future work will be queued behind it
+                            s_pendingOpenNonPooled[idx] = newTask;
+                        }
+
+                        // Set up the timeout (if needed)
+                        if (owningConnection.ConnectionTimeout > 0)
+                        {
+                            int connectionTimeoutMilliseconds = owningConnection.ConnectionTimeout * 1000;
+                            cancellationTokenSource.CancelAfter(connectionTimeoutMilliseconds);
+                        }
+
+                        // once the task is done, propagate the final results to the original caller
+                        newTask.ContinueWith(
+                            continuationAction: TryGetConnectionCompletedContinuation,
+                            state: Tuple.Create(cancellationTokenSource, retry),
+                            scheduler: TaskScheduler.Default
+                        );
+
+                        return false;
+                    }
+
+                    connection = CreateNonPooledConnection(owningConnection, poolGroup, userOptions);
+
+                    SqlClientEventSource.Metrics.EnterNonPooledConnection();
+                }
+                else
+                {
+                    if (((SqlConnection)owningConnection).ForceNewConnection)
+                    {
+                        Debug.Assert(oldConnection is not DbConnectionClosed, "Force new connection, but there is no old connection");
+                        
+                        connection = connectionPool.ReplaceConnection(owningConnection, userOptions, oldConnection);
+                    }
+                    else
+                    {
+                        if (!connectionPool.TryGetConnection(owningConnection, retry, userOptions, out connection))
+                        {
+                            return false;
+                        }
+                    }
+
+                    if (connection is null)
+                    {
+                        // connection creation failed on semaphore waiting or if max pool reached
+                        if (connectionPool.IsRunning)
+                        {
+                            SqlClientEventSource.Log.TryTraceEvent("<prov.SqlConnectionFactory.GetConnection|RES|CPOOL> {0}, GetConnection failed because a pool timeout occurred.", ObjectId);
+                            // If GetConnection failed while the pool is running, the pool timeout occurred.
+                            throw ADP.PooledOpenTimeout();
+                        }
+
+                        // We've hit the race condition, where the pool was shut down after we
+                        // got it from the group. Yield time slice to allow shutdown activities
+                        // to complete and a new, running pool to be instantiated before
+                        // retrying.
+                        Thread.Sleep(timeBetweenRetriesMilliseconds);
+                        timeBetweenRetriesMilliseconds *= 2; // double the wait time for next iteration
+                    }
+                }
+            } while (connection == null && retriesLeft-- > 0);
+
+            if (connection == null)
+            {
+                SqlClientEventSource.Log.TryTraceEvent("<prov.SqlConnectionFactory.GetConnection|RES|CPOOL> {0}, GetConnection failed because a pool timeout occurred and all retries were exhausted.", ObjectId);
+                // exhausted all retries or timed out - give up
+                throw ADP.PooledOpenTimeout();
+            }
+
+            return true;
+        }
         
         #endregion
 
