0007a10b92ab66eaf476cdc2f095a382752a02a1
Malcolm Daigle <mdaigle@microsoft.com>
2025-06-16 10:41:36 -0700

    Test failovers

---
+++ failovertest/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Data.SqlClient;
+
+var connectionString = "Server=tcp:malcolm-test.database.windows.net,1433;Initial Catalog=malcolm-test;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=Active Directory Interactive;";
+var conn = new SqlConnection(connectionString);
+conn.Open();
+
+var conn2 = new SqlConnection(connectionString);
+conn2.Open();
+
+Console.WriteLine("hello");
