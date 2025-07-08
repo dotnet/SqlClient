6ca814bba982972b04eb885208e38cec6c7f5a5d
Paul Medynski <31868385+paulmedynski@users.noreply.github.com>
2025-07-08 08:31:05 -0300

    User Story 37654: Create Extensions base package
    - Added empty Extensions package with some sample class and docs to demonstrate packaging.
    - Created CI stage to build, test, pack, and publish the Extensions NuGet package.
    - Updated downstream CI stages/jobs to use the Extensions package.
    - Updated build.proj Clean target to not delete packages/ dir.

---
+++ src/Microsoft.Data.SqlClient.Extensions/Extensions/test/SampleTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.Data.SqlClient.Extensions.Test;
+
+public class SampleTest
+{
+    [Fact]
+    public void Construction()
+    {
+        Assert.Equal("test", new Sample("test").Name);
+    }
+
+    [Fact]
+    public void SetName()
+    {
+        var sample = new Sample("test");
+        Assert.Equal("test", sample.Name);
+        sample.SetName("new name");
+        Assert.Equal("new name", sample.Name);
+    }
+}
