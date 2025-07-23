c54e7dd7991af85c6c4d03d01fdcdd7f64b297b3
Ben Russell <russellben@microsoft.com>
2025-07-23 17:11:54 -0500

    Merge BeginExecuteNonQueryInternalReadState

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCommand.NonQuery.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCommand.NonQuery.cs
@@ -276,6 +276,31 @@ namespace Microsoft.Data.SqlClient
                 SqlStatistics.StopTimer(statistics);
             }
         }
+
+        private void BeginExecuteNonQueryInternalReadStage(TaskCompletionSource<object> completion)
+        {
+            // Read SNI does not have catches for async exceptions, handle here.
+
+            try
+            {
+                // Must finish caching information before ReadSni which can activate the callback
+                // before returning.
+                CachedAsyncState.SetActiveConnectionAndResult(
+                    completion,
+                    nameof(EndExecuteNonQuery),
+                    _activeConnection);
+                _stateObj.ReadSni(completion);
+            }
+            // @TODO: CER Exception Handling was removed here (see GH#3581)
+            catch (Exception)
+            {
+                // Similarly, if an exception occurs put the stateObj back into the pool.
+                // and reset async cache information to allow a second async execute
+                CachedAsyncState?.ResetAsyncState();
+                ReliablePutStateObject();
+                throw;
+            }
+        }
         
         // @TODO: This can be inlined into InternalExecuteNonQueryAsync before restructuring into async pathway
         private int EndExecuteNonQueryAsync(IAsyncResult asyncResult)
