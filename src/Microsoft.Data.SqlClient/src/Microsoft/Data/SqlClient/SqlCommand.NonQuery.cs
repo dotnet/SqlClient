4d077a9a159aaa1b442b3a2e2eca353c5f701c37
Ben Russell <russellben@microsoft.com>
2025-07-23 13:37:59 -0500

    Merge RunExecuteNonQueryTds

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCommand.NonQuery.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCommand.NonQuery.cs
@@ -91,7 +91,7 @@ namespace Microsoft.Data.SqlClient
         #endregion
         
         #region Private Methods
-
+        
         private Task InternalExecuteNonQuery(
             TaskCompletionSource<object> completion, // @TODO: Create internally, don't pass in if possible
             bool sendToPipe, // @TODO: Must always be false!
@@ -223,6 +223,109 @@ namespace Microsoft.Data.SqlClient
             
             usedCache = innerUsedCache;
             return result;
+        }
+        
+        // @TODO: Sort args, drop TDS from name
+        private Task RunExecuteNonQueryTds(string methodName, bool isAsync, int timeout, bool asyncWrite)
+        {
+            Debug.Assert(!asyncWrite || isAsync, "AsyncWrite should be always accompanied by Async");
+
+            bool processFinallyBlock = true;
+            try
+            {
+                Task reconnectTask = _activeConnection.ValidateAndReconnect(beforeDisconnect: null, timeout);
+                if (reconnectTask is not null)
+                {
+                    long reconnectionStart = ADP.TimerCurrent();
+                    if (isAsync)
+                    {
+                        TaskCompletionSource<object> completion = new TaskCompletionSource<object>();
+                        _activeConnection.RegisterWaitingForReconnect(completion.Task);
+                        _reconnectionCompletionSource = completion;
+                        RunExecuteNonQueryTdsSetupReconnnectContinuation(
+                            methodName,
+                            isAsync,
+                            timeout,
+                            asyncWrite,
+                            reconnectTask,
+                            reconnectionStart,
+                            completion);
+                        return completion.Task;
+                    }
+                    else
+                    {
+                        AsyncHelper.WaitForCompletion(
+                            reconnectTask,
+                            timeout,
+                            static () => throw SQL.CR_ReconnectTimeout());
+                        timeout = TdsParserStaticMethods.GetRemainingTimeout(timeout, reconnectionStart);
+                    }
+                }
+
+                if (asyncWrite)
+                {
+                    _activeConnection.AddWeakReference(this, SqlReferenceCollection.CommandTag);
+                }
+
+                GetStateObject();
+
+                // Reset the encryption state in case it has been set by a previous command.
+                ResetEncryptionState();
+
+                // We just send over the raw text with no annotation - no parameters are sent over,
+                // no data reader is returned.
+                SqlClientEventSource.Log.TryTraceEvent(
+                    "SqlCommand.RunExecuteNonQueryTds | Info | " +
+                    $"Object Id {ObjectID}, " +
+                    $"Activity Id {ActivityCorrelator.Current}, " +
+                    $"Client Connection Id {_activeConnection?.ClientConnectionId}, " +
+                    $"Command executed as SQLBATCH, Command Text '{CommandText}'");
+
+                Task executeTask = _stateObj.Parser.TdsExecuteSQLBatch(
+                    CommandText,
+                    timeout,
+                    Notification,
+                    _stateObj,
+                    sync: true);
+                Debug.Assert(executeTask is null, "Shouldn't get a task when doing sync writes");
+
+                NotifyDependency();
+                if (isAsync)
+                {
+                    _activeConnection.GetOpenTdsConnection(methodName).IncrementAsyncCount();
+                }
+                else
+                {
+                    Debug.Assert(_stateObj._syncOverAsync, "Should not attempt pends in a synchronous call");
+                    TdsOperationStatus result = _stateObj.Parser.TryRun(
+                        RunBehavior.UntilDone,
+                        cmdHandler: this,
+                        dataStream: null,
+                        bulkCopyHandler: null,
+                        _stateObj,
+                        out _);
+
+                    if (result is not TdsOperationStatus.Done)
+                    {
+                        throw SQL.SynchronousCallMayNotPend();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                processFinallyBlock = ADP.IsCatchableExceptionType(e);
+                throw;
+            }
+            finally
+            {
+                if (processFinallyBlock && !isAsync)
+                {
+                    // When executing Async, we need to keep the _stateObj alive...
+                    PutStateObject();
+                }
+            }
+
+            return null;
         }
         
         #endregion
