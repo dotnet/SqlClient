5493c00cbc7124e8252490e5f1e097a32559e3e2
Wraith2 <wraith2@gmail.com>
2025-07-31 18:53:46 +0100

    remove spare packet

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsParserStateObject.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsParserStateObject.cs
@@ -3773,21 +3773,6 @@ namespace Microsoft.Data.SqlClient
                     return Math.Max(RunningDataSize - previous, 0);
                 }
 
-                internal void Clear()
-                {
-                    Buffer = null;
-                    Read = 0;
-                    NextPacket = null;
-                    if (PrevPacket != null)
-                    {
-                        PrevPacket.NextPacket = null;
-                        PrevPacket = null;
-                    }
-                    SetDebugStackImpl(null);
-                    SetDebugPacketId(0);
-                    SetDebugDataHash();
-                }
-
                 internal void SetDebugStack(string value) => SetDebugStackImpl(value);
                 internal void SetDebugPacketId(int value) => SetDebugPacketIdImpl(value);
                 internal void SetDebugDataHash() => SetDebugDataHashImpl();
@@ -4084,7 +4069,6 @@ namespace Microsoft.Data.SqlClient
             private PacketData _firstPacket;
             private PacketData _current;
             private PacketData _continuePacket;
-            private PacketData _sparePacket;
 
 #if DEBUG
             private int _packetCounter;
@@ -4165,15 +4149,8 @@ namespace Microsoft.Data.SqlClient
                     }
                 }
 #endif
-                PacketData packetData = _sparePacket;
-                if (packetData is null)
-                {
-                    packetData = new PacketData();
-                }
-                else
-                {
-                    _sparePacket = null;
-                }
+                PacketData packetData =  new PacketData();
+
                 packetData.Buffer = buffer;
                 packetData.Read = read;
 #if DEBUG
@@ -4357,13 +4334,10 @@ namespace Microsoft.Data.SqlClient
 
             private void ClearPackets()
             {
-                PacketData packet = _firstPacket;
                 _firstPacket = null;
                 _lastPacket = null;
                 _continuePacket = null;
                 _current = null;
-                packet.Clear();
-                _sparePacket = packet;
             }
 
             private void ClearState()
