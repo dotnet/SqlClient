9c67a3ec48ab3c32e5835ec60934c3958dc03e26
Ben Russell <russellben@microsoft.com>
2025-07-31 13:57:23 -0500

    Merge RunExecuteReaderTds

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCommand.Reader.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCommand.Reader.cs
@@ -6,6 +6,7 @@ using System;
 using System.Data;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Data.Common;
 
@@ -302,6 +303,364 @@ namespace Microsoft.Data.SqlClient
                     isRetry);
             }
             // @TODO: CER Exception Handling was removed here (see GH#3581)
+        }
+
+        // @TODO: This method *needs* to be broken up into separate ones for each type of execution
+        private SqlDataReader RunExecuteReaderTds(
+            CommandBehavior cmdBehavior,
+            RunBehavior runBehavior,
+            bool returnStream,
+            bool isAsync,
+            int timeout,
+            out Task task,
+            bool asyncWrite,
+            bool isRetry, // @TODO: This isn't used in netfx?
+            SqlDataReader ds = null,
+            bool describeParameterEncryptionRequest = false) // @TODO: This is hidden by an overload
+        {
+            Debug.Assert(!asyncWrite || isAsync, "AsyncWrite should be always accompanied by Async");
+
+            if (ds == null && returnStream)
+            {
+                ds = new SqlDataReader(this, cmdBehavior);
+            }
+
+            Task reconnectTask = _activeConnection.ValidateAndReconnect(beforeDisconnect: null, timeout);
+            if (reconnectTask is not null)
+            {
+                long reconnectionStart = ADP.TimerCurrent();
+                if (isAsync)
+                {
+                    TaskCompletionSource<object> completion = new TaskCompletionSource<object>();
+                    _activeConnection.RegisterWaitingForReconnect(completion.Task);
+                    _reconnectionCompletionSource = completion;
+
+                    // Sets up a recursive call
+                    RunExecuteReaderTdsSetupReconnectContinuation(
+                        cmdBehavior,
+                        runBehavior,
+                        returnStream,
+                        isAsync,
+                        timeout,
+                        asyncWrite,
+                        isRetry,
+                        ds,
+                        reconnectTask,
+                        reconnectionStart,
+                        completion);
+
+                    task = completion.Task;
+                    return ds;
+                }
+                else
+                {
+                    AsyncHelper.WaitForCompletion(
+                        reconnectTask,
+                        timeout,
+                        onTimeout: static () => throw SQL.CR_ReconnectTimeout());
+                    timeout = TdsParserStaticMethods.GetRemainingTimeout(timeout, reconnectionStart);
+                }
+            }
+
+            // Make sure we have good parameter information
+            Debug.Assert(_activeConnection.Parser is not null, "TdsParser class should not be null in Command.Execute!");
+
+            bool inSchema = (cmdBehavior & CommandBehavior.SchemaOnly) != 0;
+
+            // Create a new RPC
+            _SqlRPC rpc = null;
+
+            task = null;
+            string optionSettings = null;
+            bool processFinallyBlock = true;
+            bool decrementAsyncCountOnFailure = false;
+
+            if (isAsync)
+            {
+                _activeConnection.GetOpenTdsConnection().IncrementAsyncCount();
+                decrementAsyncCountOnFailure = true;
+            }
+
+            try
+            {
+                if (asyncWrite)
+                {
+                    _activeConnection.AddWeakReference(this, SqlReferenceCollection.CommandTag);
+                }
+
+                GetStateObject();
+                Task writeTask;
+
+                if (describeParameterEncryptionRequest)
+                {
+                    // @TODO: Execute as encrypted RPC
+                    #if DEBUG
+                    if (_sleepDuringRunExecuteReaderTdsForSpDescribeParameterEncryption)
+                    {
+                        Thread.Sleep(10000);
+                    }
+                    #endif
+
+                    Debug.Assert(_sqlRPCParameterEncryptionReqArray is not null, "RunExecuteReader rpc array not provided for describe parameter encryption request.");
+                    writeTask = _stateObj.Parser.TdsExecuteRPC(
+                        this,
+                        _sqlRPCParameterEncryptionReqArray,
+                        timeout,
+                        inSchema,
+                        Notification,
+                        _stateObj,
+                        CommandType is CommandType.StoredProcedure,
+                        sync: !asyncWrite);
+                }
+                else if (_batchRPCMode)
+                {
+                    // @TODO: Execute as batch RPC
+                    Debug.Assert(inSchema == false, "Batch RPC does not support schema only command behavior");
+                    Debug.Assert(!IsPrepared, "Batch RPC should not be prepared!");
+                    Debug.Assert(!IsDirty, "Batch RPC should not be marked as dirty!");
+                    Debug.Assert(_RPCList != null, "RunExecuteReader rpc array not provided");
+                    writeTask = _stateObj.Parser.TdsExecuteRPC(
+                        this,
+                        _RPCList,
+                        timeout,
+                        inSchema,
+                        Notification,
+                        _stateObj,
+                        CommandType is CommandType.StoredProcedure,
+                        sync: !asyncWrite);
+                }
+                else if (CommandType is CommandType.Text && GetParameterCount(_parameters) == 0)
+                {
+                    // @TODO: Execute as batch (or text without parameters)
+                    // Send over SQL Batch command if we are not a stored proc and have no parameters
+                    Debug.Assert(!IsUserPrepared, "CommandType.Text with no params should not be prepared!");
+
+                    if (returnStream)
+                    {
+                        SqlClientEventSource.Log.TryTraceEvent(
+                            "SqlCommand.RunExecuteReaderTds | Info | " +
+                            $"Object Id {ObjectID}, " +
+                            $"Activity Id {ActivityCorrelator.Current}, " +
+                            $"Client Connection Id {_activeConnection?.ClientConnectionId}, " +
+                            $"Command executed as SQLBATCH, " +
+                            $"Command Text '{CommandText}'");
+                    }
+
+                    string text = GetCommandText(cmdBehavior) + GetResetOptionsString(cmdBehavior);
+
+                    // If the query requires enclave computations, pass the enclave package in the
+                    // SqlBatch TDS stream
+                    if (requiresEnclaveComputations)
+                    {
+                        if (enclavePackage is null)
+                        {
+                            throw SQL.NullEnclavePackageForEnclaveBasedQuery(
+                                _activeConnection?.Parser.EnclaveType,
+                                _activeConnection?.EnclaveAttestationUrl);
+                        }
+
+                        writeTask = _stateObj.Parser.TdsExecuteSQLBatch(
+                            text,
+                            timeout,
+                            Notification,
+                            _stateObj,
+                            sync: !asyncWrite,
+                            enclavePackage: enclavePackage.EnclavePackageBytes);
+                    }
+                    else
+                    {
+                        writeTask = _stateObj.Parser.TdsExecuteSQLBatch(
+                            text,
+                            timeout,
+                            Notification,
+                            _stateObj,
+                            sync: !asyncWrite,
+                            enclavePackage: null);
+                    }
+                }
+                else if (CommandType is CommandType.Text)
+                {
+                    // @TODO: Execute as RPC (or text with parameters)
+                    if (IsDirty)
+                    {
+                        // Can have cached metadata if dirty because of parameters
+                        Debug.Assert(_cachedMetaData == null || !_dirty, "dirty query should not have cached metadata!");
+
+                        // Someone changed the command text or the parameter schema so we must
+                        // unprepare the command
+                        // remember that IsDirty includes test for IsPrepared! @TODO: Very confusing!
+                        if (_execType is EXECTYPE.PREPARED)
+                        {
+                            _hiddenPrepare = true;
+                        }
+
+                        Unprepare();
+                        IsDirty = false;
+                    }
+
+                    if (_execType is EXECTYPE.PREPARED)
+                    {
+                        Debug.Assert(IsPrepared && _prepareHandle != s_cachedInvalidPrepareHandle, "invalid attempt to call sp_execute without a handle!");
+                        rpc = BuildExecute(inSchema);
+                    }
+                    else if (_execType is EXECTYPE.PREPAREPENDING)
+                    {
+                        rpc = BuildPrepExec(cmdBehavior);
+
+                        // Next time through, only do an exec
+                        _execType = EXECTYPE.PREPARED;
+                        _preparedConnectionCloseCount = _activeConnection.CloseCount;
+                        _preparedConnectionReconnectCount = _activeConnection.ReconnectCount;
+
+                        // Mark ourselves as preparing the command
+                        _inPrepare = true;
+                    }
+                    else
+                    {
+                        Debug.Assert(_execType is EXECTYPE.UNPREPARED, "Invalid execType!");
+                        BuildExecuteSql(cmdBehavior, commandText: null, _parameters, ref rpc);
+                    }
+
+                    rpc.options = TdsEnums.RPC_NOMETADATA;
+                    if (returnStream)
+                    {
+                        SqlClientEventSource.Log.TryTraceEvent(
+                            "SqlCommand.RunExecuteReaderTds | Info | " +
+                            $"Object Id {ObjectID}, " +
+                            $"Activity Id {ActivityCorrelator.Current}, " +
+                            $"Client Connection Id {_activeConnection.ClientConnectionId}, " +
+                            $"Command executed as RPC, " +
+                            $"RPC Name '{rpc.rpcName}'");
+                    }
+
+                    Debug.Assert(_rpcArrayOf1[0] == rpc);
+                    writeTask = _stateObj.Parser.TdsExecuteRPC(
+                        this,
+                        _rpcArrayOf1,
+                        timeout,
+                        inSchema,
+                        Notification,
+                        _stateObj,
+                        CommandType is CommandType.StoredProcedure, // @TODO: uhhhhh this shouldn't ever be true?
+                        sync: !asyncWrite);
+                }
+                else
+                {
+                    // @TODO: Execute as sproc
+                    Debug.Assert(CommandType is CommandType.StoredProcedure, "unknown command type!");
+
+                    BuildRPC(inSchema, _parameters, ref rpc);
+
+                    // If we need to augment the command because a user has changed the command
+                    // behavior (e.g. FillSchema) then batch sql them over. This is inefficient (3
+                    // round trips) but the only way we can get metadata only from a stored proc.
+                    optionSettings = GetSetOptionsString(cmdBehavior);
+
+                    if (returnStream)
+                    {
+                        SqlClientEventSource.Log.TryTraceEvent(
+                            "SqlCommand.RunExecuteReaderTds | Info | " +
+                            $"Object Id {ObjectID}, " +
+                            $"Activity Id {ActivityCorrelator.Current}, " +
+                            $"Client Connection Id {_activeConnection?.ClientConnectionId}, " +
+                            $"Command executed as RPC, " +
+                            $"RPC Name '{CommandText}'");
+                    }
+
+                    // Turn set options ON
+                    if (optionSettings is not null)
+                    {
+                        Task executeTask = _stateObj.Parser.TdsExecuteSQLBatch(
+                            optionSettings,
+                            timeout,
+                            Notification,
+                            _stateObj,
+                            sync: true);
+
+                        Debug.Assert(executeTask is null, "Shouldn't get a task when doing sync writes");
+                        Debug.Assert(_stateObj._syncOverAsync, "Should not attempt pends in a synchronous call");
+
+                        TdsOperationStatus result = _stateObj.Parser.TryRun(
+                            RunBehavior.UntilDone,
+                            cmdHandler: this,
+                            dataStream: null,
+                            bulkCopyHandler: null,
+                            _stateObj,
+                            out bool _);
+                        if (result is not TdsOperationStatus.Done)
+                        {
+                            throw SQL.SynchronousCallMayNotPend();
+                        }
+
+                        // And turn OF Fwhen the ds exhausts the stream on Close()
+                        optionSettings = GetResetOptionsString(cmdBehavior);
+                    }
+
+                    // Execute sproc
+                    Debug.Assert(_rpcArrayOf1[0] == rpc);
+                    writeTask = _stateObj.Parser.TdsExecuteRPC(
+                        this,
+                        _rpcArrayOf1,
+                        timeout,
+                        inSchema,
+                        Notification,
+                        _stateObj,
+                        isCommandProc: CommandType is CommandType.StoredProcedure, // @TODO: This should always be true...
+                        sync: !asyncWrite);
+                }
+
+                Debug.Assert(writeTask is null || isAsync, "Returned task in sync mode");
+
+                if (isAsync)
+                {
+                    decrementAsyncCountOnFailure = false;
+                    if (writeTask is not null)
+                    {
+                        task = RunExecuteReaderTdsSetupContinuation(runBehavior, ds, optionSettings, writeTask);
+                    }
+                    else
+                    {
+                        CachedAsyncState.SetAsyncReaderState(ds, runBehavior, optionSettings);
+                    }
+                }
+                else
+                {
+                    // Always execute - even if no reader!
+                    FinishExecuteReader(
+                        ds,
+                        runBehavior,
+                        optionSettings,
+                        isInternal: false,
+                        forDescribeParameterEncryption: false,
+                        shouldCacheForAlwaysEncrypted: !describeParameterEncryptionRequest);
+                }
+            }
+            catch (Exception e)
+            {
+                processFinallyBlock = ADP.IsCatchableExceptionType(e);
+
+                if (decrementAsyncCountOnFailure)
+                {
+                    if (_activeConnection.InnerConnection is SqlInternalConnectionTds innerConnectionTds)
+                    {
+                        // It may be closed
+                        innerConnectionTds.DecrementAsyncCount();
+                    }
+                }
+
+                throw;
+            }
+            finally
+            {
+                if (processFinallyBlock && !isAsync)
+                {
+                    // When executing async, we need to keep the _stateObj alive...
+                    PutStateObject();
+                }
+            }
+
+            Debug.Assert(isAsync || _stateObj == null, "non-null state object in RunExecuteReader");
+            return ds;
         }
 
         private SqlDataReader RunExecuteReaderWithRetry(
