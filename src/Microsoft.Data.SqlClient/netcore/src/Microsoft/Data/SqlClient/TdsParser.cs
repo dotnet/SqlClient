9d8e2145dbe66bd84153e8da6f5f489cc00b4f64
Malcolm Daigle <mdaigle@microsoft.com>
2025-08-05 11:53:20 -0700

    Revert 1af7327454f9cafbf3aaaa8b3d615489aad480ab

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/TdsParser.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/TdsParser.cs
@@ -13085,10 +13085,11 @@ namespace Microsoft.Data.SqlClient
 
             if (canContinue)
             {
-                temp = stateObj.TryTakeSnapshotStorage() as char[];
-                Debug.Assert(temp != null || !isContinuing, "if continuing stored buffer must be present to contain previous data to continue from");
-                Debug.Assert(temp == null || length == int.MaxValue || temp.Length == length, "stored buffer length must be null or must have been created with the correct length");
-                
+                if (isContinuing || isStarting)
+                {
+                    temp = stateObj.TryTakeSnapshotStorage() as char[];
+                    Debug.Assert(temp == null || length == int.MaxValue || temp.Length == length, "stored buffer length must be null or must have been created with the correct length");
+                }
                 if (temp != null)
                 {
                     startOffset = stateObj.GetSnapshotTotalSize();
@@ -13104,7 +13105,7 @@ namespace Microsoft.Data.SqlClient
                 supportRentedBuff: !canContinue, // do not use the arraypool if we are going to keep the buffer in the snapshot
                 rentedBuff: ref buffIsRented,
                 startOffset,
-                canContinue
+                isStarting || isContinuing
             );
 
             if (result == TdsOperationStatus.Done)
@@ -13131,7 +13132,7 @@ namespace Microsoft.Data.SqlClient
             }
             else if (result == TdsOperationStatus.NeedMoreData)
             {
-                if (canContinue)
+                if (isStarting || isContinuing)
                 {
                     stateObj.SetSnapshotStorage(temp);
                 }
