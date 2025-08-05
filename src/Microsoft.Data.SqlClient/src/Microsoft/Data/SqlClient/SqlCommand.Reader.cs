57c7ee3ddd8f0a2081149497813cf994be3a4636
Ben Russell <russellben@microsoft.com>
2025-08-05 17:02:51 -0500

    Merge RunExecuteReader#2

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCommand.Reader.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCommand.Reader.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 using Microsoft.Data.Common;
 
 #if NETFRAMEWORK
-using System.Runtime.CompilerServices;
 using Microsoft.Data.SqlClient.Utilities;
 #endif
 
@@ -122,7 +121,7 @@ namespace Microsoft.Data.SqlClient
                 returnStream,
                 completion: null,
                 timeout: CommandTimeout,
-                task: out Task unused,
+                executeTask: out Task unused,
                 usedCache: out _,
                 method: method);
 
@@ -135,7 +134,175 @@ namespace Microsoft.Data.SqlClient
 
         #region Private Methods
 
+        // @TODO: We're passing way too many arguments around here... can we simplify this some?
+        private SqlDataReader RunExecuteReader(
+            CommandBehavior cmdBehavior,
+            RunBehavior runBehavior,
+            bool returnStream,
+            TaskCompletionSource<object> completion,
+            int timeout,
+            out Task executeTask,
+            out bool usedCache, // @TODO: This can be eliminated if we do not retry via recursion
+            bool asyncWrite = false,
+            bool isRetry = false,
+            [CallerMemberName] string method = "")
+        {
+            bool isAsync = completion is not null;
 
+            usedCache = false;
+            executeTask = null;
+
+            _rowsAffected = -1;
+            _rowsAffectedBySpDescribeParameterEncryption = -1;
+
+            if ((cmdBehavior & CommandBehavior.SingleRow) != 0)
+            {
+                // CommandBehavior.SingleRow implies CommandBehavior.SingleResult
+                cmdBehavior |= CommandBehavior.SingleResult;
+            }
+
+            // This function may throw for an invalid connection
+            if (!isRetry)
+            {
+                ValidateCommand(isAsync, method);
+            }
+
+            // Only call after validate - requires non-null connection!
+            CheckNotificationStateAndAutoEnlist();
+
+            SqlStatistics statistics = Statistics;
+            if (statistics is not null)
+            {
+                if ((!IsDirty && IsPrepared && !_hiddenPrepare) ||
+                    (IsPrepared && _execType == EXECTYPE.PREPAREPENDING))
+                {
+                    statistics.SafeIncrement(ref statistics._preparedExecs);
+                }
+                else
+                {
+                    statistics.SafeIncrement(ref statistics._unpreparedExecs);
+                }
+            }
+
+            // Reset the encryption related state of the command and its parameters.
+            ResetEncryptionState();
+
+            if (IsColumnEncryptionEnabled)
+            {
+                // @TODO: Make this a separate method
+
+                PrepareForTransparentEncryption(
+                    isAsync,
+                    timeout,
+                    completion,
+                    out Task prepareEncryptionTask,
+                    asyncWrite: asyncWrite && isAsync,
+                    out usedCache,
+                    isRetry);
+
+                long firstAttemptStart = ADP.TimerCurrent();
+                try
+                {
+                    return RunExecuteReaderTdsWithTransparentParameterEncryption(
+                        cmdBehavior,
+                        runBehavior,
+                        returnStream,
+                        isAsync,
+                        timeout,
+                        out executeTask,
+                        asyncWrite: asyncWrite && isAsync,
+                        isRetry,
+                        ds: null,
+                        describeParameterEncryptionTask: prepareEncryptionTask);
+                }
+                catch (EnclaveDelegate.RetryableEnclaveQueryExecutionException)
+                {
+                    if (isRetry)
+                    {
+                        // Do not retry after the second attempt.
+                        throw;
+                    }
+
+                    // @TODO: This same pattern is used below. Can we do this without a recursive call?
+                    // Retry if the command failed with an appropriate error.
+                    // First invalidate the entry from the cache, so that we refresh our encryption
+                    // metadata.
+                    SqlQueryMetadataCache.GetInstance().InvalidateCacheEntry(this);
+                    InvalidateEnclaveSession();
+
+                    return RunExecuteReader(
+                        cmdBehavior,
+                        runBehavior,
+                        returnStream,
+                        completion,
+                        TdsParserStaticMethods.GetRemainingTimeout(timeout, firstAttemptStart),
+                        out executeTask,
+                        out usedCache,
+                        isAsync,
+                        isRetry: true,
+                        method);
+                }
+                catch (SqlException ex)
+                {
+                    // We only want to retry once, so don't retry if we are already in retry.
+                    // If we didn't use the cache, we don't want to retry
+                    if (isRetry || (!usedCache && !ShouldUseEnclaveBasedWorkflow))
+                    {
+                        throw;
+                    }
+
+                    // Check if we have an error indicating that we can retry.
+                    bool shouldRetry = false;
+                    foreach (SqlError error in ex.Errors)
+                    {
+                        if ((usedCache && error.Number == TdsEnums.TCE_CONVERSION_ERROR_CLIENT_RETRY) ||
+                            (ShouldUseEnclaveBasedWorkflow && error.Number == TdsEnums.TCE_ENCLAVE_INVALID_SESSION_HANDLE))
+                        {
+                            shouldRetry = true;
+                            break;
+                        }
+                    }
+
+                    if (!shouldRetry)
+                    {
+                        throw;
+                    }
+
+                    // Retry if the command failed with an appropriate error.
+                    // First invalidate the entry from the cache, so that we refresh our encryption
+                    // metadata.
+                    SqlQueryMetadataCache.GetInstance().InvalidateCacheEntry(this);
+                    InvalidateEnclaveSession();
+
+                    return RunExecuteReader(
+                        cmdBehavior,
+                        runBehavior,
+                        returnStream,
+                        completion,
+                        TdsParserStaticMethods.GetRemainingTimeout(timeout, firstAttemptStart),
+                        out executeTask,
+                        out usedCache,
+                        asyncWrite: isAsync,
+                        isRetry: true,
+                        method);
+                }
+            }
+            else
+            {
+                // @TODO: Reminder, this is where we execute if transparent parameter encryption is not needed.
+                usedCache = false;
+                return RunExecuteReaderTds(
+                    cmdBehavior,
+                    runBehavior,
+                    returnStream,
+                    isAsync,
+                    timeout,
+                    out executeTask,
+                    asyncWrite: asyncWrite && isAsync,
+                    isRetry);
+            }
+            // @TODO: CER Exception Handling was removed here (see GH#3581)
+        }
 
         private SqlDataReader RunExecuteReaderWithRetry(
             CommandBehavior cmdBehavior,
