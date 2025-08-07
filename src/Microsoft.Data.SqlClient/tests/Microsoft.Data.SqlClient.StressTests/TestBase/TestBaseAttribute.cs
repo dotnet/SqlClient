941855c57bc95304cb230cd6ddcbd17c80b28a00
Ben Russell <russellben@microsoft.com>
2025-08-07 18:20:11 -0500

    Cleanup TestBaseAttribute

--- src/Microsoft.Data.SqlClient/tests/Microsoft.Data.SqlClient.StressTests/TestBase/TestBaseAttribute.cs
+++ src/Microsoft.Data.SqlClient/tests/Microsoft.Data.SqlClient.StressTests/TestBase/TestBaseAttribute.cs
@@ -7,61 +7,23 @@ using Microsoft.Data.SqlClient.StressTests.Runner;
 
 namespace Microsoft.Data.SqlClient.StressTests.TestBase
 {
-    public class TestAttributeBase : Attribute
+    public abstract class TestAttributeBase : Attribute
     {
-        private string _title;
-        private string _description = "none provided";
-        private string _applicationName = "unknown";
-        private string _improvement = "ADONETV3";
-        private string _owner = "unknown";
-        private string _category = "unknown";
-        private TestPriority _priority = TestPriority.Bvt;
-
-        public TestAttributeBase(string title)
+        protected TestAttributeBase(string title)
         {
-            _title = title;
+            Title = title;
         }
 
-        public string Title
-        {
-            get { return _title; }
-            set { _title = value; }
-        }
+        public string Title { get; set; }
 
-        public string Description
-        {
-            get { return _description; }
-            set { _description = value; }
-        }
+        public string Description { get; set; } = "none provided";
 
-        public string Improvement
-        {
-            get { return _improvement; }
-            set { _improvement = value; }
-        }
+        public string Improvement { get; set; } = "ADONETV3";
 
-        public string Owner
-        {
-            get { return _owner; }
-            set { _owner = value; }
-        }
+        public string Owner { get; set; } = "unknown";
 
-        public string ApplicationName
-        {
-            get { return _applicationName; }
-            set { _applicationName = value; }
-        }
+        public TestPriority Priority { get; set; } = TestPriority.Bvt;
 
-        public TestPriority Priority
-        {
-            get { return _priority; }
-            set { _priority = value; }
-        }
-
-        public string Category
-        {
-            get { return _category; }
-            set { _category = value; }
-        }
+        public string Category { get; set; } = "unknown";
     }
 }
