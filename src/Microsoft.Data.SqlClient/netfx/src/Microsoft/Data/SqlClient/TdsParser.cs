9499343d7bca7d7f5bba97a36c79e4462d58deb1
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2025-08-09 21:51:06 +0100

    netfx: reorder WriteTceFeatureRequest

--- src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/TdsParser.cs
+++ src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/TdsParser.cs
@@ -8601,21 +8601,6 @@ namespace Microsoft.Data.SqlClient
             return totalLen;
         }
 
-        internal int WriteDataClassificationFeatureRequest(bool write /* if false just calculates the length */)
-        {
-            int len = 6; // 1byte = featureID, 4bytes = featureData length, 1 bytes = Version
-
-            if (write)
-            {
-                // Write Feature ID, length of the version# field and Sensitivity Classification Version#
-                _physicalStateObj.WriteByte(TdsEnums.FEATUREEXT_DATACLASSIFICATION);
-                WriteInt(1, _physicalStateObj);
-                _physicalStateObj.WriteByte(TdsEnums.DATA_CLASSIFICATION_VERSION_MAX_SUPPORTED);
-            }
-
-            return len; // size of data written
-        }
-
         internal int WriteTceFeatureRequest(bool write /* if false just calculates the length */)
         {
             int len = 6; // 1byte = featureID, 4bytes = featureData length, 1 bytes = Version
@@ -8626,6 +8611,21 @@ namespace Microsoft.Data.SqlClient
                 _physicalStateObj.WriteByte(TdsEnums.FEATUREEXT_TCE);
                 WriteInt(1, _physicalStateObj);
                 _physicalStateObj.WriteByte(TdsEnums.MAX_SUPPORTED_TCE_VERSION);
+            }
+
+            return len; // size of data written
+        }
+
+        internal int WriteDataClassificationFeatureRequest(bool write /* if false just calculates the length */)
+        {
+            int len = 6; // 1byte = featureID, 4bytes = featureData length, 1 bytes = Version
+
+            if (write)
+            {
+                // Write Feature ID, length of the version# field and Sensitivity Classification Version#
+                _physicalStateObj.WriteByte(TdsEnums.FEATUREEXT_DATACLASSIFICATION);
+                WriteInt(1, _physicalStateObj);
+                _physicalStateObj.WriteByte(TdsEnums.DATA_CLASSIFICATION_VERSION_MAX_SUPPORTED);
             }
 
             return len; // size of data written
