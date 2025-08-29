5f5b7db8881ad5339f34ccb52005c66980cda1ca
Apoorv Deshmukh <apdeshmukh@microsoft.com>
2025-08-29 16:24:13 +0530

    Enable JSON tests and split vector API test into multiple testcases

--- src/Microsoft.Data.SqlClient/tests/ManualTests/DataCommon/DataTestUtility.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/DataCommon/DataTestUtility.cs
@@ -92,12 +92,6 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
         //SQL Server EngineEdition
         private static string s_sqlServerEngineEdition;
 
-        // Currently, only Azure SQL supports vectors and JSON.
-        // Our CI images with specific SQL Server versions lag
-        // behind with vector and JSON support.
-        // JSON Column type
-        public static readonly bool IsJsonSupported = !IsNotAzureServer();
-        
         // Azure Synapse EngineEditionId == 6
         // More could be read at https://learn.microsoft.com/en-us/sql/t-sql/functions/serverproperty-transact-sql?view=sql-server-ver16#propertyname
         public static bool IsAzureSynapse
@@ -179,7 +173,6 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests
             ManagedIdentitySupported = c.ManagedIdentitySupported;
             IsManagedInstance = c.IsManagedInstance;
             AliasName = c.AliasName;
-            IsJsonSupported = c.IsJsonSupported;
 
 #if NETFRAMEWORK
             System.Net.ServicePointManager.SecurityProtocol |= System.Net.SecurityProtocolType.Tls12;
--- src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/VectorTest/VectorAPIValidationTest.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/VectorTest/VectorAPIValidationTest.cs
@@ -10,38 +10,83 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests.SQL.VectorTest
 {
     public sealed class VectorAPIValidationTest
     {
-        // We need this testcase to validate ref assembly for vector APIs
+        // We need these testcases to validate ref assembly for vector APIs
         // Unit tests are covered under SqlVectorTest.cs
         [Fact]
-        public void VectorAPITest()
+        public void ValidateVectorSqlDbType()
         {
             // Validate that SqlVector<float> is a valid type and has valid SqlDbType
             Assert.True(typeof(SqlVector<float>).IsValueType, "SqlVector<float> should be a value type.");
             Assert.Equal(36, (int)SqlDbTypeExtensions.Vector);
+        }
 
+        [Fact]
+        public void TestSqlVectorCreationAPIWithFloatArr()
+        {
             // Validate ctor1 with float[] : public SqlVector(System.ReadOnlyMemory<T> memory) { }
-            var vector = new SqlVector<float>(VectorFloat32TestData.testData);
-            Assert.Equal(VectorFloat32TestData.testData, vector.Memory.ToArray());
+            var testData = new float[] { 1.1f, 2.2f, 3.3f };
+            var vector = new SqlVector<float>(testData);
+            Assert.Equal(testData, vector.Memory.ToArray());
             Assert.Equal(3, vector.Length);
+        }
 
+        [Fact]
+        public void TestSqlVectorCreationAPIWithROM()
+        {
             // Validate ctor2 with ReadOnlyMemory<T> : public SqlVector(ReadOnlyMemory<T> memory) { }
-            vector = new SqlVector<float>(new ReadOnlyMemory<float>(VectorFloat32TestData.testData));
-            Assert.Equal(VectorFloat32TestData.testData, vector.Memory.ToArray());
+            var testData = new ReadOnlyMemory<float>(new float[] { 1.1f, 2.2f, 3.3f });
+            var vector = new SqlVector<float>(testData);
+            Assert.Equal(testData.ToArray(), vector.Memory.ToArray());
             Assert.Equal(3, vector.Length);
+        }
 
-            //Validate IsNull property
-            Assert.False(vector.IsNull, "IsNull should be false for non-null vector.");
-
-            // Validate Null property returns null
-            Assert.Null(SqlVector<float>.Null);
-
-            //Validate length property
-            Assert.Equal(3, vector.Length);
-
+        [Fact]
+        public void TestSqlVectorCreationAPICreateNull()
+        {
             // Validate CreateNull method
-            vector = SqlVector<float>.CreateNull(5);
+            var vector = SqlVector<float>.CreateNull(5);
             Assert.True(vector.IsNull);
             Assert.Equal(5, vector.Length);
+        }
+
+        [Fact]
+        public void TestIsNullProperty()
+        {
+            //Validate IsNull property
+            var testData = new ReadOnlyMemory<float>(new float[] { 1.1f, 2.2f, 3.3f });
+            var vector = new SqlVector<float>(testData);
+            Assert.False(vector.IsNull, "IsNull should be false for non-null vector.");
+            vector = SqlVector<float>.CreateNull(3);
+            Assert.True(vector.IsNull, "IsNull should be true for null vector.");
+        }
+
+        [Fact]
+        public void TestNullProperty()
+        {
+            // Validate Null property returns null
+            Assert.Null(SqlVector<float>.Null);
+        }
+
+        [Fact]
+        public void TestLengthProperty()
+        {
+            // Validate Length property is correctly populated for null and non-null vectors
+            var testData = new float[] { 1.1f, 2.2f, 3.3f };
+            var vector = new SqlVector<float>(testData);
+            Assert.Equal(3, vector.Length);
+            vector = SqlVector<float>.CreateNull(3);
+            Assert.Equal(3, vector.Length);
+        }
+
+        [Fact]
+        public void TestMemoryProperty()
+        {
+            // Validate Memory property is correctly populated for non-null and null vectors
+            var testData = new float[] { 1.1f, 2.2f, 3.3f };
+            var vector = new SqlVector<float>(testData);
+            Assert.Equal(testData, vector.Memory.ToArray());
+            vector = SqlVector<float>.CreateNull(3);
+            Assert.True( vector.Memory.IsEmpty, "Null vector of given size point to empty ROM");
         }
     }
 }
