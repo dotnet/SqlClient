21840359e2b7c55c64929100df6e3b29909e74c2
Saransh Sharma <sarasharma@microsoft.com>
2025-09-10 20:05:18 -0700

    Remove IsUserAgentSupportEnabled flag

--- src/Microsoft.Data.SqlClient/tests/tools/TDS/TDS.Servers/GenericTDSServerSession.cs
+++ src/Microsoft.Data.SqlClient/tests/tools/TDS/TDS.Servers/GenericTDSServerSession.cs
@@ -124,11 +124,6 @@ namespace Microsoft.SqlServer.TDS.Servers
         /// </summary>
         public bool IsVectorSupportEnabled { get; set; }
 
-        /// <summary>
-        /// Indicates whether this session supports User Agent Feature Extension
-        /// </summary>
-        public bool IsUserAgentSupportEnabled { get; set; }
-
         #region Session Options
 
         /// <summary>
