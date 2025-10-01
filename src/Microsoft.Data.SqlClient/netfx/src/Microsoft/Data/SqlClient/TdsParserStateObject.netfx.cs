3defa6fc954386d04005b1059441e8a17e9d915c
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2025-10-01 16:27:48 +0100

    netfx, netcore: sync SniWriteStatisticsAndTracing

--- src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/TdsParserStateObject.netfx.cs
+++ src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/TdsParserStateObject.netfx.cs
@@ -29,6 +29,7 @@ namespace Microsoft.Data.SqlClient
                 statistics.SafeAdd(ref statistics._bytesSent, _outBytesUsed);
                 statistics.RequestNetworkServerTimer();
             }
+#if NETFRAMEWORK
             if (SqlClientEventSource.Log.IsAdvancedTraceOn())
             {
                 // If we have tracePassword variables set, we are flushing TDSLogin and so we need to
@@ -60,6 +61,7 @@ namespace Microsoft.Data.SqlClient
                 }
             }
             SqlClientEventSource.Log.TryAdvancedTraceBinEvent("TdsParser.WritePacket | INFO | ADV | State Object Id {0}, Packet sent. Out buffer: {1}, Out Bytes Used: {2}", ObjectID, _outBuff, _outBytesUsed);
+#endif
         }
     }
 }
