cfd5d5b27f89722fa9813b280a7cc7d5141e86de
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2025-10-01 16:28:39 +0100

    Merge SniWriteStatisticsAndTracing

--- src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/TdsParserStateObject.netfx.cs
+++ src/Microsoft.Data.SqlClient/netfx/src/Microsoft/Data/SqlClient/TdsParserStateObject.netfx.cs
@@ -16,52 +16,5 @@ namespace Microsoft.Data.SqlClient
 {
     internal partial class TdsParserStateObject
     {
-        //////////////////////////////////////////////
-        // Statistics, Tracing, and related methods //
-        //////////////////////////////////////////////
-
-        private void SniWriteStatisticsAndTracing()
-        {
-            SqlStatistics statistics = _parser.Statistics;
-            if (statistics != null)
-            {
-                statistics.SafeIncrement(ref statistics._buffersSent);
-                statistics.SafeAdd(ref statistics._bytesSent, _outBytesUsed);
-                statistics.RequestNetworkServerTimer();
-            }
-#if NETFRAMEWORK
-            if (SqlClientEventSource.Log.IsAdvancedTraceOn())
-            {
-                // If we have tracePassword variables set, we are flushing TDSLogin and so we need to
-                // blank out password in buffer.  Buffer has already been sent to netlib, so no danger
-                // of losing info.
-                if (_tracePasswordOffset != 0)
-                {
-                    for (int i = _tracePasswordOffset; i < _tracePasswordOffset +
-                        _tracePasswordLength; i++)
-                    {
-                        _outBuff[i] = 0;
-                    }
-
-                    // Reset state.
-                    _tracePasswordOffset = 0;
-                    _tracePasswordLength = 0;
-                }
-                if (_traceChangePasswordOffset != 0)
-                {
-                    for (int i = _traceChangePasswordOffset; i < _traceChangePasswordOffset +
-                        _traceChangePasswordLength; i++)
-                    {
-                        _outBuff[i] = 0;
-                    }
-
-                    // Reset state.
-                    _traceChangePasswordOffset = 0;
-                    _traceChangePasswordLength = 0;
-                }
-            }
-            SqlClientEventSource.Log.TryAdvancedTraceBinEvent("TdsParser.WritePacket | INFO | ADV | State Object Id {0}, Packet sent. Out buffer: {1}, Out Bytes Used: {2}", ObjectID, _outBuff, _outBytesUsed);
-#endif
-        }
     }
 }
