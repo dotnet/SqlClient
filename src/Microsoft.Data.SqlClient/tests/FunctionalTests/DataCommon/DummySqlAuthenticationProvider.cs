14ca60418dc525b30abca05b3eb4bc4d7ff000b4
Cheena Malhotra <13396919+cheenamalhotra@users.noreply.github.com>
2025-10-03 09:39:17 -0700

    Fix SetProvider to return immediately if user-defined provider found (#3620)
    * Fix SetProvider to return immediately if user-defined provider found
    
    * Include test
    
    * Fix tests
    
    * Remove unwanted changes
    
    * Update config file name
    
    * Rename file back to app.config
    
    * Copy always
    
    * Disable tests for now.
    
    * Fix framework inclusion
    
    * Fix test failures
    
    * Touch ups
    
    * Fix test (continued)

---
+++ src/Microsoft.Data.SqlClient/tests/FunctionalTests/DataCommon/DummySqlAuthenticationProvider.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Data.SqlClient.FunctionalTests.DataCommon
+{
+    /// <summary>
+    /// Dummy class to override default Sql Authentication provider in functional tests.
+    /// This type returns a dummy access token and is only used for registration test from app.config file.
+    /// Since no actual connections are intended to be made in Functional tests, 
+    /// this type is added by default to validate config file registration scenario.
+    /// </summary>
+    public class DummySqlAuthenticationProvider : SqlAuthenticationProvider
+    {
+        public static string DUMMY_TOKEN_STR = "dummy_access_token";
+
+        public override Task<SqlAuthenticationToken> AcquireTokenAsync(SqlAuthenticationParameters parameters)
+        => Task.FromResult(new SqlAuthenticationToken(DUMMY_TOKEN_STR, new DateTimeOffset(DateTime.Now.AddHours(2))));
+
+        // Supported authentication modes don't matter for dummy test, but added to demonstrate config file usage.
+        public override bool IsSupported(SqlAuthenticationMethod authenticationMethod)
+        => authenticationMethod == SqlAuthenticationMethod.ActiveDirectoryInteractive;
+    }
+}
--- src/Microsoft.Data.SqlClient/tests/FunctionalTests/SqlAuthenticationProviderTest.cs
+++ src/Microsoft.Data.SqlClient/tests/FunctionalTests/SqlAuthenticationProviderTest.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Data.SqlClient.FunctionalTests.DataCommon;
 using Xunit;
 
 namespace Microsoft.Data.SqlClient.Tests
@@ -11,7 +12,6 @@ namespace Microsoft.Data.SqlClient.Tests
         [Theory]
         [InlineData(SqlAuthenticationMethod.ActiveDirectoryIntegrated)]
         [InlineData(SqlAuthenticationMethod.ActiveDirectoryPassword)]
-        [InlineData(SqlAuthenticationMethod.ActiveDirectoryInteractive)]
         [InlineData(SqlAuthenticationMethod.ActiveDirectoryServicePrincipal)]
         [InlineData(SqlAuthenticationMethod.ActiveDirectoryDeviceCodeFlow)]
         [InlineData(SqlAuthenticationMethod.ActiveDirectoryManagedIdentity)]
@@ -22,5 +22,18 @@ namespace Microsoft.Data.SqlClient.Tests
         {
             Assert.IsType<ActiveDirectoryAuthenticationProvider>(SqlAuthenticationProvider.GetProvider(method));
         }
+
+        #if NETFRAMEWORK
+        // This test is only valid for .NET Framework
+
+        // Overridden by app.config in this project
+        [Theory]
+        [InlineData(SqlAuthenticationMethod.ActiveDirectoryInteractive)]
+        public void DefaultAuthenticationProviders_Interactive(SqlAuthenticationMethod method)
+        {
+            Assert.IsType<DummySqlAuthenticationProvider>(SqlAuthenticationProvider.GetProvider(method));
+        }
+        
+        #endif
     }
 }
