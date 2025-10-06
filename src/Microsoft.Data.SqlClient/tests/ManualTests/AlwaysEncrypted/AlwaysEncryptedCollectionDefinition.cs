ccc79a10216622a7905bbaaba5a0b7eb591df5b9
Malcolm Daigle <mdaigle@microsoft.com>
2025-10-06 16:27:49 -0700

    Cleanup

--- src/Microsoft.Data.SqlClient/tests/ManualTests/AlwaysEncrypted/AlwaysEncryptedCollectionDefinition.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/AlwaysEncrypted/AlwaysEncryptedCollectionDefinition.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using Xunit;
 
 namespace Microsoft.Data.SqlClient.ManualTesting.Tests.AlwaysEncrypted
 {
-    [CollectionDefinition("AlwaysEncrypted")]
+    /// <summary>
+    /// This class has no code, and is never created. It serves only to
+    /// associate the AlwaysEncrypted [CollectionDefinition] and its
+    /// ICollectionFixture<> interfaces.
+    /// </summary>
+    [CollectionDefinition("AlwaysEncryptedCertStore")]
     public class AlwaysEncryptedCollectionDefinition :ICollectionFixture<SQLSetupStrategyCertStoreProvider>
     {
     }
