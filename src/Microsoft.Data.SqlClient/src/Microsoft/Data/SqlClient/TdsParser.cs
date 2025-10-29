2cd4d776dfc722ecd46f86deaa01c2c3cc982633
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2025-10-29 19:05:25 +0000

    Correct indentation of #pragma warning

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsParser.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsParser.cs
@@ -8999,9 +8999,9 @@ namespace Microsoft.Data.SqlClient
                         byte workflow = 0x00;
                         switch (fedAuthFeatureData.authentication)
                         {
-#pragma warning disable 0618 // Type or member is obsolete
+                            #pragma warning disable 0618 // Type or member is obsolete
                             case SqlAuthenticationMethod.ActiveDirectoryPassword:
-#pragma warning restore 0618 // Type or member is obsolete
+                            #pragma warning restore 0618 // Type or member is obsolete
                                 workflow = TdsEnums.MSALWORKFLOW_ACTIVEDIRECTORYPASSWORD;
                                 break;
                             case SqlAuthenticationMethod.ActiveDirectoryIntegrated:
