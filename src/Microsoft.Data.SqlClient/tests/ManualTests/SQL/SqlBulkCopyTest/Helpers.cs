fa2ba2f41729c59b8f73affb7fb22f9baf69dfe0
Paul Medynski <31868385+paulmedynski@users.noreply.github.com>
2025-11-10 15:07:34 -0400

    User Story 40114: Release activities for v5.1.8
    - Fixed Helpers namespace for real.
    - Added sunit.runner.json config to avoid XUnitExtensions strong name errors.

--- src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/SqlBulkCopyTest/Helpers.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/SqlBulkCopyTest/Helpers.cs
@@ -5,7 +5,7 @@
 using System;
 using System.Data.Common;
 
-namespace Microsoft.Data.SqlClient.ManualTesting.Tests.SQL
+namespace Microsoft.Data.SqlClient.ManualTesting.Tests
 {
     public class Helpers
     {
