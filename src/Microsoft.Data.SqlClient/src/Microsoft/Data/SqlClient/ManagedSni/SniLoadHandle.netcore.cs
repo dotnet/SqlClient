fe18fc99fb5bd8bbbd4d1f0c11902cb2bfbc8c6c
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2025-11-14 06:55:33 +0000

    Simplify properties which return a constant value - convert these to consts.

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/ManagedSni/SniLoadHandle.netcore.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/ManagedSni/SniLoadHandle.netcore.cs
@@ -15,10 +15,7 @@ namespace Microsoft.Data.SqlClient.ManagedSni
     {
         public static readonly SniLoadHandle SingletonInstance = new SniLoadHandle();
 
-        public readonly EncryptionOptions _encryptionOption = EncryptionOptions.OFF;
         public ThreadLocal<SniError> _lastError = new ThreadLocal<SniError>(static () => new SniError(SniProviders.INVALID_PROV, 0, TdsEnums.SNI_SUCCESS, string.Empty));
-
-        private readonly uint _status = TdsEnums.SNI_SUCCESS;
 
         /// <summary>
         /// Last SNI error
@@ -39,30 +36,18 @@ namespace Microsoft.Data.SqlClient.ManagedSni
         /// <summary>
         /// SNI library status
         /// </summary>
-        public uint Status
-        {
-            get
-            {
-                return _status;
-            }
-        }
+        public const uint Status = TdsEnums.SNI_SUCCESS;
 
         /// <summary>
         /// Encryption options setting
         /// </summary>
-        public EncryptionOptions Options
-        {
-            get
-            {
-                return _encryptionOption;
-            }
-        }
+        public const EncryptionOptions Options = EncryptionOptions.OFF;
 
         /// <summary>
         /// Verify client encryption possibility
         /// </summary>
         // TODO: by adding support ENCRYPT_NOT_SUP, it could be calculated.
-        public bool ClientOSEncryptionSupport => true;
+        public const bool ClientOSEncryptionSupport = true;
     }
 }
 
