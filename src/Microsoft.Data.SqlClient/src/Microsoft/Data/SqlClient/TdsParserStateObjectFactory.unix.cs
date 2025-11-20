8e3d6836a9969774b11d581e06d9787b58056217
Ben Russell <russellben@microsoft.com>
2025-11-19 18:34:58 -0600

    Add _UNIX wrapper to TdsParserStateObjectFactory.unix.cs

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsParserStateObjectFactory.unix.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsParserStateObjectFactory.unix.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if NET && _UNIX
+
 using Microsoft.Data.SqlClient.ManagedSni;
 
 namespace Microsoft.Data.SqlClient
@@ -30,3 +32,5 @@ namespace Microsoft.Data.SqlClient
         }
     }
 }
+
+#endif
