5cd3bd2facb89ec7657e68c2a34b01a461c77e21
Ben Russell <russellben@microsoft.com>
2025-11-19 18:04:11 -0600

    Split PacketHandle.Windows into PacketHandle.netcore.windows and PacketHandle.netfx.

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/PacketHandle.netcore.windows.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/PacketHandle.netcore.windows.cs
@@ -2,20 +2,24 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+// @TODO: Introduce polymorphism to remove need for this level of indirection
+#if NET && _WINDOWS
+
 using System;
 
 namespace Microsoft.Data.SqlClient
 {
-    // This structure is used for transporting packet handle references between the
-    // TdsParserStateObject base class and Managed or Native implementations. 
-    // It prevents the native IntPtr type from being boxed and prevents the need to cast from
-    // object which loses compile time type safety.
-    // It carries type information so that assertions about the type of handle can be made in the
-    // implemented abstract methods. 
-    // It is a ref struct so that it can only be used to transport the handles and not store them
-
-    // N.B. If you change this type you must also change the version for the other platform
-
+    /// <summary>
+    /// This structure is used for transporting packet handle references between the
+    /// TdsParserStateObject base class and Managed or Native implementations. It prevents the
+    /// native IntPtr type from being boxed and prevents the need to cast from object which loses
+    /// compile time type safety. It carries type information so that assertions about the type of
+    /// handle can be made in the implemented abstract methods.
+    /// </summary>
+    /// <remarks>
+    /// It is a ref struct so that it can only be used to transport the handles and not store them.
+    /// If you change this type you must also change the version for the other platform.
+    /// </remarks>
     internal readonly ref struct PacketHandle
     {
         /// <summary>
@@ -24,28 +28,26 @@ namespace Microsoft.Data.SqlClient
         /// packet which has been read from the native SNI layer.
         /// </summary>
         public const int NativePointerType = 1;
+
         /// <summary>
         /// PacketHandle is transporting a native packet. The NativePacket field is valid.
         /// A PacketHandle has this type when managed code is directly referencing a packet
         /// which is due to be passed to the native SNI layer.
         /// </summary>
         public const int NativePacketType = 2;
-        
-        #if NET
+
         /// <summary>
         /// PacketHandle is transporting a managed packet. The ManagedPacket field is valid.
         /// A PacketHandle used by the managed SNI layer will always have this type.
         /// </summary>
         public const int ManagedPacketType = 3;
 
+        // @TODO: To auto-properties
         public readonly ManagedSni.SniPacket ManagedPacket;
-        #endif
-        
         public readonly SNIPacket NativePacket;
         public readonly IntPtr NativePointer;
         public readonly int Type;
 
-        #if NET
         private PacketHandle(IntPtr nativePointer, SNIPacket nativePacket, ManagedSni.SniPacket managedPacket, int type)
         {
             Type = type;
@@ -53,30 +55,16 @@ namespace Microsoft.Data.SqlClient
             NativePointer = nativePointer;
             NativePacket = nativePacket;
         }
-        #else
-        private PacketHandle(IntPtr nativePointer, SNIPacket nativePacket, int type)
-        {
-            Type = type;
-            NativePointer = nativePointer;
-            NativePacket = nativePacket;
-        }
-        #endif
 
-        #if NET
         public static PacketHandle FromManagedPacket(ManagedSni.SniPacket managedPacket) =>
-            new PacketHandle(default, default, managedPacket, ManagedPacketType);
-
-        public static PacketHandle FromNativePointer(IntPtr nativePointer) =>
-            new PacketHandle(nativePointer, default, default, NativePointerType);
+            new PacketHandle(nativePointer: IntPtr.Zero, nativePacket: null, managedPacket, ManagedPacketType);
 
         public static PacketHandle FromNativePacket(SNIPacket nativePacket) =>
-            new PacketHandle(default, nativePacket, default, NativePacketType);
-        #else
-        public static PacketHandle FromNativePointer(IntPtr nativePointer) =>
-            new PacketHandle(nativePointer, default, NativePointerType);
+            new PacketHandle(nativePointer: IntPtr.Zero, nativePacket, managedPacket: null, NativePacketType);
 
-        public static PacketHandle FromNativePacket(SNIPacket nativePacket) =>
-            new PacketHandle(default, nativePacket, NativePacketType);
-        #endif
+        public static PacketHandle FromNativePointer(IntPtr nativePointer) =>
+            new PacketHandle(nativePointer, nativePacket: null, managedPacket: null, NativePointerType);
     }
 }
+
+#endif
