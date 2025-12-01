93c26f90e90cf69cd4a34948ebead8572cf54d9e
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2025-12-01 06:07:27 +0000

    Merge logic to create CollectionDataSet

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlMetaDataFactory.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlMetaDataFactory.cs
@@ -7,8 +7,10 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Text;
+using System.Xml;
 using Microsoft.Data.Common;
 using Microsoft.Data.ProviderBase;
 
@@ -16,14 +18,33 @@ namespace Microsoft.Data.SqlClient
 {
     internal sealed class SqlMetaDataFactory : DbMetaDataFactory
     {
+        // Well-known column names
+        private const string CollectionNameKey = "CollectionName";
+        private const string PopulationMechanismKey = "PopulationMechanism";
+        private const string PopulationStringKey = "PopulationString";
+        private const string MaximumVersionKey = "MaximumVersion";
+        private const string MinimumVersionKey = "MinimumVersion";
+        private const string RestrictionDefaultKey = "RestrictionDefault";
+        private const string RestrictionNumberKey = "RestrictionNumber";
+        private const string NumberOfRestrictionsKey = "NumberOfRestrictions";
+        private const string RestrictionNameKey = "RestrictionName";
+        private const string ParameterNameKey = "ParameterName";
 
+        // Well-known server versions
         private const string ServerVersionNormalized90 = "09.00.0000";
         private const string ServerVersionNormalized10 = "10.00.0000";
+
         private static readonly HashSet<int> s_assemblyPropertyUnsupportedEngines = new() { 6, 9, 11 };
 
-        public SqlMetaDataFactory(Stream XMLStream, string serverVersion) :
-                base(XMLStream, serverVersion)
-        { }
+        public SqlMetaDataFactory(Stream xmlStream, string serverVersion)
+        {
+            ADP.CheckArgumentNull(xmlStream, nameof(xmlStream));
+            ADP.CheckArgumentNull(serverVersion, nameof(serverVersion));
+
+            ServerVersion = serverVersion;
+
+            CollectionDataSet = LoadDataSetFromXml(xmlStream);
+        }
 
         private static void AddUDTsToDataTypesTable(DataTable dataTypesTable, SqlConnection connection, string serverVersion)
         {
@@ -257,5 +278,241 @@ namespace Microsoft.Data.SqlClient
 
             return resultTable;
         }
+
+        #region Create MetaDataCollections DataSet from XML
+        private DataSet LoadDataSetFromXml(Stream XmlStream)
+        {
+            DataSet metaDataCollectionsDataSet = new("NewDataSet")
+            {
+                Locale = CultureInfo.InvariantCulture
+            };
+            XmlReaderSettings settings = new()
+            {
+                XmlResolver = null,
+                IgnoreComments = true,
+                IgnoreWhitespace = true
+            };
+            using XmlReader reader = XmlReader.Create(XmlStream, settings);
+
+            // Build up the schema DataSet manually, then load data from XmlStream. The schema of the DataSet is defined at:
+            // * https://learn.microsoft.com/en-us/sql/connect/ado-net/common-schema-collections
+            // * https://learn.microsoft.com/en-us/sql/connect/ado-net/sql-server-schema-collections
+            // Building the schema manually is necessary because DataSet.ReadXml uses XML serialization. This is slow, and it
+            // increases the binary size of AOT assemblies by approximately 4MB.
+
+            bool readContainer = reader.Read();
+
+            // Skip past the XML declaration and the outer container element. This XML document is hardcoded;
+            // these checks will need to be adjusted if its structure changes.
+            Debug.Assert(readContainer);
+            Debug.Assert(reader.NodeType == XmlNodeType.XmlDeclaration);
+
+            readContainer = reader.Read();
+            Debug.Assert(readContainer);
+            Debug.Assert(reader.NodeType == XmlNodeType.Element);
+            Debug.Assert(reader.Name == "MetaData");
+            Debug.Assert(reader.Depth == 0);
+
+            // Iterate over each "table element" of the outer container element.
+            // LoadDataTable will read the child elements of each table element.
+            while (reader.Read()
+                && reader.Depth == 1)
+            {
+                DataTable dataTable = null;
+                Action<DataRow> rowFixup = null;
+
+                Debug.Assert(reader.NodeType == XmlNodeType.Element);
+
+                switch (reader.Name)
+                {
+                    case "MetaDataCollectionsTable":
+                        dataTable = CreateMetaDataCollectionsDataTable();
+                        break;
+                    case "RestrictionsTable":
+                        dataTable = CreateRestrictionsDataTable();
+                        break;
+                    case "DataSourceInformationTable":
+                        dataTable = CreateDataSourceInformationDataTable();
+                        rowFixup = FixUpDataSourceInformationRow;
+                        break;
+                    case "DataTypesTable":
+                        dataTable = CreateDataTypesDataTable();
+                        break;
+                    case "ReservedWordsTable":
+                        dataTable = CreateReservedWordsDataTable();
+                        break;
+                    default:
+                        Debug.Fail($"Unexpected table element name: {reader.Name}");
+                        break;
+                }
+
+                if (dataTable is not null)
+                {
+                    LoadDataTable(reader, dataTable, rowFixup);
+
+                    metaDataCollectionsDataSet.Tables.Add(dataTable);
+                }
+            }
+
+            return metaDataCollectionsDataSet;
+        }
+
+        private static void LoadDataTable(XmlReader reader, DataTable table, Action<DataRow> rowFixup)
+        {
+            int parentDepth = reader.Depth;
+
+            table.BeginLoadData();
+
+            // One outer loop per element, each loop reading every property of the row
+            while (reader.Read()
+                && reader.Depth == parentDepth + 1)
+            {
+                Debug.Assert(reader.NodeType == XmlNodeType.Element);
+                Debug.Assert(reader.Name == table.TableName);
+
+                int childDepth = reader.Depth;
+                DataRow row = table.NewRow();
+
+                // Read every child property. Hardcoded structure - start with the element name, advance to the text, then to the EndElement
+                while (reader.Read()
+                    && reader.Depth == childDepth + 1)
+                {
+                    DataColumn column;
+                    bool successfulRead;
+
+                    Debug.Assert(reader.NodeType == XmlNodeType.Element);
+
+                    column = table.Columns[reader.Name];
+                    Debug.Assert(column is not null);
+
+                    successfulRead = reader.Read();
+                    Debug.Assert(successfulRead);
+                    Debug.Assert(reader.NodeType == XmlNodeType.Text);
+
+                    row[column] = reader.Value;
+
+                    successfulRead = reader.Read();
+                    Debug.Assert(successfulRead);
+                    Debug.Assert(reader.NodeType == XmlNodeType.EndElement);
+                }
+
+                rowFixup?.Invoke(row);
+
+                table.Rows.Add(row);
+
+                Debug.Assert(reader.NodeType == XmlNodeType.EndElement);
+            }
+
+            table.EndLoadData();
+            table.AcceptChanges();
+        }
+
+        private void FixUpDataSourceInformationRow(DataRow dataSourceInfoRow)
+        {
+            Debug.Assert(dataSourceInfoRow.Table.Columns.Contains(DbMetaDataColumnNames.DataSourceProductVersion));
+            Debug.Assert(dataSourceInfoRow.Table.Columns.Contains(DbMetaDataColumnNames.DataSourceProductVersionNormalized));
+
+            dataSourceInfoRow[DbMetaDataColumnNames.DataSourceProductVersion] = ServerVersion;
+            dataSourceInfoRow[DbMetaDataColumnNames.DataSourceProductVersionNormalized] = ServerVersion;
+        }
+
+        private static DataTable CreateMetaDataCollectionsDataTable()
+            => new(DbMetaDataCollectionNames.MetaDataCollections)
+            {
+                Columns =
+                {
+                    new DataColumn(DbMetaDataColumnNames.CollectionName, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.NumberOfRestrictions, typeof(int)),
+                    new DataColumn(DbMetaDataColumnNames.NumberOfIdentifierParts, typeof(int)),
+                    new DataColumn(PopulationMechanismKey, typeof(string)),
+                    new DataColumn(PopulationStringKey, typeof(string)),
+                    new DataColumn(MinimumVersionKey, typeof(string)),
+                    new DataColumn(MaximumVersionKey, typeof(string))
+                }
+            };
+
+        private static DataTable CreateRestrictionsDataTable()
+            => new(DbMetaDataCollectionNames.Restrictions)
+            {
+                Columns =
+                {
+                    new DataColumn(DbMetaDataColumnNames.CollectionName, typeof(string)),
+                    new DataColumn(RestrictionNameKey, typeof(string)),
+                    new DataColumn(ParameterNameKey, typeof(string)),
+                    new DataColumn(RestrictionDefaultKey, typeof(string)),
+                    new DataColumn(RestrictionNumberKey, typeof(int)),
+                    new DataColumn(MinimumVersionKey, typeof(string)),
+                    new DataColumn(MaximumVersionKey, typeof(string))
+                }
+            };
+
+        private static DataTable CreateDataSourceInformationDataTable()
+            => new(DbMetaDataCollectionNames.DataSourceInformation)
+            {
+                Columns =
+                {
+                    new DataColumn(DbMetaDataColumnNames.CompositeIdentifierSeparatorPattern, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.DataSourceProductName, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.DataSourceProductVersion, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.DataSourceProductVersionNormalized, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.GroupByBehavior, typeof(GroupByBehavior)),
+                    new DataColumn(DbMetaDataColumnNames.IdentifierPattern, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.IdentifierCase, typeof(IdentifierCase)),
+                    new DataColumn(DbMetaDataColumnNames.OrderByColumnsInSelect, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.ParameterMarkerFormat, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.ParameterMarkerPattern, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.ParameterNameMaxLength, typeof(int)),
+                    new DataColumn(DbMetaDataColumnNames.ParameterNamePattern, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.QuotedIdentifierPattern, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.QuotedIdentifierCase, typeof(IdentifierCase)),
+                    new DataColumn(DbMetaDataColumnNames.StatementSeparatorPattern, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.StringLiteralPattern, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.SupportedJoinOperators, typeof(SupportedJoinOperators))
+                }
+            };
+
+        private static DataTable CreateDataTypesDataTable()
+            => new(DbMetaDataCollectionNames.DataTypes)
+            {
+                Columns =
+                {
+                    new DataColumn(DbMetaDataColumnNames.TypeName, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.ProviderDbType, typeof(int)),
+                    new DataColumn(DbMetaDataColumnNames.ColumnSize, typeof(long)),
+                    new DataColumn(DbMetaDataColumnNames.CreateFormat, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.CreateParameters, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.DataType, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.IsAutoIncrementable, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.IsBestMatch, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.IsCaseSensitive, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.IsFixedLength, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.IsFixedPrecisionScale, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.IsLong, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.IsNullable, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.IsSearchable, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.IsSearchableWithLike, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.IsUnsigned, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.MaximumScale, typeof(short)),
+                    new DataColumn(DbMetaDataColumnNames.MinimumScale, typeof(short)),
+                    new DataColumn(DbMetaDataColumnNames.IsConcurrencyType, typeof(bool)),
+                    new DataColumn(MaximumVersionKey, typeof(string)),
+                    new DataColumn(MinimumVersionKey, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.IsLiteralSupported, typeof(bool)),
+                    new DataColumn(DbMetaDataColumnNames.LiteralPrefix, typeof(string)),
+                    new DataColumn(DbMetaDataColumnNames.LiteralSuffix, typeof(string))
+                }
+            };
+
+        private static DataTable CreateReservedWordsDataTable()
+            => new(DbMetaDataCollectionNames.ReservedWords)
+            {
+                Columns =
+                {
+                    new DataColumn(DbMetaDataColumnNames.ReservedWord, typeof(string)),
+                    new DataColumn(MinimumVersionKey, typeof(string)),
+                    new DataColumn(MaximumVersionKey, typeof(string))
+                }
+            };
+        #endregion
     }
 }
