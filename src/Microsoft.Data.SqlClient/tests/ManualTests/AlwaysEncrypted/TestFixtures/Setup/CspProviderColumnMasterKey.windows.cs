213cf19a78ad451db699d83cf7112e158fc06ec5
Ben Russell <russellben@microsoft.com>
2026-01-13 18:33:52 -0600

    Cleanup ManualTests project file, make conditional files conditional

--- src/Microsoft.Data.SqlClient/tests/ManualTests/AlwaysEncrypted/TestFixtures/Setup/CspProviderColumnMasterKey.windows.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/AlwaysEncrypted/TestFixtures/Setup/CspProviderColumnMasterKey.windows.cs
@@ -2,7 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
+#if _WINDOWS
+
 using System.Security.Cryptography.X509Certificates;
 
 namespace Microsoft.Data.SqlClient.ManualTesting.Tests.AlwaysEncrypted.Setup
@@ -29,3 +30,5 @@ namespace Microsoft.Data.SqlClient.ManualTesting.Tests.AlwaysEncrypted.Setup
 
     }
 }
+
+#endif
