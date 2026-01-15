349bc47d95e658b8412b74c87e32e0561f6f1586
Paul Medynski <31868385+paulmedynski@users.noreply.github.com>
2026-01-15 09:48:38 -0400

    - Fixed stale job names. - Addressed some Copilot feedback.

--- src/Microsoft.Data.SqlClient.Extensions/Azure/test/StringExtensions.cs
+++ src/Microsoft.Data.SqlClient.Extensions/Azure/test/StringExtensions.cs
@@ -2,8 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-// Adds the missing Empty() method to string that doesn't waste time on null
-// checks like String.IsNullOrEmpty() does, and has a nice short name.
+/// <summary>
+/// Adds the missing Empty() method to string that doesn't waste time on null
+/// checks like String.IsNullOrEmpty() does, and has a nice short name.
+/// </summary>
 internal static class StringExtensions
 {
     internal static bool Empty(this string str)
