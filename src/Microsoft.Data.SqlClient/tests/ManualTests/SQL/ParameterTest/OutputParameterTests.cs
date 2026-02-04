ddbf90c0a92633a6ad779ec39e61b38dde5ad9d6
Malcolm Daigle <mdaigle@microsoft.com>
2026-02-04 10:45:44 -0800

    Convert OutputParameterTests to xunit assertion.

--- src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/ParameterTest/OutputParameterTests.cs
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/ParameterTest/OutputParameterTests.cs
@@ -2,132 +2,48 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.IO;
-using System.Text;
+using System.Data;
 using Xunit;
 
 namespace Microsoft.Data.SqlClient.ManualTesting.Tests
 {
     /// <summary>
     /// Tests for output parameters.
-    /// These tests run independently with their own baseline comparison.
     /// </summary>
     public class OutputParameterTests
     {
-        private readonly string _connStr;
-
-        public OutputParameterTests()
-        {
-            _connStr = DataTestUtility.TCPConnectionString;
-        }
-
+        /// <summary>
+        /// Tests that setting an output SqlParameter to an invalid value (e.g. a string in a decimal param)
+        /// doesn't throw, since the value is cleared before execution starts.
+        /// The output value should be correctly set by SQL Server after execution.
+        /// </summary>
         [ConditionalFact(typeof(DataTestUtility), nameof(DataTestUtility.AreConnStringsSetup), nameof(DataTestUtility.IsNotAzureSynapse))]
-        public void OutputParameterTest()
+        public void InvalidValueInOutputParameter_ShouldSucceed()
         {
-            Assert.True(RunTestAndCompareWithBaseline());
-        }
+            using var connection = new SqlConnection(DataTestUtility.TCPConnectionString);
+            connection.Open();
 
-        private bool RunTestAndCompareWithBaseline()
-        {
-            string outputPath = "OutputParameter.out";
-            string baselinePath;
-#if DEBUG
-            if (DataTestUtility.IsNotAzureServer() || DataTestUtility.IsManagedInstance)
+            // Command simply sets the output param
+            using var command = new SqlCommand("SET @decimal = 1.23", connection);
+
+            // Create valid param
+            var decimalParam = new SqlParameter("decimal", new decimal(2.34))
             {
-                baselinePath = "OutputParameter_DebugMode.bsl";
-            }
-            else
-            {
-                baselinePath = "OutputParameter_DebugMode_Azure.bsl";
-            }
-#else
-            if (DataTestUtility.IsNotAzureServer() || DataTestUtility.IsManagedInstance)
-            {
-                baselinePath = "OutputParameter_ReleaseMode.bsl";
-            }
-            else
-            {
-                baselinePath = "OutputParameter_ReleaseMode_Azure.bsl";
-            }
-#endif
+                SqlDbType = SqlDbType.Decimal,
+                Direction = ParameterDirection.Output,
+                Scale = 2,
+                Precision = 5
+            };
+            command.Parameters.Add(decimalParam);
 
-            var fstream = new FileStream(outputPath, FileMode.Create, FileAccess.Write, FileShare.Read);
-            var swriter = new StreamWriter(fstream, Encoding.UTF8);
-            var twriter = new TvpTest.CarriageReturnLineFeedReplacer(swriter);
-            Console.SetOut(twriter);
+            // Set value of param to invalid value (string instead of decimal)
+            decimalParam.Value = "Not a decimal";
 
-            // Run Test
-            OutputParameter.Run(_connStr);
+            // Execute - should not throw
+            command.ExecuteNonQuery();
 
-            Console.Out.Flush();
-            Console.Out.Dispose();
-
-            // Recover the standard output stream
-            StreamWriter standardOutput = new(Console.OpenStandardOutput());
-            standardOutput.AutoFlush = true;
-            Console.SetOut(standardOutput);
-
-            // Compare output file
-            var comparisonResult = FindDiffFromBaseline(baselinePath, outputPath);
-
-            if (string.IsNullOrEmpty(comparisonResult))
-            {
-                return true;
-            }
-
-            Console.WriteLine("OutputParameterTest Failed!");
-            Console.WriteLine("Please compare baseline: {0} with output: {1}", Path.GetFullPath(baselinePath), Path.GetFullPath(outputPath));
-            Console.WriteLine("Comparison Results:");
-            Console.WriteLine(comparisonResult);
-            return false;
-        }
-
-        private static string FindDiffFromBaseline(string baselinePath, string outputPath)
-        {
-            var expectedLines = File.ReadAllLines(baselinePath);
-            var outputLines = File.ReadAllLines(outputPath);
-
-            var comparisonSb = new StringBuilder();
-
-            var expectedLength = expectedLines.Length;
-            var outputLength = outputLines.Length;
-            var findDiffLength = Math.Min(expectedLength, outputLength);
-
-            for (var lineNo = 0; lineNo < findDiffLength; lineNo++)
-            {
-                if (!expectedLines[lineNo].Equals(outputLines[lineNo]))
-                {
-                    comparisonSb.AppendFormat("** DIFF at line {0} \n", lineNo);
-                    comparisonSb.AppendFormat("A : {0} \n", outputLines[lineNo]);
-                    comparisonSb.AppendFormat("E : {0} \n", expectedLines[lineNo]);
-                }
-            }
-
-            var startIndex = findDiffLength - 1;
-            if (startIndex < 0)
-            {
-                startIndex = 0;
-            }
-
-            if (findDiffLength < expectedLength)
-            {
-                comparisonSb.AppendFormat("** MISSING \n");
-                for (var lineNo = startIndex; lineNo < expectedLength; lineNo++)
-                {
-                    comparisonSb.AppendFormat("{0} : {1}", lineNo, expectedLines[lineNo]);
-                }
-            }
-            if (findDiffLength < outputLength)
-            {
-                comparisonSb.AppendFormat("** EXTRA \n");
-                for (var lineNo = startIndex; lineNo < outputLength; lineNo++)
-                {
-                    comparisonSb.AppendFormat("{0} : {1}", lineNo, outputLines[lineNo]);
-                }
-            }
-
-            return comparisonSb.ToString();
+            // Validate - the output value should be set correctly by SQL Server
+            Assert.Equal(new decimal(1.23), (decimal)decimalParam.Value);
         }
     }
 }
