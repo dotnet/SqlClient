ba6087d8ac4f19a8b105a89e850268f0fc06b694
Malcolm Daigle <mdaigle@microsoft.com>
2026-02-26 13:10:30 -0800

    Feature | Add enhanced routing env change support. (#3970)
    * Add enhanced routing env change support.
    
    * Update comments.
    
    * Remove private setter.
    
    * Add using statements.

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlUtil.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlUtil.cs
@@ -1182,6 +1182,15 @@ namespace Microsoft.Data.SqlClient
             return exc;
         }
 
+        internal static Exception ROR_InvalidEnhancedRoutingInfo(SqlConnectionInternal internalConnection)
+        {
+            SqlErrorCollection errors = new SqlErrorCollection();
+            errors.Add(new SqlError(0, (byte)0x00, TdsEnums.FATAL_ERROR_CLASS, null, (StringsHelper.GetString(Strings.SQLROR_InvalidEnhancedRoutingInfo)), "", 0));
+            SqlException exc = SqlException.CreateException(errors, null, internalConnection, innerException: null, batchCommand: null);
+            exc._doNotReconnect = true;
+            return exc;
+        }
+
         internal static Exception ROR_TimeoutAfterRoutingInfo(SqlConnectionInternal internalConnection)
         {
             SqlErrorCollection errors = new SqlErrorCollection();
