4bbf86bfe6c3309f903474f187e7f3dcabaaa37d
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2026-03-08 21:37:40 +0000

    Add initial logging implementation for generic tracing

---
+++ src/Microsoft.Data.SqlClient.Extensions/Abstractions/src/Logging/CategoryNames.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.Data.SqlClient.Extensions.Abstractions.Logging;
+
+/// <summary>
+/// The names of logging categories written to by Microsoft.Data.SqlClient.
+/// </summary>
+public static class CategoryNames
+{
+    /// <summary>
+    /// Captures basic application flow trace events.
+    /// </summary>
+    public const string Trace = "Microsoft.Data.SqlClient.Logging.Trace";
+}
