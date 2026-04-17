7e416bd9ab157e7ab3e3175346f105d955d702df
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2026-04-17 06:11:31 +0100

    Split conditional compilation blocks

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlBatch.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlBatch.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -43,7 +43,9 @@ namespace Microsoft.Data.SqlClient
 
         /// <include file='../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlBatch.xml' path='docs/members[@name="SqlBatch"]/CreateDbBatchCommand/*'/>
         protected override DbBatchCommand CreateDbBatchCommand() => new SqlBatchCommand();
-        #else
+        #endif
+
+        #if NETFRAMEWORK
         /// <inheritdoc cref="System.IAsyncDisposable.DisposeAsync"/>
         public virtual ValueTask DisposeAsync()
         {
