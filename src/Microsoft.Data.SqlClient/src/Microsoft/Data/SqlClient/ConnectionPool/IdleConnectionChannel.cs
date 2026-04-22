ccdf316c8bf3faf2ca1fa4050f0a39074a95eed3
Malcolm Daigle <mdaigle@microsoft.com>
2026-04-22 16:15:02 -0700

    Fix using statement.

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/ConnectionPool/IdleConnectionChannel.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/ConnectionPool/IdleConnectionChannel.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 using System.Threading;
+using System.Threading.Tasks;
 using System.Threading.Channels;
 using Microsoft.Data.ProviderBase;
 
@@ -49,7 +50,7 @@ namespace Microsoft.Data.SqlClient.ConnectionPool
                 }
                 return true;
             }
-            
+
             return false;
         }
 
