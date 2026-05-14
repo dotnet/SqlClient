3d8176ac607dc4d7ee813508755b6c435a8a4ab3
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2026-05-14 20:13:26 +0100

    Replace ActiveIssue attributes with [Theory(Skip = "...")]

--- src/Microsoft.Data.SqlClient/tests/UnitTests/Microsoft/Data/Sql/DacResponseProcessorTest.cs
+++ src/Microsoft.Data.SqlClient/tests/UnitTests/Microsoft/Data/Sql/DacResponseProcessorTest.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -9,25 +9,22 @@ namespace Microsoft.Data.Sql.UnitTests;
 
 public class DacResponseProcessorTest
 {
-    [Theory]
+    [Theory(Skip = "Implementation in progress, see GH #3700")]
     [MemberData(nameof(SsrpPacketTestData.EmptyPacketBuffer), MemberType = typeof(SsrpPacketTestData), DisableDiscoveryEnumeration = true)]
-    [ActiveIssue("https://github.com/dotnet/SqlClient/issues/3700")]
     public void Process_EmptyBuffer_ReturnsFalse(ReadOnlySequence<byte> packetBuffers)
     {
         _ = packetBuffers;
     }
 
-    [Theory]
+    [Theory(Skip = "Implementation in progress, see GH #3700")]
     [MemberData(nameof(SsrpPacketTestData.InvalidSVR_RESP_DACPackets), MemberType = typeof(SsrpPacketTestData), DisableDiscoveryEnumeration = true)]
-    [ActiveIssue("https://github.com/dotnet/SqlClient/issues/3700")]
     public void Process_InvalidDacResponse_ReturnsFalse(ReadOnlySequence<byte> packetBuffers)
     {
         _ = packetBuffers;
     }
 
-    [Theory]
+    [Theory(Skip = "Implementation in progress, see GH #3700")]
     [MemberData(nameof(SsrpPacketTestData.ValidSVR_RESP_DACPacketBuffer), MemberType = typeof(SsrpPacketTestData), DisableDiscoveryEnumeration = true)]
-    [ActiveIssue("https://github.com/dotnet/SqlClient/issues/3700")]
     public void Process_ValidDacResponse_ReturnsData(ReadOnlySequence<byte> packetBuffers, int expectedDacPort)
     {
         _ = packetBuffers;
--- src/Microsoft.Data.SqlClient/tests/UnitTests/Microsoft/Data/Sql/SqlDataSourceResponseProcessorTest.cs
+++ src/Microsoft.Data.SqlClient/tests/UnitTests/Microsoft/Data/Sql/SqlDataSourceResponseProcessorTest.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -9,49 +9,43 @@ namespace Microsoft.Data.Sql.UnitTests;
 
 public class SqlDataSourceResponseProcessorTest
 {
-    [Theory]
+    [Theory(Skip = "Implementation in progress, see GH #3700")]
     [MemberData(nameof(SsrpPacketTestData.EmptyPacketBuffer), MemberType = typeof(SsrpPacketTestData), DisableDiscoveryEnumeration = true)]
-    [ActiveIssue("https://github.com/dotnet/SqlClient/issues/3700")]
     public void Process_EmptyBuffer_ReturnsFalse(ReadOnlySequence<byte> packetBuffers)
     {
         _ = packetBuffers;
     }
 
-    [Theory]
+    [Theory(Skip = "Implementation in progress, see GH #3700")]
     [MemberData(nameof(SsrpPacketTestData.InvalidSVR_RESPPackets), MemberType = typeof(SsrpPacketTestData), DisableDiscoveryEnumeration = true)]
-    [ActiveIssue("https://github.com/dotnet/SqlClient/issues/3700")]
     public void Process_InvalidSqlDataSourceResponse_ReturnsFalse(ReadOnlySequence<byte> packetBuffers)
     {
         _ = packetBuffers;
     }
 
-    [Theory]
+    [Theory(Skip = "Implementation in progress, see GH #3700")]
     [MemberData(nameof(SsrpPacketTestData.InvalidRESP_DATAPackets), MemberType = typeof(SsrpPacketTestData), DisableDiscoveryEnumeration = true)]
-    [ActiveIssue("https://github.com/dotnet/SqlClient/issues/3700")]
     public void Process_InvalidSqlDataSourceResponse_RESP_DATA_ReturnsFalse(ReadOnlySequence<byte> packetBuffers)
     {
         _ = packetBuffers;
     }
 
-    [Theory]
+    [Theory(Skip = "Implementation in progress, see GH #3700")]
     [MemberData(nameof(SsrpPacketTestData.InvalidTCP_INFOPackets), MemberType = typeof(SsrpPacketTestData), DisableDiscoveryEnumeration = true)]
-    [ActiveIssue("https://github.com/dotnet/SqlClient/issues/3700")]
     public void Process_InvalidSqlDataSourceResponse_TCP_INFO_ReturnsFalse(ReadOnlySequence<byte> packetBuffers)
     {
         _ = packetBuffers;
     }
 
-    [Theory]
+    [Theory(Skip = "Implementation in progress, see GH #3700")]
     [MemberData(nameof(SsrpPacketTestData.Invalid_CLNT_UCAST_INST_SVR_RESPPackets), MemberType = typeof(SsrpPacketTestData), DisableDiscoveryEnumeration = true)]
-    [ActiveIssue("https://github.com/dotnet/SqlClient/issues/3700")]
     public void Process_InvalidSqlDataSourceResponseToCLNT_UCAST_INST_ReturnsFalse(ReadOnlySequence<byte> packetBuffers)
     {
         _ = packetBuffers;
     }
 
-    [Theory]
+    [Theory(Skip = "Implementation in progress, see GH #3700")]
     [MemberData(nameof(SsrpPacketTestData.ValidSVR_RESPPacketBuffer), MemberType = typeof(SsrpPacketTestData), DisableDiscoveryEnumeration = true)]
-    [ActiveIssue("https://github.com/dotnet/SqlClient/issues/3700")]
     public void Process_ValidSqlDataSourceResponse_ReturnsData(ReadOnlySequence<byte> packetBuffers, string expectedVersion, int expectedTcpPort, string? expectedPipeName)
     {
         _ = packetBuffers;
