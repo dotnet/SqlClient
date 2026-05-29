27a0d1d3c8c269e6e1be64e4e1bc1aaa47d57b11
Cheena Malhotra <13396919+cheenamalhotra@users.noreply.github.com>
2026-05-28 23:26:50 -0700

    Tests | Address transient errors in CI pipelines (#4314)
    * Retry installing dotnet in CI + address flaky tests
    
    * One more flaky test
    
    * FIx TestDefaultAppContextSwitchValues to be deterministic

--- src/Microsoft.Data.SqlClient/tests/UnitTests/SimulatedServerTests/ConnectionRoutingTestsAzure.cs
+++ src/Microsoft.Data.SqlClient/tests/UnitTests/SimulatedServerTests/ConnectionRoutingTestsAzure.cs
@@ -159,6 +159,7 @@ namespace Microsoft.Data.SqlClient.UnitTests.SimulatedServerTests
         }
 
         [Fact]
+        [Trait("Category", "flaky")]
         public void NetworkTimeoutAtRoutedLocation_RetryDisabled_ShouldFail()
         {
             // Arrange
