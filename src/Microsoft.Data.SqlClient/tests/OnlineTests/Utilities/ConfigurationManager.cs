f6bd19f7ac5bbabd43cbab40e73203b269f4f8ac
Ben Russell <russellben@microsoft.com>
2026-07-02 14:35:19 -0500

    Sketch of caching type names for a connection, and loading config from file.

---
+++ src/Microsoft.Data.SqlClient/tests/OnlineTests/Utilities/ConfigurationManager.cs
@@ -0,0 +1,102 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace Microsoft.Data.SqlClient.OnlineTests.Utilities;
+
+public static class ConfigurationManager
+{
+    #region Constants
+
+    public const string ConfigPathEnvironmentVariable = "MDS_ONLINE_TEST_CONFIG";
+
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        AllowTrailingCommas = true,
+        PropertyNameCaseInsensitive = true,
+        ReadCommentHandling = JsonCommentHandling.Skip,
+    };
+
+    #endregion
+
+    #region Properties
+
+    public static Configuration? Configuration { get; private set; }
+
+    #endregion
+
+    #region Methods
+
+    [System.Runtime.CompilerServices.ModuleInitializer]
+    public static void Initialize()
+    {
+        // Skip initialization if we're already initialized
+        if (Configuration is not null)
+        {
+            return;
+        }
+
+        // Deserialize the configuration and cache metadata about the server
+        DeserializedConfig config = LoadConfig();
+
+    }
+
+    private static DeserializedConfig LoadConfig()
+    {
+        // Determine list of paths to try:
+        List<string?> configPaths = [
+            Environment.GetEnvironmentVariable(ConfigPathEnvironmentVariable),
+            "config.jsonc",
+            "config.json"
+        ];
+
+        // Go down the list to load the config
+        foreach (string? config in configPaths)
+        {
+            DeserializedConfig? deserializedConfig = LoadConfigInternal(config);
+            if (deserializedConfig is not null)
+            {
+                return deserializedConfig;
+            }
+        }
+
+        // Made to the end without loading anything. Throw.
+        IEnumerable<string> expandedPaths = configPaths.OfType<string>().Select(Path.GetFullPath);
+        string triedPaths = string.Join(", ", expandedPaths);
+
+        throw new FileNotFoundException($"Failed to load configuration from: {triedPaths}");
+    }
+
+    private static DeserializedConfig? LoadConfigInternal(string? configPath)
+    {
+        if (configPath is null)
+        {
+            return null;
+        }
+
+        try
+        {
+            using StreamReader sr = new StreamReader(configPath);
+            return JsonSerializer.Deserialize<DeserializedConfig>(sr.ReadToEnd(), JsonSerializerOptions)
+                   ?? throw new InvalidOperationException($"Failed to deserialize config from '{configPath}");
+        }
+        catch (Exception e) when (e is FileNotFoundException or DirectoryNotFoundException)
+        {
+            // File did not exist at the give path. Return null to signal to try a different location.
+            return null;
+        }
+    }
+
+    #endregion
+
+    public class DeserializedConfig
+    {
+        public string[] ConnectionStrings { get; set; }
+    }
+}
---
+++ src/Microsoft.Data.SqlClient/tests/OnlineTests/Utilities/ConnectionMetadata.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Microsoft.Data.SqlClient.OnlineTests.Utilities;
+
+public class ConnectionMetadata
+{
+    private readonly HashSet<string> _cachedTypeNames;
+    private readonly Dictionary<ConnectionTraits, Lazy<bool>> _cachedConnectionTraits;
+
+    public ConnectionMetadata(string connectionString)
+    {
+        ConnectionString = connectionString;
+
+        // Verify that the connection is usable by caching the metadata
+        using SqlConnection connection = new(connectionString);
+        connection.Open();
+
+        _cachedTypeNames = ReadTypeNames(connection);
+
+        // Initialize trait handlers
+        _cachedConnectionTraits = new Dictionary<ConnectionTraits, Lazy<bool>>
+        {
+            { ConnectionTraits.SupportsJson, new Lazy<bool>(() => _cachedTypeNames.Contains("json")) },
+            { ConnectionTraits.SupportsVector, new Lazy<bool>(() => _cachedTypeNames.Contains("vector")) }
+        };
+    }
+
+    public string ConnectionString { get; private set; }
+
+    public bool HasTrait(ConnectionTraits trait) =>
+        _cachedConnectionTraits.TryGetValue(trait, out Lazy<bool>? lazyTraitValue)
+            ? lazyTraitValue.Value
+            : false;
+
+    private static HashSet<string> ReadTypeNames(SqlConnection connection)
+    {
+        using SqlCommand command = connection.CreateCommand();
+        command.CommandType = CommandType.Text;
+        command.CommandText = "SELECT [name] FROM SYS.TYPES";
+
+        using SqlDataReader reader = command.ExecuteReader();
+
+        HashSet<string> typeNames = new();
+        while (reader.Read())
+        {
+            typeNames.Add(reader.GetString(0));
+        }
+
+        return typeNames;
+    }
+}
