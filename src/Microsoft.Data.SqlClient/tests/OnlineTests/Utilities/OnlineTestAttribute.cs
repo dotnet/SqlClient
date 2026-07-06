7941018f6aaba51091438fcbabda0cd10b737873
Ben Russell <russellben@microsoft.com>
2026-07-06 13:58:42 -0500

    Skeleton of the test attribute and discoverer

---
+++ src/Microsoft.Data.SqlClient/tests/OnlineTests/Utilities/OnlineTestAttribute.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Microsoft.Data.SqlClient.OnlineTests.Utilities;
+
+[AttributeUsage(AttributeTargets.Method)]
+[XunitTestCaseDiscoverer(
+    "Microsoft.Data.SqlClient.OnlineTests.Utilities.OnlineTestDiscoverer",
+    "Microsoft.Data.SqlClient.OnlineTests")]
+public class OnlineTestAttribute : TheoryAttribute
+{
+    public OnlineTestAttribute(
+        ConnectionTraits[]? requiredTraits = null,
+        ConnectionTraits[]? blockedTraits = null)
+    {
+        BlockedTraits = blockedTraits ?? [];
+        RequiredTraits = requiredTraits ?? [];
+    }
+
+    public ConnectionTraits[] BlockedTraits { get; }
+
+    public ConnectionTraits[] RequiredTraits { get; }
+}
---
+++ src/Microsoft.Data.SqlClient/tests/OnlineTests/Utilities/OnlineTestDiscoverer.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Microsoft.Data.SqlClient.OnlineTests.Utilities;
+
+public class OnlineTestDiscoverer : TheoryDiscoverer
+{
+    public OnlineTestDiscoverer(IMessageSink diagnosticMessageSink)
+        : base(diagnosticMessageSink)
+    {
+    }
+
+    protected override IEnumerable<IXunitTestCase> CreateTestCasesForTheory(
+        ITestFrameworkDiscoveryOptions discoveryOptions,
+        ITestMethod testMethod,
+        IAttributeInfo theoryAttribute)
+    {
+        return base.CreateTestCasesForTheory(discoveryOptions, testMethod, theoryAttribute);
+    }
+}
