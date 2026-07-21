4a86f1e6c0d8faa88e1ff82c38bb22af7819e1bc
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2026-07-21 15:50:48 +0100

    Merge TdsParserStateObjectNative and NativeSspiContextProvider

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SSPI/NativeSspiContextProvider.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SSPI/NativeSspiContextProvider.cs
@@ -1,8 +1,6 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-
-#if _WINDOWS
 
 using System;
 using System.Buffers;
@@ -85,5 +83,3 @@ namespace Microsoft.Data.SqlClient
         }
     }
 }
-
-#endif
--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsParserStateObjectNative.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsParserStateObjectNative.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if _WINDOWS
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -532,5 +530,3 @@ namespace Microsoft.Data.SqlClient
         }
     }
 }
-
-#endif
