7a6fb7183241ddebc1e7bc35ddf68084c734a500
Ben Russell <russellben@microsoft.com>
2026-09-02 18:36:00 -0500

    Fields => properties where possible, add comments

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/Parser/Tokens/TdsUdtTypeInfo.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/Parser/Tokens/TdsUdtTypeInfo.cs
@@ -7,21 +7,50 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Microsoft.Data.SqlClient.Parser.Tokens;
 
+/// <summary>
+/// Represents UDT metadata inside a TYPE_INFO TDS object used by metadata and return value tokens.
+/// </summary>
 internal sealed class TdsUdtTypeInfo
 {
-    #if NET
-    [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
-    #endif
-    internal Type Type;
-
-    internal string DatabaseName;
-
-    internal string SchemaName;
-
-    internal string TypeName;
-
+    /// <summary>
+    /// Represents the fully qualified type name of the UDT, including the name of the assembly
+    /// in which the type is defined.
+    /// </summary>
     internal string AssemblyQualifiedName;
 
+    /// <summary>
+    /// Represents the name of the database associated with the UDT.
+    /// </summary>
+    // @TODO: This cannot be an auto property yet because this value is set via an out parameter in TryRead*.
+    internal string DatabaseName;
+
+    /// <summary>
+    /// Represents the schema name of the UDT within the TYPE_INFO TDS object.
+    /// </summary>
+    // @TODO: This cannot be an auto property yet because this value is set via an out parameter in TryRead*.
+    internal string SchemaName;
+
+    /// <summary>
+    /// Represents the name of the UDT as specified in the TYPE_INFO TDS object.
+    /// </summary>
+    // @TODO: This cannot be an auto property yet because this value is set via an out parameter in TryRead*.
+    internal string TypeName;
+
+    /// <summary>
+    /// Represents the data type the UDT, providing metadata necessary for runtime type resolution
+    /// and compatibility checks.
+    /// </summary>
+    #if NET
+    [field: DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
+    #endif
+    internal Type Type { get; set; }
+
+    /// <summary>
+    /// Copies the information from the provided <see cref="TdsUdtTypeInfo"/> instance to the current instance.
+    /// </summary>
+    /// <param name="original">
+    /// The <see cref="TdsUdtTypeInfo"/> instance from which to copy the information.
+    /// </param>
     public void CopyFrom(TdsUdtTypeInfo original)
     {
         if (original != null)
